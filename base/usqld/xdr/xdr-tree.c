536136a39bf11eaf3de62887c5620f7ee1fc7062
occ <occ@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-03-01 15:07:58 +0000

    Initial revision
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@33 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/usqld/xdr/xdr-tree.c
@@ -0,0 +1,92 @@
+#include "xdr.h"
+#include <sys/types.h>
+#include <stdlib.h>
+#include "xdr.h"
+#include <assert.h>
+
+int  XDR_tree_new_compound(XDR_type type,
+			   size_t nelems,
+			   XDR_tree_compound ** out_t){
+  XDR_tree_compound * t ;
+  assert(NULL!=out_t);
+
+  t = mylloc(XDR_tree_compound);
+  assert(NULL!=t);
+  t->type = type;
+  t->subelems = myllocn(XDR_tree*,nelems);
+  if(NULL==t->subelems){
+    free(t);
+    return XDR_PMALLOC;
+  }
+  bzero(t->subelems,sizeof(XDR_tree*) * nelems);
+  t->nelems = nelems;
+  *out_t   = t;
+  return XDR_OK;
+}
+
+
+void XDR_tree_new_simple(XDR_type type,XDR_tree_simple **out_t){
+  XDR_tree_simple * t = mylloc(XDR_tree_simple);
+  assert(NULL!=t);
+  t->type = type;
+  bzero(t->val,sizeof(XDR_tree_simple_val));
+  *out_t = t;
+}
+
+int  XDR_tree_new_str (XDR_type type,
+		       size_t len,
+		       XDR_tree_str** out_t){
+  
+  XDR_tree_str * t = mylloc(XDR_tree_str);
+  assert(NULL!=t);
+  t->type = type;
+  t->len = len;
+
+  if(type==XDR_STRING)
+    len++; //null char;
+
+  t->data = myllocn(unsigned char,len);
+  if(NULL !=t->data){
+    free(t);
+    return XDR_PMALLOC;
+  }
+  bzero(t->data,len);
+
+  *out_t = t;
+  return XDR_OK;
+}
+
+
+void XDR_tree_free(XDR_tree * t){
+
+  switch(t->type){
+  case XDR_STRING:
+  case XDR_VAROPAQUE:
+  case XDR_FIXEDOPAQUE:
+    {
+      XDR_tree_str * o = (XDR_tree_str*) t;
+      if(NULL!=o->data)
+	free(o->data);    
+    }
+    break;
+  case XDR_STRUCT:
+  case XDR_FIXEDARRAY:
+  case XDR_VARARRAY:
+  case XDR_UNION:
+    {
+      XDR_tree_compound * c = (XDR_tree_compound *)t;
+      int i;
+      
+      for(i==0;i<c->nelems;i++){
+	if(NULL!=c->subelems[i])
+	  XDR_tree_free(c->subelems[i]);
+      }
+      
+      if(c->subelems!=NULL){
+	free(c->subelems);
+      }
+    }
+  }
+  
+  free(t);
+}
