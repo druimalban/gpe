6dd99692344eeb5a602d1ad8fefa3896dc0f446f
occ <occ@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-03-14 20:43:46 +0000

    added a new tests directory instead of stuff just sitting in /src/
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@75 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/usqld/test/usqld-client-test.c
@@ -0,0 +1,71 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "usql.h"
+#include "usqld-protocol.h"
+#include "xdr.h"
+
+int test_print_results(void * f,int nrows,char ** heads,char ** vals){
+  int i;
+  for(i = 0;i<nrows;i++){
+    printf("%s\t|",heads[i]);
+  }
+  printf("\n");
+  for(i = 0;i<nrows;i++){
+    printf("%s\t|",vals[i]);
+  }
+  printf("\n");
+  return 0;
+}
+
+int main (int argc, char ** argv) 
+{
+  usqld_conn * con;
+  char * errstring;
+  XDR_schema * s;
+  
+  
+  s = usqld_get_protocol();
+  XDR_dump_schema(s);
+  if(NULL==(con=usqld_connect("occ2.cs.bath.ac.uk",
+			      "test",&errstring))){
+    fprintf(stderr,"couldn't connect to database: %s\n",errstring);
+    exit(1);
+  }
+  fprintf(stderr,"connected to database\n");
+  if(USQLD_OK!=
+     usqld_exec(con,
+		"CREATE TABLE foo(x int, y int)",
+		NULL,NULL,&errstring)){
+    fprintf(stderr,"couldn't query database: %s\n",errstring);
+    exit(1);
+  }
+
+  if(USQLD_OK!=
+     usqld_exec(con,
+		"INSERT INTO foo VALUES(1,2)",
+		NULL,NULL,&errstring)){
+    fprintf(stderr,"couldn't query database: %s\n",errstring);
+    exit(1);
+  }
+
+  if(USQLD_OK!=
+     usqld_exec(con,
+		"INSERT INTO foo VALUES(2,3)",
+		NULL,NULL,&errstring)){
+    fprintf(stderr,"couldn't query database: %s\n",errstring);
+    exit(1);
+  }
+  
+  if(USQLD_OK!=
+     usqld_exec(con,
+		"SELECT x,y from foo",
+		test_print_results,NULL,&errstring)){
+    fprintf(stderr,"couldn't query database: %s\n",errstring);
+    exit(1);
+  }
+  return 0;
+}
