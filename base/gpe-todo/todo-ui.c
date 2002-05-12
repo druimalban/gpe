dcf6262fcff468e60805935b1c02bf809600705e
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-05-12 18:08:27 +0000

    UI tweaks
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@178 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-todo/todo-ui.c
+++ base/gpe-todo/todo-ui.c
@@ -10,11 +10,14 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <time.h>
+#include <libintl.h>
 
 #include <gtk/gtk.h>
 
 #include "gtkdatecombo.h"
 #include "todo.h"
+
+#define _(_x) gettext(_x)
 
 struct edit_todo
 {
@@ -85,13 +88,13 @@ edit_todo(struct todo_list *list, struct todo_item *item)
 {
   GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   GtkWidget *vbox = gtk_vbox_new (FALSE, 0);
-  GtkWidget *hbox = gtk_hbox_new (FALSE, 0);
   GtkWidget *text = gtk_text_new (NULL, NULL);
   GtkWidget *duebox = gtk_hbox_new (FALSE, 0);
   GtkWidget *donebox = gtk_hbox_new (FALSE, 0);
   GtkWidget *buttonbox = gtk_hbox_new (FALSE, 0);
-  GtkWidget *buttonok = gtk_button_new_with_label ("OK");
-  GtkWidget *buttoncancel = gtk_button_new_with_label ("Cancel");
+  GtkWidget *buttonok = gtk_button_new_with_label (_("Save"));
+  GtkWidget *buttoncancel = gtk_button_new_with_label (_("Cancel"));
+  GtkWidget *buttondelete = gtk_button_new_with_label (_("Delete"));
   GtkWidget *state = gtk_option_menu_new ();
   GtkWidget *state_menu = gtk_menu_new ();
   struct edit_todo *t = g_malloc(sizeof(struct edit_todo));
@@ -100,13 +103,13 @@ edit_todo(struct todo_list *list, struct todo_item *item)
   char buf[32];
 
   gtk_menu_append (GTK_MENU (state_menu), 
-		   gtk_menu_item_new_with_label ("Not started"));
+		   gtk_menu_item_new_with_label (_("Not started")));
   gtk_menu_append (GTK_MENU (state_menu), 
-		   gtk_menu_item_new_with_label ("In progress"));
+		   gtk_menu_item_new_with_label (_("In progress")));
   gtk_menu_append (GTK_MENU (state_menu), 
-		   gtk_menu_item_new_with_label ("Completed"));
+		   gtk_menu_item_new_with_label (_("Completed")));
 
-  t->duetoggle = gtk_toggle_button_new_with_label ("Due:");
+  t->duetoggle = gtk_check_button_new_with_label (_("Due:"));
   t->duedate = gtk_date_combo_new ();
 
   t->list = list;
@@ -128,15 +131,15 @@ edit_todo(struct todo_list *list, struct todo_item *item)
   gtk_signal_connect (GTK_OBJECT (buttoncancel), "clicked",
 		      GTK_SIGNAL_FUNC (click_cancel), window);
 
-  gtk_box_pack_end (GTK_BOX (buttonbox), buttoncancel, FALSE, FALSE, 4);
-  gtk_box_pack_end (GTK_BOX (buttonbox), buttonok, FALSE, FALSE, 4);
+  gtk_box_pack_start (GTK_BOX (buttonbox), buttondelete, TRUE, FALSE, 4);
+  gtk_box_pack_start (GTK_BOX (buttonbox), buttoncancel, TRUE, FALSE, 4);
+  gtk_box_pack_start (GTK_BOX (buttonbox), buttonok, TRUE, FALSE, 4);
 
   gtk_option_menu_set_menu (GTK_OPTION_MENU (state), state_menu);
 
   gtk_box_pack_start (GTK_BOX (vbox), state, FALSE, FALSE, 2);
   gtk_box_pack_start (GTK_BOX (vbox), duebox, FALSE, FALSE, 2);
   gtk_box_pack_start (GTK_BOX (vbox), text, TRUE, TRUE, 4);
-  gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 4);
   gtk_box_pack_start (GTK_BOX (vbox), buttonbox, FALSE, FALSE, 2);
 
   gtk_text_set_editable (GTK_TEXT (text), TRUE);
@@ -150,12 +153,11 @@ edit_todo(struct todo_list *list, struct todo_item *item)
       the_time = item->time;
       gtk_editable_insert_text (GTK_EDITABLE (text), item->what, 
 				strlen (item->what), &p);
-      //      gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON (check),
-      //				    item->completed);
     }
   else
     {
-      time(&the_time);
+      time (&the_time);
+      gtk_widget_set_sensitive (buttondelete, FALSE);
     }
 
   t->text = text;
