012bf384d470adf6f3e79ebfaf5d91ee5c6023a5
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-05-26 12:41:36 +0000

    tweak layout of day-view some more
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@217 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-calendar/day_view.c
+++ base/gpe-calendar/day_view.c
@@ -25,6 +25,8 @@ extern GdkFont *timefont, *datefont;
   
 gint bias = 8;
 
+GSList *strings;
+
 #define SECONDS_IN_DAY (24*60*60)
 
 void selection_made( GtkWidget      *clist,
@@ -33,9 +35,9 @@ void selection_made( GtkWidget      *clist,
                      GdkEventButton *event,
                      GtkWidget      *widget)
 {
-    event_t ev;
+  event_t ev;
     
-    if (event->type == GDK_2BUTTON_PRESS)
+  if (event->type == GDK_2BUTTON_PRESS)
     {
       guint hour = row;
       GtkWidget *appt;
@@ -54,7 +56,38 @@ void selection_made( GtkWidget      *clist,
 	}
       gtk_widget_show (appt);
     }
-    return;
+}
+
+static char *
+format_event (event_t ev)
+{
+  char buf[256];
+  char *p = buf;
+  size_t l = sizeof (buf);
+  size_t r;
+  struct tm tm;
+  time_t t;
+  event_details_t evd;
+
+  localtime_r (&ev->start, &tm);
+  r = strftime (p, l, "%R-", &tm);
+  if (r == 0) return NULL;
+  p += r;
+  l -= r;
+
+  t = ev->start + ev->duration;
+  localtime_r (&t, &tm);
+  r = strftime (p, l, "%R ", &tm);
+  if (r == 0) return NULL;
+  p += r;
+  l -= r;
+
+  evd = event_db_get_details (ev);
+  if (evd == NULL) return NULL;
+  strncpy (p, evd->summary, l - 1);
+  p[l - 1] = 0;
+  
+  return g_strdup (buf);
 }
 
 static gint
@@ -65,12 +98,12 @@ day_view_update ()
   GdkColor light_color, dark_color, time_color;
   guint hour, j, row=0;
   time_t start, end;
-  event_t ev;
-  event_details_t evd;
   struct tm tm_start, tm_end;
   char buf[10];
   gchar *line_info[2];
   GSList *day_events[24];
+  guint width = 0, i;
+  GSList *iter;
       
   light_color.red = 60000;
   light_color.green = 60000;
@@ -84,82 +117,118 @@ day_view_update ()
   dark_color.green = 45000;
   dark_color.blue = 45000;
      
-  light_style = gtk_style_copy(gtk_widget_get_style(day_list));
-  dark_style = gtk_style_copy(gtk_widget_get_style(day_list));
-  time_style = gtk_style_copy(gtk_widget_get_style(day_list));
+  light_style = gtk_style_copy (gtk_widget_get_style (day_list));
+  dark_style = gtk_style_copy (gtk_widget_get_style (day_list));
+  time_style = gtk_style_copy (gtk_widget_get_style (day_list));
+
+  if (strings)
+    {
+      for (iter = strings; iter; iter = iter->next)
+	g_free (iter->data);
+      g_slist_free (strings);
+      strings = NULL;
+    }
   
-  for(j = 0; j < 5; j++) {
+  for (j = 0; j < 5; j++) 
+    {
       light_style->base[j] = light_color;
       dark_style->base[j] = dark_color;
       time_style->base[j] = time_color;
       time_style->fg[j] = light_color;
-  }
-
-  gtk_clist_freeze(GTK_CLIST(day_list));
-  gtk_clist_clear(GTK_CLIST(day_list));
-
-  gtk_clist_set_column_width (GTK_CLIST(day_list), 0, 50);
-  gtk_clist_set_column_width (GTK_CLIST(day_list), 1, 55);
+    }
   
+  gtk_clist_freeze (GTK_CLIST (day_list));
+  gtk_clist_clear (GTK_CLIST (day_list));
+
   for (hour = 0; hour <= 23; hour++)
     {
-      GSList *iter;
-      int found=0;
-      
       localtime_r (&viewtime, &tm_start);
+
       tm_start.tm_hour = hour;
       tm_start.tm_min = 0;
       tm_start.tm_sec = 0;
-      start=mktime (&tm_start);
+      start = mktime (&tm_start);
       localtime_r (&viewtime, &tm_end);
       tm_end.tm_hour = hour+1;
       tm_end.tm_min = 0;
       tm_end.tm_sec = 0;
-      end=mktime (&tm_end);
-      
+      end = mktime (&tm_end);
+
       day_events[hour] = event_db_list_for_period (start, end-1);
       
       for (iter = day_events[hour]; iter; iter = iter->next)
-	{
-	  found=1;
-	  ev = (event_t) iter->data;
-	  evd = event_db_get_details (ev);
-
-          line_info[1] = evd->summary;
-          strftime (buf, sizeof (buf), "%l:%M%p", &tm_start);
-          line_info[0] = buf;
-      
-          gtk_clist_append(GTK_CLIST(day_list), line_info);
-	  gtk_clist_set_row_data(GTK_CLIST(day_list), row, ev);
-	
-	  gtk_clist_set_cell_style(GTK_CLIST(day_list), row, 1, time_style);
-	  row++;
-	  
-       } 
-       
-       if (!found) {
-       
-	  line_info[1]=NULL;
-          strftime (buf, sizeof (buf), "%l:%M%p", &tm_start);
-          line_info[0]=buf;
-      
-          gtk_clist_append(GTK_CLIST(day_list), line_info);
-		
-	  if (hour & 1)
-             gtk_clist_set_cell_style(GTK_CLIST(day_list), row, 1, light_style); 
-          else 
-	     gtk_clist_set_cell_style(GTK_CLIST(day_list), row, 1, dark_style);
-          row++;
-	  
-       }
-       
-       gtk_clist_set_cell_style(GTK_CLIST(day_list), hour, 0, time_style);
-
+	((event_t)iter->data)->mark = FALSE;
     }
-  
-  gtk_clist_moveto(GTK_CLIST(day_list), bias, 0, 0.0, 0.0);
 
-  gtk_clist_thaw(GTK_CLIST(day_list));
+  for (hour = 0; hour <= 23; hour++)
+    {
+      guint w;
+      gchar *text = NULL;
+      event_t ev = NULL;
+ 
+      tm_start.tm_hour = hour;
+      tm_start.tm_min = 0;
+      tm_start.tm_sec = 0;
+
+      for (iter = day_events[hour]; iter; iter = iter->next)
+	{
+	  ev = (event_t) iter->data;
+
+	  if (ev->mark == FALSE)
+	    {
+	      ev->mark = TRUE;
+	      text = format_event (ev);
+	      g_slist_append (strings, text);
+	      break;
+	    }
+	} 
+
+      line_info[1] = text;
+      strftime (buf, sizeof (buf), "%R", &tm_start);
+      line_info[0] = buf;
+      
+      w = gdk_string_width (time_style->font, buf);
+      if (w > width)
+	width = w;
+
+      gtk_clist_append (GTK_CLIST (day_list), line_info);
+
+      if (ev)
+	gtk_clist_set_row_data (GTK_CLIST (day_list), row, ev);
+      
+      gtk_clist_set_cell_style (GTK_CLIST (day_list), row, 1, 
+				day_events[hour] ? dark_style : light_style); 
+      row++;
+
+      for (; iter; iter = iter->next)
+	{
+	  ev = (event_t) iter->data;
+
+	  if (ev->mark)
+	    continue;
+
+	  ev->mark = TRUE;
+
+	  line_info[0] = NULL;
+	  line_info[1] = format_event (ev);
+	  g_slist_append (strings, line_info[1]);
+
+          gtk_clist_append (GTK_CLIST (day_list), line_info);
+	  gtk_clist_set_row_data (GTK_CLIST (day_list), row, ev);
+	
+	  gtk_clist_set_cell_style (GTK_CLIST (day_list), row, 1, dark_style);
+	  row++;
+       } 
+    }
+
+  for (i = 0; i < row; i++)
+    gtk_clist_set_cell_style (GTK_CLIST (day_list), i, 0, time_style);
+
+  gtk_clist_set_column_width (GTK_CLIST (day_list), 0, width + 4);
+
+  gtk_clist_moveto (GTK_CLIST (day_list), bias, 0, 0.0, 0.0);
+
+  gtk_clist_thaw (GTK_CLIST (day_list));
   
   return TRUE;
 }
@@ -189,10 +258,10 @@ day_view(void)
   gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window),
 		GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
   
-  day_view_update();
-  gtk_container_add(GTK_CONTAINER(scrolled_window), day_list);
+  day_view_update ();
+  gtk_container_add (GTK_CONTAINER (scrolled_window), day_list);
   
-  gtk_box_pack_start (GTK_BOX(vbox), scrolled_window, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (vbox), scrolled_window, TRUE, TRUE, 0);
   gtk_box_pack_start (GTK_BOX (vbox), datesel, FALSE, FALSE, 0);
   
   gtk_signal_connect(GTK_OBJECT (datesel), "changed",
