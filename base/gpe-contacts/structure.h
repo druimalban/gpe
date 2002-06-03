70205b5c3f6d38ce36acdf643dd8ddf1eeeb9b75
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-06-03 16:05:21 +0000

    new file
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@312 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/gpe-contacts/structure.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2002 Philip Blundell <philb@gnu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
+#ifndef STRUCTURE_H
+#define STRUCTURE_H
+
+#include <stdio.h>
+#include <glib.h>
+#include <gtk/gtk.h>
+
+typedef enum
+{
+  PAGE,
+  GROUP,
+  ITEM_MULTI_LINE,
+  ITEM_SINGLE_LINE
+} edit_thing_type;
+
+struct edit_thing
+{
+  edit_thing_type type;
+  gchar *name;
+  guint tag;
+  GSList *children;
+  struct edit_thing *parent;
+};
+
+typedef struct edit_thing *edit_thing_t;
+
+extern void edit_structure (void);
+extern GtkWidget *edit_window (void);
+
+extern void print_structure (FILE *);
+extern gboolean read_structure (gchar *);
+extern void initial_structure (void);
+
+#endif
+
