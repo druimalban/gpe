07c2c302a1d3f586183ff5516c0dee872811f3a0
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-06-13 21:00:01 +0000

    new stuff
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@401 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-timesheet/main.c
+++ base/gpe-timesheet/main.c
@@ -33,6 +33,44 @@ struct pix my_pix[] = {
   { NULL, NULL }
 };
 
+static void
+start_timing(GtkWidget *w, gpointer user_data)
+{
+  GtkCTree *ct = GTK_CTREE (user_data);
+  if (GTK_CLIST (ct)->selection)
+    {
+      GtkCTreeNode *node = GTK_CTREE_NODE (GTK_CLIST (ct)->selection->data);
+      struct pix *px = find_pixmap ("tick");
+      gtk_ctree_node_set_pixmap (ct, node, 1, px->pixmap, px->mask);
+    }
+}
+
+static void
+stop_timing(GtkWidget *w, gpointer user_data)
+{
+  GtkCTree *ct = GTK_CTREE (user_data);
+  if (GTK_CLIST (ct)->selection)
+    {
+      GtkCTreeNode *node = GTK_CTREE_NODE (GTK_CLIST (ct)->selection->data);
+      gtk_ctree_node_set_text (ct, node, 1, "");
+    }
+}
+
+static void
+new_task(GtkWidget *w, gpointer p)
+{
+  GtkCTree *ctree = GTK_CTREE (p);
+  GtkCTreeNode *node;
+  gchar *text[2];
+  
+  text[0] = "New task";
+  text[1] = "";
+
+  node = gtk_ctree_insert_node (ctree, NULL, NULL,
+				text, 0, NULL, NULL, NULL, NULL,
+				TRUE, TRUE);
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -41,6 +79,7 @@ main(int argc, char *argv[])
   GtkWidget *toolbar;
   GtkWidget *pw;
   GtkWidget *tree;
+  GtkWidget *chatter;
   struct pix *p;
 
   if (gpe_application_init (&argc, &argv) == FALSE)
@@ -54,16 +93,19 @@ main(int argc, char *argv[])
   gtk_toolbar_set_button_relief (GTK_TOOLBAR (toolbar), GTK_RELIEF_NONE);
 
   tree = gtk_ctree_new (2, 0);
+  chatter = gtk_label_new ("");
+  gtk_misc_set_alignment (GTK_MISC (chatter), 0.0, 0.5);
 
   vbox_top = gtk_vbox_new (FALSE, 0);
   gtk_box_pack_start (GTK_BOX (vbox_top), toolbar, FALSE, FALSE, 0);
   gtk_box_pack_start (GTK_BOX (vbox_top), tree, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (vbox_top), chatter, FALSE, FALSE, 0);
 
   p = find_pixmap ("new");
   pw = gtk_pixmap_new (p->pixmap, p->mask);
   gtk_toolbar_append_item (GTK_TOOLBAR (toolbar), _("New task"), 
 			   _("New task"), _("New task"),
-			   pw, NULL, NULL);
+			   pw, new_task, tree);
 
   p = find_pixmap ("delete");
   pw = gtk_pixmap_new (p->pixmap, p->mask);
@@ -75,24 +117,30 @@ main(int argc, char *argv[])
   pw = gtk_pixmap_new (p->pixmap, p->mask);
   gtk_toolbar_append_item (GTK_TOOLBAR (toolbar), _("Start timing"),
 			   _("Start timing"), _("Start timing"),
-			   pw, NULL, NULL);
+			   pw, start_timing, tree);
   
   p = find_pixmap ("stop");
   pw = gtk_pixmap_new (p->pixmap, p->mask);
   gtk_toolbar_append_item (GTK_TOOLBAR (toolbar), _("Stop timing"),
 			   _("Stop timing"), _("Stop timing"),
-			   pw, NULL, NULL);
+			   pw, stop_timing, tree);
 
 
   gtk_widget_show (toolbar);
   gtk_widget_show (vbox_top);
+  gtk_widget_show (tree);
+  gtk_widget_show (chatter);
 
   window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
   gtk_container_add (GTK_CONTAINER (window), vbox_top);
+  gtk_widget_set_usize (window, window_x, window_y);
+
+  gtk_widget_realize (window);
+
+  gtk_clist_set_column_width (GTK_CLIST (tree), 0, 
+			      tree->allocation.width - 32);
 
   gtk_widget_show (window);
-
-  gtk_widget_set_usize (window, window_x, window_y);
 
   gtk_main ();
 
