c5973f85c68548ecc9cc0d5a91a0f48c4b9caec2
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-06-20 22:47:30 +0000

    new file
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@610 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/libgpewidget/what.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2002 Philip Blundell <philb@gnu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
+#ifndef WHAT_H
+#define WHAT_H
+
+extern void what_init (void);
+
+#endif
