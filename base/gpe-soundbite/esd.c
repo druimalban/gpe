fc33b7e3e6bb8c6724726622ee93c819dc8dea28
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-07-17 19:41:10 +0000

    pass hostname to esd as NULL
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@830 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-soundbite/esd.c
+++ base/gpe-soundbite/esd.c
@@ -11,8 +11,6 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
-static char *host = "192.168.2.2";
-
 int 
 sound_device_open (int mode)
 {
@@ -21,12 +19,12 @@ sound_device_open (int mode)
     if (mode == O_RDONLY)
       return esd_record_stream_fallback (ESD_BITS16 | ESD_STEREO | ESD_STREAM | ESD_RECORD,
 					 rate,
-					 host, NULL);
+					 NULL, NULL);
 
     if (mode == O_WRONLY)
       return esd_play_stream_fallback (ESD_BITS16 | ESD_STEREO | ESD_STREAM | ESD_PLAY,
 					 rate,
-					 host, NULL);
+					 NULL, NULL);
 
     return -1;
 }
