5b9fecfab90e1c1223a6eac5c7dc0c80ce474733
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-07-22 21:16:01 +0000

    add esddsp packaging
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@927 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/esound/dist.sh
+++ base/esound/dist.sh
@@ -11,9 +11,20 @@ ipkg-build dist.esd
 
 mkdir -p dist.libesd0/CONTROL
 mkdir -p dist.libesd0/usr/lib
-install .libs/libesd.so.0.2.28 dist.esd/usr/lib
+install .libs/libesd.so.0.2.28 dist.libesd0/usr/lib
 cp familiar/control.libesd0 dist.libesd0/CONTROL/control
-cp familiar/postinst.libesd0 dist.libesd0/CONTROL
+cp familiar/postinst.libesd0 dist.libesd0/CONTROL/postinst
 chown -R root.root dist.libesd0
 ipkg-build dist.libesd0
+
+mkdir -p dist.esddsp/CONTROL
+mkdir -p dist.esddsp/usr/lib
+mkdir -p dist.esddsp/usr/bin
+install .libs/libesddsp.so.0.2.28 dist.esddsp/usr/lib
+install esddsp dist.esddsp/usr/bin
+cp familiar/control.esddsp dist.esddsp/CONTROL/control
+cp familiar/postinst.esddsp dist.esddsp/CONTROL/postinst
+chown -R root.root esddsp
+ipkg-build dist.esddsp
+
 
