2482bd706d04cd5ec5c0135b304d7d5407daa8e4
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-08-15 21:29:04 +0000

    New file
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@1292 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/gpe-nmf/frontend.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2002 Philip Blundell <philb@gnu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
+#include "playlist_db.h"
+#include "player.h"
+
+struct nmf_frontend
+{
+  player_t player;
+  struct playlist *playlist;
+  GtkWidget *playlist_widget;
+  gboolean playing;
+  GtkAdjustment *progress_adjustment;
+  GtkWidget *progress_slider;
+  GtkWidget *time_label, *artist_label, *title_label;
+  GtkTreeModel *model;
+};
+
+void update_track_info (struct nmf_frontend *fe, struct playlist *p);
+void playlist_edit_push (GtkWidget *w, struct playlist *p);
+GtkWidget *playlist_edit (struct nmf_frontend *fe, struct playlist *p);
