d8bc1a3a42e7ba760f7f794cd56eea30f0be9d38
cmarqu <cmarqu@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-08-28 21:39:13 +0000

    New file.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@1444 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/gpe-ownerinfo1/src/main.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2002 Colin Marquardt <ipaq@marquardt-home.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
+
+gint
+maybe_upgrade_datafile (FILE *fp);
+
+guint
+upgrade_to_v2 (FILE *fp, guint version);
