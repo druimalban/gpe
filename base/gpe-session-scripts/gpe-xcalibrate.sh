aa115d7a43eaeab2347d471356881c136d22eb7c
cmarqu <cmarqu@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-09-11 18:11:14 +0000

    * gpe-xcalibrate.sh: Add comment on the fixed xcalibrate, but don't evaluate the return code yet (not until ipkg supports versioned dependencies).
    * familiar/control.gpe-session-scripts (Depends): Versioned
    dependency on xcalibrate (>= 0.2-2).
    
    
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@1592 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-session-scripts/gpe-xcalibrate.sh
+++ base/gpe-session-scripts/gpe-xcalibrate.sh
@@ -3,4 +3,8 @@ if test -x /usr/X11R6/bin/xcalibrate; then
     # don't look at xcalibrate's exit code, it always returns 1:
     /usr/X11R6/bin/xcalibrate;
     /usr/X11R6/bin/xcalibrate -view >/etc/xcalibrate.conf;
+
+    ## Beginning with xcalibrate 0.2-2, we could use the line below,
+    ## but it's maybe too dangerous until ipkg supports versioned dependencies:    
+    # /usr/X11R6/bin/xcalibrate && /usr/X11R6/bin/xcalibrate -view >/etc/xcalibrate.conf;
 fi
