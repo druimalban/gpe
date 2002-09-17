8e11c04dbe6ca5d04276323f3a6e430acf9b1d89
cmarqu <cmarqu@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-09-17 20:31:21 +0000

    (page_add): Use the correct spacing and packing according to http://developer.gnome.org/projects/gup/hig/1.0/layout.html#layout-window (Appmgr_Build_Objects): Ditto.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@1628 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-conf/appmgr_setup.c
+++ base/gpe-conf/appmgr_setup.c
@@ -29,7 +29,8 @@
 #include "applets.h"
 
 #include "appmgr_setup.h"
-#include "gpe/errorbox.h"
+#include <gpe/errorbox.h>
+#include <gpe/spacing.h>
 
 struct package *config=NULL;
 
@@ -55,59 +56,107 @@ int dont_launch_exists()
 
 void page_add (GtkVBox *vb)
 {
-	GtkWidget *frame, *lvbox;
+	GtkWidget *catvbox1;
+	GtkWidget *catlabel1;
+	GtkWidget *catconthbox1;
+	GtkWidget *catindentlabel1;
+	GtkWidget *controlvbox1;
+	GtkWidget *catvbox2;
+	GtkWidget *catlabel2;
+	GtkWidget *catconthbox2;
+	GtkWidget *catindentlabel2;
+	GtkWidget *controlvbox2;
+	GSList *controlvbox2_group = NULL;
+	
 	GtkWidget *label;
+
+	gchar *gpe_catindent = gpe_get_catindent ();
+	guint gpe_boxspacing = gpe_get_boxspacing ();
 
 	cfg_load();
 
+	/* -------------------------------------------------------------------------- */
+	catvbox1 = gtk_vbox_new (FALSE, gpe_boxspacing);
+	gtk_box_pack_start (GTK_BOX (vb), catvbox1, TRUE, TRUE, 0);
+	
+	catlabel1 = gtk_label_new (_("General")); /* FIXME: GTK2: make this bold */
+	gtk_box_pack_start (GTK_BOX (catvbox1), catlabel1, FALSE, FALSE, 0);
+	gtk_label_set_justify (GTK_LABEL (catlabel1), GTK_JUSTIFY_LEFT);
+	gtk_misc_set_alignment (GTK_MISC (catlabel1), 0, 0.5);
+	
+	catconthbox1 = gtk_hbox_new (FALSE, 0);
+	gtk_box_pack_start (GTK_BOX (catvbox1), catconthbox1, TRUE, TRUE, 0);
+	
+	catindentlabel1 = gtk_label_new (gpe_catindent);
+	gtk_box_pack_start (GTK_BOX (catconthbox1), catindentlabel1, FALSE, FALSE, 0);
+	
+	controlvbox1 = gtk_vbox_new (FALSE, gpe_boxspacing);
+	gtk_box_pack_start (GTK_BOX (catconthbox1), controlvbox1, TRUE, TRUE, 0);
+	
+	
 	/* Show the 'All' group */
 	all_group_check =
 		gtk_check_button_new_with_label ("Show 'All' group");
 	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(all_group_check),
 				      cfg_options.show_all_group);
 
-	gtk_box_pack_start_defaults (GTK_BOX(vb), all_group_check);
+	gtk_box_pack_start (GTK_BOX(controlvbox1), all_group_check, FALSE, FALSE, 0);
 
 	/* Autohide group titles */
 	auto_hide_group_labels_check =
 		gtk_check_button_new_with_label ("Auto-hide group labels");
 	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(auto_hide_group_labels_check),
 				      cfg_options.auto_hide_group_labels);
-	gtk_box_pack_start_defaults (GTK_BOX(vb), auto_hide_group_labels_check);
+	gtk_box_pack_start (GTK_BOX(controlvbox1), auto_hide_group_labels_check, FALSE, FALSE, 0);
 
 	/* Show 'recent' tab */
 	show_recent_check =
 		gtk_check_button_new_with_label ("Show 'Recent' tab");
 	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(show_recent_check),
 				      cfg_options.show_recent_apps);
-	gtk_box_pack_start_defaults (GTK_BOX(vb), show_recent_check);
+	gtk_box_pack_start (GTK_BOX(controlvbox1), show_recent_check, FALSE, FALSE, 0);
 
 	/* Don't launch at startup */
 	dont_launch_check =
 		gtk_check_button_new_with_label ("Do not launch at startup");
 	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON( dont_launch_check),
 				      dont_launch_exists());
-	gtk_box_pack_start_defaults (GTK_BOX(vb), dont_launch_check);
+	gtk_box_pack_start (GTK_BOX(controlvbox1), dont_launch_check, FALSE, FALSE, 0);
 
-	/* Tab view type */
-	frame = gtk_frame_new ("Tab view");
-	gtk_box_pack_start_defaults (GTK_BOX(vb), frame);
-	lvbox = gtk_vbox_new (0,0);
-	gtk_container_add (GTK_CONTAINER(frame), lvbox);
+	/* -------------------------------------------------------------------------- */
+	catvbox2 = gtk_vbox_new (FALSE, gpe_boxspacing);
+	gtk_box_pack_start (GTK_BOX (vb), catvbox2, TRUE, TRUE, 0);
+	
+	catlabel2 = gtk_label_new (_("Tab View")); /* FIXME: GTK2: make this bold */
+	gtk_box_pack_start (GTK_BOX (catvbox2), catlabel2, FALSE, FALSE, 0);
+	gtk_label_set_justify (GTK_LABEL (catlabel2), GTK_JUSTIFY_LEFT);
+	gtk_misc_set_alignment (GTK_MISC (catlabel2), 0, 0.5);
+	
+	catconthbox2 = gtk_hbox_new (FALSE, 0);
+	gtk_box_pack_start (GTK_BOX (catvbox2), catconthbox2, TRUE, TRUE, 0);
+	
+	catindentlabel2 = gtk_label_new (gpe_catindent);
+	gtk_box_pack_start (GTK_BOX (catconthbox2), catindentlabel2, FALSE, FALSE, 0);
+	
+	controlvbox2 = gtk_vbox_new (FALSE, gpe_boxspacing);
+	gtk_box_pack_start (GTK_BOX (catconthbox2), controlvbox2, TRUE, TRUE, 0);
 
 
-	/* ->Icon */
-	tab_view_icon = gtk_radio_button_new_with_label (NULL, "Icon");
-	gtk_box_pack_start_defaults (GTK_BOX(lvbox), tab_view_icon);
+	tab_view_icon = gtk_radio_button_new_with_label (controlvbox2_group, _("Icon"));
+	controlvbox2_group = gtk_radio_button_group (GTK_RADIO_BUTTON (tab_view_icon));
+	gtk_box_pack_start (GTK_BOX (controlvbox2), tab_view_icon, FALSE, FALSE, 0);
+	
+	tab_view_list = gtk_radio_button_new_with_label_from_widget (GTK_RADIO_BUTTON(tab_view_icon), _("List"));
+	controlvbox2_group = gtk_radio_button_group (GTK_RADIO_BUTTON (tab_view_list));
+	gtk_box_pack_start (GTK_BOX (controlvbox2), tab_view_list, FALSE, FALSE, 0);
 
-	/* ->List */
-	tab_view_list = gtk_radio_button_new_with_label_from_widget (GTK_RADIO_BUTTON(tab_view_icon), "List");
-	gtk_box_pack_start_defaults (GTK_BOX(lvbox), tab_view_list);
 	gtk_toggle_button_set_active (GTK_TOGGLE_BUTTON(tab_view_list),
 				      cfg_options.tab_view == TAB_VIEW_LIST);
-
+	
+	/* -------------------------------------------------------------------------- */
 	/*  Label */
-	label = gtk_label_new("Changes will take effect\nafter relog");
+	label = gtk_label_new(_("Changes will take effect after relog"));
+	gtk_label_set_line_wrap (GTK_LABEL (label), TRUE);
 	gtk_box_pack_start_defaults (GTK_BOX(vb), label);
 
 	gtk_widget_show_all (GTK_WIDGET(vb));
@@ -162,7 +211,11 @@ GtkWidget *Appmgr_Build_Objects()
 {
 	GtkWidget  *vbox;
 
-	vbox = gtk_vbox_new (0,0);
+        guint gpe_catspacing = gpe_get_catspacing ();
+	guint gpe_border = gpe_get_border ();
+  
+	vbox = gtk_vbox_new (FALSE, gpe_catspacing);
+	gtk_container_set_border_width (GTK_CONTAINER (vbox), gpe_border);
 
 	page_add (GTK_VBOX(vbox));
 
