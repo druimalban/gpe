cc48143454534301d4ea08a9b0b6a8830b1df011
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-09-21 16:08:11 +0000

    call gpe_application_init; replace GDK_ROOT_WINDOW with GDK_ROOT_PARENT
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@1642 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-setbg/gpe-setbg.c
+++ base/gpe-setbg/gpe-setbg.c
@@ -2,6 +2,7 @@
 #include <gdk/gdk.h>
 #include <gdk/gdkx.h>
 #include <gpe/pixmaps.h>
+#include <gpe/init.h>
 
 static struct gpe_icon my_icons[] = {
   { "default_bg", PREFIX "/share/pixmaps/gpe-login-bg.png" },
@@ -14,12 +15,12 @@ main (int argc, char *argv[])
   GdkPixmap *pixmap;
   GdkBitmap *bitmap;
   
-  gdk_init (&argc, &argv);
+  gpe_application_init (&argc, &argv);
 
   if (gpe_load_icons (my_icons) == FALSE) exit (1);
   if (gpe_find_icon_pixmap ("default_bg", &pixmap, &bitmap)) {
     gdk_window_set_back_pixmap (
-				(GdkWindow *) GDK_ROOT_WINDOW (),
+				GDK_ROOT_PARENT (),
 				//(GdkWindow *) RootWindow (GDK_DISPLAY (), DefaultScreen (GDK_DISPLAY ())),
 				pixmap,
 				0);
