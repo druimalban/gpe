0aa64cd0d20550b3cb04f6769eda7bd089a691f4
cmarqu <cmarqu@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-09-22 02:31:19 +0000

    * Makefile: Install gpe-default-bg.png
    * gpe-setbg.c: Add copyright info.
    (my_icons): Try to load gpe-default-bg.png now.
    (main): Accept file from the command line. Be verbose.
    
    
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@1649 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-setbg/gpe-setbg.c
+++ base/gpe-setbg/gpe-setbg.c
@@ -1,11 +1,26 @@
+/*
+ * Copyright (C) 2002 Colin Marquardt <ipaq@marquardt-home.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
 #include <stdio.h>
+#include <unistd.h> /* for access() */
+
+#include <glib.h>
+
 #include <gdk/gdk.h>
 #include <gdk/gdkx.h>
+
 #include <gpe/pixmaps.h>
 #include <gpe/init.h>
 
+
 static struct gpe_icon my_icons[] = {
-  { "default_bg", PREFIX "/share/pixmaps/gpe-login-bg.png" },
+  { "login_bg", PREFIX "/share/pixmaps/gpe-default-bg.png" },
   { NULL, NULL }
 };
 
@@ -17,15 +32,27 @@ main (int argc, char *argv[])
   
   gpe_application_init (&argc, &argv);
 
-  if (gpe_load_icons (my_icons) == FALSE) exit (1);
-  if (gpe_find_icon_pixmap ("default_bg", &pixmap, &bitmap)) {
-    gdk_window_set_back_pixmap (
-				GDK_ROOT_PARENT (),
-				//(GdkWindow *) RootWindow (GDK_DISPLAY (), DefaultScreen (GDK_DISPLAY ())),
-				pixmap,
-				0);
+  if (argc > 2) {
+    g_error ("gpe-setbg: Expecting at most 1 argument.");
+    exit (1);
+  }
+  if (argc == 2) {
+    if (access (argv[1], R_OK) == 0) /* set file if given on the command line */
+      my_icons[0].filename = g_strdup (argv[1]);
+    else {
+      g_print ("gpe-setbg: ERROR: Could not read image file\n'%s'\n", argv[1]);
+      exit (1);
+    }
+  }
+
+#warning FIXME: fail silently here (at least not with a dialog box, ugly at login)
+  if (gpe_load_icons (my_icons) == FALSE)
+    exit (1);
+  
+  if (gpe_find_icon_pixmap ("login_bg", &pixmap, &bitmap)) {
+    g_print ("gpe-setbg: Setting background.\n");
+    gdk_window_set_back_pixmap (GDK_ROOT_PARENT (), pixmap, 0);
   }
 
   return (0);
-
 }
