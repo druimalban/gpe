bf693bc75b1bafe9d3d05ccd3cd9b45027c1d5d5
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-10-13 14:22:54 +0000

    initial checkin
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@1785 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/xst/xst.c
@@ -0,0 +1,265 @@
+/*
+ * Copyright (C) 2002 Philip Blundell <philb@gnu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <X11/Xmd.h>
+
+#include <glib.h>
+
+#include "xsettings-common.h"
+#include "xsettings-client.h"
+
+Display *dpy;
+XSettingsClient *client;
+
+struct _XSettingsClient
+{
+  Display *display;
+  int screen;
+  XSettingsNotifyFunc notify;
+  XSettingsWatchFunc watch;
+  void *cb_data;
+
+  Window manager_window;
+  Atom manager_atom;
+  Atom selection_atom;
+  Atom xsettings_atom;
+
+  XSettingsList *settings;
+};
+
+Window
+xsettings_client_manager_window (XSettingsClient *client)
+{
+  return ((struct _XSettingsClient *)client)->manager_window;
+}
+
+static void
+print_xsetting (XSettingsSetting *setting)
+{
+  printf ("%s ", setting->name);
+
+  switch (setting->type)
+    {
+    case XSETTINGS_TYPE_INT:
+      printf ("(integer) %d", setting->data.v_int);
+      break;
+
+    case XSETTINGS_TYPE_COLOR:
+      printf ("(colour) %04x %04x %04x %04x",
+	      setting->data.v_color.red,
+	      setting->data.v_color.green,
+	      setting->data.v_color.blue,
+	      setting->data.v_color.alpha);
+      break;
+
+    case XSETTINGS_TYPE_STRING:
+      printf ("(string) %s", setting->data.v_string);
+      break;
+    }
+
+  printf ("\n");
+}
+
+
+gboolean
+xsetting_unstringize (char *s, XSettingsSetting *setting)
+{
+  int red, green, blue, alpha;
+
+  if (s[0] == 0 || s[1] != ':')
+    return FALSE;
+
+  switch (s[0])
+    {
+    case 'C':
+      setting->type = XSETTINGS_TYPE_COLOR;
+      sscanf (s + 2, "%04x %04x %04x %04x",
+	      &red,
+	      &green,
+	      &blue,
+	      &alpha);
+      setting->data.v_color.red = red;
+      setting->data.v_color.green = green;
+      setting->data.v_color.blue = blue;
+      setting->data.v_color.alpha = alpha;
+      break;
+
+    case 'I':
+      setting->type = XSETTINGS_TYPE_INT;
+      setting->data.v_int = atoi (s + 2);
+      break;
+
+    case 'S':
+      setting->type = XSETTINGS_TYPE_STRING;
+      setting->data.v_string = g_strdup (s + 2);
+      break;
+
+    default:
+      return FALSE;
+    }
+
+  return TRUE;
+}
+
+static void
+notify_func (const char       *name,
+	     XSettingsAction   action,
+	     XSettingsSetting *setting,
+	     void             *cb_data)
+{
+  if (action == XSETTINGS_ACTION_NEW)
+    print_xsetting (setting);
+}
+
+static void
+usage (void)
+{
+  fprintf (stderr, "Usage: xst read  [property]\n");
+  fprintf (stderr, "       xst write <property> <format> <value>\n");
+}
+
+int
+main (int argc, char *argv[])
+{
+  char *disp = NULL;
+  gboolean flag_read = FALSE, flag_write = FALSE;
+  gchar *prop = NULL, *format = NULL, *value = NULL;
+  XSettingsNotifyFunc notify_fp = NULL;
+
+  if ((dpy = XOpenDisplay (disp)) == NULL)
+    {
+      fprintf (stderr, "Cannot connect to X server on display %s.\n",
+	      XDisplayName (disp));
+      exit (1);
+    }
+
+  if (argc < 2)
+    {
+      usage ();
+      exit (1);
+    }
+
+  if (!strcmp (argv[1], "read"))
+    {
+      flag_read = TRUE;
+      if (argc == 3)
+	prop = argv[2];
+    }
+  else if (!strcmp (argv[1], "write"))
+    {
+      flag_write = TRUE;
+      if (argc < 5)
+	{
+	  usage ();
+	  exit (1);
+	}
+      prop = argv[2];
+      format = argv[3];
+      value = argv[4];
+    }
+  else
+    {
+      usage ();
+      exit (1);
+    }
+
+  if (flag_read && (prop == NULL))
+    notify_fp = notify_func;
+
+  client = xsettings_client_new (dpy, DefaultScreen (dpy), notify_fp, NULL, NULL);
+  if (client == NULL)
+    {
+      fprintf (stderr, "Cannot create XSettings client\n");
+      exit (1);
+    }
+
+  if (prop)
+    {
+      XSettingsSetting *setting;
+      
+      if (flag_read)
+	{
+	  xsettings_client_get_setting (client, prop, &setting);
+	  print_xsetting (setting);
+	}
+      
+      if (flag_write)
+	{
+	  Atom gpe_settings_update_atom = XInternAtom (dpy, "GPE_SETTINGS_UPDATE", 0);
+	  Window win = xsettings_client_manager_window (client);
+	  XSettingsType type;
+	  size_t length, name_len;
+	  gchar *buffer;
+	  XClientMessageEvent ev;
+
+	  if (!strcmp (format, "int"))
+	    {
+	      type = XSETTINGS_TYPE_INT;
+	      length = 4;
+	    }
+	  else if (!strcmp (format, "col"))
+	    {
+	      type = XSETTINGS_TYPE_COLOR;
+	      length = 8;
+	    }
+	  else if (!strcmp (format, "str"))
+	    {
+	      type = XSETTINGS_TYPE_STRING;
+	      length = 4 + (strlen (value) + 3) & ~3;
+	    }
+	  else
+	    {
+	      fprintf (stderr, "Format \"%s\" not recognised (try int/col/str)\n", format);
+	      exit (1);
+	    }
+
+	  name_len = strlen (prop);
+	  name_len = (name_len + 3) & ~3;
+	  buffer = g_malloc (length + 4 + name_len);
+	  *buffer = type;
+	  buffer[1] = 0;
+	  buffer[2] = name_len & 0xff;
+	  buffer[3] = (name_len >> 8) & 0xff;
+	  memcpy (buffer + 4, prop, name_len);
+
+	  switch (type)
+	    {
+	    case XSETTINGS_TYPE_INT:
+	      *((unsigned long *)(buffer + 4 + name_len)) = atoi (value);
+	      break;
+	    case XSETTINGS_TYPE_STRING:
+	      *((unsigned long *)(buffer + 4 + name_len)) = strlen (value);
+	      memcpy (buffer + 8 + name_len, value, strlen (value));
+	      break;
+	    case XSETTINGS_TYPE_COLOR:
+	      break;
+	    }
+
+	  XChangeProperty (dpy, win, gpe_settings_update_atom, gpe_settings_update_atom,
+			   8, PropModeReplace, buffer, length + 4 + name_len);
+
+	  ev.type = ClientMessage;
+	  ev.window = win;
+	  ev.message_type = gpe_settings_update_atom;
+	  ev.format = 32;
+	  ev.data.l[0] = gpe_settings_update_atom;
+	  XSendEvent (dpy, win, FALSE, 0, (XEvent *)&ev);
+	}
+    }
+
+  xsettings_client_destroy (client);
+  XCloseDisplay (dpy);
+  
+  exit (0);
+}
