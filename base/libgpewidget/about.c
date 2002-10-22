675f87208c64b157684cf791dc0a5c3489b9f5f6
pionchon <pionchon@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-10-22 20:54:58 +0000

    moved to LGPL
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@1849 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/libgpewidget/about.c
+++ base/libgpewidget/about.c
@@ -1,11 +1,11 @@
-/*
+/* GPE - libgpewidget
  * Copyright (C) 2002 Damien Tanner <dctanner@magenet.com>
  * Copyright (C) 2002 luc pionchon
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version
- * 2 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  */
 
 #include <gtk/gtk.h>
