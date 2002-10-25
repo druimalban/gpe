8c40f5f4e558cc02cae5bbc526892472a784d767
mibus <mibus@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-10-25 05:53:35 +0000

    Point out copyright (Robert Mibus) and licence (LGPL).
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@1852 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/libredalert/redalert.h
+++ base/libredalert/redalert.h
@@ -1,3 +1,22 @@
+/*
+  libredalert - a middle-layer between atd and apps
+  Copyright (C) 2002  Robert Mibus <mibus@handhelds.org>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
 /* 'redalert' is a temporary name.
    (unless no-one can come up with a better one.
    other possibilities discussed on IRC:
