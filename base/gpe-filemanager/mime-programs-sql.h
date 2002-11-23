c1aec1621c28f590f5ffbb5137c7c69e3fa0143a
dctanner <dctanner@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2002-11-23 13:46:52 +0000

    Mime programs database.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@1960 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/gpe-filemanager/mime-programs-sql.h
@@ -0,0 +1,17 @@
+extern GSList *mime_programs;
+
+struct mime_program
+{
+  int id;
+  const char *name;
+  const char *mime;
+  const char *command;
+};
+
+extern int sql_start (void);
+
+extern struct mime_program *new_mime_program (const char *name, const char *mime, const char *command);
+
+extern void del_mime_program (struct mime_program *e);
+
+extern void sql_close (void);
