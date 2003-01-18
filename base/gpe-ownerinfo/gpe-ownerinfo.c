04f350d2b8a41e6bca64f1e72f71b7061315c684
cmarqu <cmarqu@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-01-18 20:25:36 +0000

    (main): Add notebook and use the second page for the bigger photo.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@2611 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-ownerinfo/gpe-ownerinfo.c
+++ base/gpe-ownerinfo/gpe-ownerinfo.c
@@ -66,13 +66,13 @@
 #define UPGRADE_NOT_NEEDED  0
 
 GtkWidget *GPE_Ownerinfo;
-GdkPixmap *pixmap = NULL;
 GtkWidget *name;
 GtkWidget *phone;
 GtkWidget *email;
 GtkWidget *address;
-GdkPixbuf *pixbuf;
+GdkPixbuf *photopixbuf;
 GtkWidget *smallphotobutton;
+GtkWidget *bigphotobutton;
 gchar *photofile = PREFIX "/share/gpe/pixmaps/default/tux-48.png";
 
 
@@ -91,8 +91,8 @@ on_expose_event (GtkWidget *widget, GdkEventExpose *event, gpointer data)
   maxheight = widget->allocation.height;
   g_message ("allocation for drawing area: %d x %d", maxwidth, maxheight);
 
-  width  = gdk_pixbuf_get_width (pixbuf);
-  height = gdk_pixbuf_get_height (pixbuf);
+  width  = gdk_pixbuf_get_width (photopixbuf);
+  height = gdk_pixbuf_get_height (photopixbuf);
   g_message ("pixbuf: %d x %d", width, height);
 
   if (width > maxwidth)
@@ -113,11 +113,11 @@ on_expose_event (GtkWidget *widget, GdkEventExpose *event, gpointer data)
   resultheight = (gint) (height * scale);
   g_message ("resulting size: %d x %d", resultwidth, resultheight);
 
-  scaledpixbuf = gdk_pixbuf_scale_simple (pixbuf, resultwidth, resultheight, GDK_INTERP_BILINEAR);
+  scaledpixbuf = gdk_pixbuf_scale_simple (photopixbuf, resultwidth, resultheight, GDK_INTERP_BILINEAR);
 
   gdk_pixbuf_render_to_drawable_alpha (scaledpixbuf, widget->window,
 				       0, 0,                      /* src_x, src_y */
-				       (gint) ((maxwidth - resultwidth)/2), 0, /* dest_x, dest_y */
+				       (gint) ((maxwidth - resultwidth)/2), 0.5, /* dest_x, dest_y */
 				       -1, -1,                    /* use the pixbuf size */
 				       GDK_PIXBUF_ALPHA_BILEVEL,  /* ignored */
 				       128,                       /* ignored */
@@ -128,154 +128,20 @@ on_expose_event (GtkWidget *widget, GdkEventExpose *event, gpointer data)
   return TRUE;
 }
 
-
-GtkWidget*
-create_GPE_Ownerinfo (void)
+void
+on_smallphotobutton_clicked            (GtkButton       *button,
+                                        GtkWidget       *notebook)
 {
-  GtkWidget *GPE_Ownerinfo;
-  GtkWidget *mainvbox;
-  GtkWidget *catlabel;
-  GtkWidget *cathbox;
-  GtkWidget *indentlabel;
-  GtkWidget *indentedhbox;
-  GtkWidget *leftcolvbox;
-  GtkWidget *namelabel;
-  GtkWidget *phonelabel;
-  GtkWidget *emaillabel;
-  GtkWidget *addresslabel;
-  GtkWidget *address_button_vbox;
-  GtkWidget *drawingarea;
-  GtkWidget *rightcolvbox;
-  GtkWidget *scrolledwindow;
-  GtkWidget *viewport;
-  
-  gchar *gpe_catindent = gpe_get_catindent ();
-  //guint gpe_catspacing = gpe_get_catspacing ();
-  guint gpe_boxspacing = gpe_get_boxspacing ();
-  //guint gpe_border     = gpe_get_border ();
-
-  GPE_Ownerinfo = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  gtk_widget_set_name (GPE_Ownerinfo, "GPE_Ownerinfo");
-  gtk_object_set_data (GTK_OBJECT (GPE_Ownerinfo), "GPE_Ownerinfo", GPE_Ownerinfo);
-  //gtk_widget_set_usize (GPE_Ownerinfo, 240, 120);
-  //gtk_container_set_border_width (GTK_CONTAINER (GPE_Ownerinfo), gpe_border);
-  gtk_window_set_title (GTK_WINDOW (GPE_Ownerinfo), _("GPE Owner Info"));
-
-  mainvbox = gtk_vbox_new (FALSE, 0);
-  //mainvbox = gtk_vbox_new (FALSE, gpe_catspacing);
-  gtk_widget_show (mainvbox);
-  gtk_container_add (GTK_CONTAINER (GPE_Ownerinfo), mainvbox);
-
-  catlabel = gtk_label_new (_("Owner Information"));
-  /* FIXME: what about gettext here? */
-  gtk_label_set_markup (GTK_LABEL (catlabel), "<span weight='bold'>Owner Information</span>");
-  /* <span weight="bold">Category Header</span>
-     Set the label property Use Markup to Yes, and X Align to 0.0 to left align the label.
-  */
-  gtk_widget_show (catlabel);
-  gtk_box_pack_start (GTK_BOX (mainvbox), catlabel, FALSE, FALSE, 0);
-  gtk_label_set_justify (GTK_LABEL (catlabel), GTK_JUSTIFY_LEFT);
-  gtk_misc_set_alignment (GTK_MISC (catlabel), 0, 0.5);
-
-  cathbox = gtk_hbox_new (FALSE, 0);
-  gtk_widget_show (cathbox);
-  gtk_box_pack_start (GTK_BOX (mainvbox), cathbox, TRUE, TRUE, 0);
-
-  indentlabel = gtk_label_new (gpe_catindent);
-  gtk_widget_show (indentlabel);
-  gtk_box_pack_start (GTK_BOX (cathbox), indentlabel, FALSE, FALSE, 0);
-
-  indentedhbox = gtk_hbox_new (FALSE, 0);
-  gtk_widget_show (indentedhbox);
-  gtk_box_pack_start (GTK_BOX (cathbox), indentedhbox, TRUE, TRUE, 0);
-
-  leftcolvbox = gtk_vbox_new (FALSE, 0);
-  gtk_widget_show (leftcolvbox);
-  gtk_box_pack_start (GTK_BOX (indentedhbox), leftcolvbox, FALSE, FALSE, gpe_boxspacing);
-
-  namelabel = gtk_label_new (_("Name:"));
-  gtk_widget_show (namelabel);
-  gtk_box_pack_start (GTK_BOX (leftcolvbox), namelabel, FALSE, TRUE, 0);
-  gtk_label_set_justify (GTK_LABEL (namelabel), GTK_JUSTIFY_LEFT);
-  gtk_misc_set_alignment (GTK_MISC (namelabel), 0, 0.5);
-
-  emaillabel = gtk_label_new (_("E-Mail:"));
-  gtk_widget_show (emaillabel);
-  gtk_box_pack_start (GTK_BOX (leftcolvbox), emaillabel, FALSE, TRUE, 0);
-  gtk_label_set_justify (GTK_LABEL (emaillabel), GTK_JUSTIFY_LEFT);
-  gtk_misc_set_alignment (GTK_MISC (emaillabel), 0, 0.5);
-
-  phonelabel = gtk_label_new (_("Phone:"));
-  gtk_widget_show (phonelabel);
-  gtk_box_pack_start (GTK_BOX (leftcolvbox), phonelabel, FALSE, TRUE, 0);
-  gtk_label_set_justify (GTK_LABEL (phonelabel), GTK_JUSTIFY_LEFT);
-  gtk_misc_set_alignment (GTK_MISC (phonelabel), 0, 0.5);
-
-  address_button_vbox = gtk_vbox_new (FALSE, 0);
-  gtk_widget_show (address_button_vbox);
-  gtk_box_pack_start (GTK_BOX (leftcolvbox), address_button_vbox, TRUE, TRUE, 0);
-
-  addresslabel = gtk_label_new (_("Address:"));
-  gtk_widget_show (addresslabel);
-  gtk_box_pack_start (GTK_BOX (address_button_vbox), addresslabel, FALSE, FALSE, 0);
-  gtk_label_set_justify (GTK_LABEL (addresslabel), GTK_JUSTIFY_LEFT);
-  gtk_misc_set_alignment (GTK_MISC (addresslabel), 0, 0);
-
-  smallphotobutton = gtk_button_new ();
-  gtk_widget_show (smallphotobutton);
-  gtk_box_pack_start (GTK_BOX (address_button_vbox), smallphotobutton, TRUE, TRUE, 0);
-
-  drawingarea = gtk_drawing_area_new ();
-
-  gtk_widget_add_events (GTK_WIDGET (drawingarea),
-			 GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK);
-  g_signal_connect (G_OBJECT (drawingarea), "expose_event",  
-                    G_CALLBACK (on_expose_event), NULL);
-
-
-  gtk_widget_show (drawingarea);
-  gtk_container_add (GTK_CONTAINER (smallphotobutton), drawingarea);
-  
-  rightcolvbox = gtk_vbox_new (FALSE, 0);
-  gtk_widget_show (rightcolvbox);
-  gtk_box_pack_start (GTK_BOX (indentedhbox), rightcolvbox, TRUE, TRUE, 0);
-
-  name = gtk_label_new (_("Foo Bar"));
-  gtk_widget_show (name);
-  gtk_box_pack_start (GTK_BOX (rightcolvbox), name, FALSE, TRUE, 0);
-  gtk_label_set_justify (GTK_LABEL (name), GTK_JUSTIFY_LEFT);
-  gtk_misc_set_alignment (GTK_MISC (name), 0, 0.5);
-
-  email = gtk_label_new (_("foo@localhost"));
-  gtk_widget_show (email);
-  gtk_box_pack_start (GTK_BOX (rightcolvbox), email, FALSE, TRUE, 0);
-  gtk_label_set_justify (GTK_LABEL (email), GTK_JUSTIFY_LEFT);
-  gtk_misc_set_alignment (GTK_MISC (email), 0, 0.5);
-
-  phone = gtk_label_new (_("999 9999"));
-  gtk_widget_show (phone);
-  gtk_box_pack_start (GTK_BOX (rightcolvbox), phone, FALSE, TRUE, 0);
-  gtk_label_set_justify (GTK_LABEL (phone), GTK_JUSTIFY_LEFT);
-  gtk_misc_set_alignment (GTK_MISC (phone), 0, 0.5);
-
-  scrolledwindow = gtk_scrolled_window_new (NULL, NULL);
-  gtk_widget_show (scrolledwindow);
-  gtk_box_pack_start (GTK_BOX (rightcolvbox), scrolledwindow, TRUE, TRUE, 0);
-  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-
-  viewport = gtk_viewport_new (NULL, NULL);
-  gtk_widget_show (viewport);
-  gtk_container_add (GTK_CONTAINER (scrolledwindow), viewport);
-  gtk_viewport_set_shadow_type (GTK_VIEWPORT (viewport), GTK_SHADOW_NONE);
-
-  address = gtk_label_new (_("Address 1\nAddress 2\nAddress 3\nAddress 4"));
-  gtk_widget_show (address);
-  gtk_container_add (GTK_CONTAINER (viewport), address);
-  gtk_label_set_justify (GTK_LABEL (address), GTK_JUSTIFY_LEFT);
-  gtk_misc_set_alignment (GTK_MISC (address), 0, 0);
-
-  return GPE_Ownerinfo;
+  gtk_notebook_set_page (GTK_NOTEBOOK (notebook), 1);
 }
+
+void
+on_bigphotobutton_clicked              (GtkButton       *button,
+                                        GtkWidget       *notebook)
+{
+  gtk_notebook_set_page (GTK_NOTEBOOK (notebook), 0);
+}
+
 
 /*
  *  Return value: upgrade status
@@ -451,6 +317,29 @@ maybe_upgrade_datafile ()
 int
 main (int argc, char *argv[])
 {
+  GtkWidget *GPE_Ownerinfo;
+  GtkWidget *notebook;
+  GtkWidget *mainvbox;
+  GtkWidget *catlabel;
+  GtkWidget *cathbox;
+  GtkWidget *indentlabel;
+  GtkWidget *indentedhbox;
+  GtkWidget *leftcolvbox;
+  GtkWidget *namelabel;
+  GtkWidget *phonelabel;
+  GtkWidget *emaillabel;
+  GtkWidget *addresslabel;
+  GtkWidget *address_button_vbox;
+  GtkWidget *smallphotodrawingarea;
+  GtkWidget *bigphotodrawingarea;
+  GtkWidget *rightcolvbox;
+  GtkWidget *scrolledwindow;
+  GtkWidget *viewport;
+  
+  gchar *gpe_catindent = gpe_get_catindent ();
+  //guint gpe_catspacing = gpe_get_catspacing ();
+  guint gpe_boxspacing = gpe_get_boxspacing ();
+  //guint gpe_border     = gpe_get_border ();
 
   gchar *ownername, *owneremail, *ownerphone, *owneraddress;
   gchar *ownerphotofile;
@@ -587,10 +476,162 @@ main (int argc, char *argv[])
     exit (1);
 
   /* FIXME: check error */
-  pixbuf = gdk_pixbuf_new_from_file (photofile, NULL);
-  
-  GPE_Ownerinfo = create_GPE_Ownerinfo ();
+  photopixbuf = gdk_pixbuf_new_from_file (photofile, NULL);
 
+  /* draw the GUI */
+  GPE_Ownerinfo = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  gtk_widget_set_name (GPE_Ownerinfo, "GPE_Ownerinfo");
+  gtk_object_set_data (GTK_OBJECT (GPE_Ownerinfo), "GPE_Ownerinfo", GPE_Ownerinfo);
+  //gtk_widget_set_usize (GPE_Ownerinfo, 240, 120);
+  //gtk_container_set_border_width (GTK_CONTAINER (GPE_Ownerinfo), gpe_border);
+  gtk_window_set_title (GTK_WINDOW (GPE_Ownerinfo), _("GPE Owner Info"));
+
+  /* notebook with two pages;
+   * page 1 holds the small photo and the info text,
+   * page 2 just the big photo:
+   */
+  notebook = gtk_notebook_new ();
+  gtk_widget_show (notebook);
+  GTK_WIDGET_UNSET_FLAGS (notebook, GTK_CAN_FOCUS);
+  gtk_notebook_set_show_tabs (GTK_NOTEBOOK (notebook), FALSE);
+  gtk_notebook_set_show_border (GTK_NOTEBOOK (notebook), FALSE);
+  gtk_container_add (GTK_CONTAINER (GPE_Ownerinfo), notebook);
+
+  /*
+   * The first notebook page
+   */
+  mainvbox = gtk_vbox_new (FALSE, 0);
+  //mainvbox = gtk_vbox_new (FALSE, gpe_catspacing);
+  gtk_widget_show (mainvbox);
+  gtk_container_add (GTK_CONTAINER (notebook), mainvbox);
+
+  catlabel = gtk_label_new (_("Owner Information"));
+  /* FIXME: what about gettext here? */
+  gtk_label_set_markup (GTK_LABEL (catlabel), "<span weight='bold'>Owner Information</span>");
+  /* <span weight="bold">Category Header</span>
+     Set the label property Use Markup to Yes, and X Align to 0.0 to left align the label.
+  */
+  gtk_widget_show (catlabel);
+  gtk_box_pack_start (GTK_BOX (mainvbox), catlabel, FALSE, FALSE, 0);
+  gtk_label_set_justify (GTK_LABEL (catlabel), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (catlabel), 0, 0.5);
+
+  cathbox = gtk_hbox_new (FALSE, 0);
+  gtk_widget_show (cathbox);
+  gtk_box_pack_start (GTK_BOX (mainvbox), cathbox, TRUE, TRUE, 0);
+
+  indentlabel = gtk_label_new (gpe_catindent);
+  gtk_widget_show (indentlabel);
+  gtk_box_pack_start (GTK_BOX (cathbox), indentlabel, FALSE, FALSE, 0);
+
+  indentedhbox = gtk_hbox_new (FALSE, 0);
+  gtk_widget_show (indentedhbox);
+  gtk_box_pack_start (GTK_BOX (cathbox), indentedhbox, TRUE, TRUE, 0);
+
+  leftcolvbox = gtk_vbox_new (FALSE, 0);
+  gtk_widget_show (leftcolvbox);
+  gtk_box_pack_start (GTK_BOX (indentedhbox), leftcolvbox, FALSE, FALSE, gpe_boxspacing);
+
+  namelabel = gtk_label_new (_("Name:"));
+  gtk_widget_show (namelabel);
+  gtk_box_pack_start (GTK_BOX (leftcolvbox), namelabel, FALSE, TRUE, 0);
+  gtk_label_set_justify (GTK_LABEL (namelabel), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (namelabel), 0, 0.5);
+
+  emaillabel = gtk_label_new (_("E-Mail:"));
+  gtk_widget_show (emaillabel);
+  gtk_box_pack_start (GTK_BOX (leftcolvbox), emaillabel, FALSE, TRUE, 0);
+  gtk_label_set_justify (GTK_LABEL (emaillabel), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (emaillabel), 0, 0.5);
+
+  phonelabel = gtk_label_new (_("Phone:"));
+  gtk_widget_show (phonelabel);
+  gtk_box_pack_start (GTK_BOX (leftcolvbox), phonelabel, FALSE, TRUE, 0);
+  gtk_label_set_justify (GTK_LABEL (phonelabel), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (phonelabel), 0, 0.5);
+
+  address_button_vbox = gtk_vbox_new (FALSE, 0);
+  gtk_widget_show (address_button_vbox);
+  gtk_box_pack_start (GTK_BOX (leftcolvbox), address_button_vbox, TRUE, TRUE, 0);
+
+  addresslabel = gtk_label_new (_("Address:"));
+  gtk_widget_show (addresslabel);
+  gtk_box_pack_start (GTK_BOX (address_button_vbox), addresslabel, FALSE, FALSE, 0);
+  gtk_label_set_justify (GTK_LABEL (addresslabel), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (addresslabel), 0, 0);
+
+  smallphotobutton = gtk_button_new ();
+  gtk_widget_show (smallphotobutton);
+  //  GTK_WIDGET_UNSET_FLAGS (smallphotobutton, GTK_CAN_FOCUS);
+  //  gtk_button_set_relief (GTK_BUTTON (smallphotobutton), GTK_RELIEF_NONE);
+  gtk_box_pack_start (GTK_BOX (address_button_vbox), smallphotobutton, TRUE, TRUE, 0);
+
+  smallphotodrawingarea = gtk_drawing_area_new ();
+  gtk_widget_add_events (GTK_WIDGET (smallphotodrawingarea),
+			 GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK);
+  g_signal_connect (G_OBJECT (smallphotodrawingarea), "expose_event",  
+                    G_CALLBACK (on_expose_event), NULL);
+  gtk_widget_show (smallphotodrawingarea);
+  gtk_container_add (GTK_CONTAINER (smallphotobutton), smallphotodrawingarea);
+  
+  rightcolvbox = gtk_vbox_new (FALSE, 0);
+  gtk_widget_show (rightcolvbox);
+  gtk_box_pack_start (GTK_BOX (indentedhbox), rightcolvbox, TRUE, TRUE, 0);
+
+  name = gtk_label_new (_("Foo Bar"));
+  gtk_widget_show (name);
+  gtk_box_pack_start (GTK_BOX (rightcolvbox), name, FALSE, TRUE, 0);
+  gtk_label_set_justify (GTK_LABEL (name), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (name), 0, 0.5);
+
+  email = gtk_label_new (_("foo@localhost"));
+  gtk_widget_show (email);
+  gtk_box_pack_start (GTK_BOX (rightcolvbox), email, FALSE, TRUE, 0);
+  gtk_label_set_justify (GTK_LABEL (email), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (email), 0, 0.5);
+
+  phone = gtk_label_new (_("999 9999"));
+  gtk_widget_show (phone);
+  gtk_box_pack_start (GTK_BOX (rightcolvbox), phone, FALSE, TRUE, 0);
+  gtk_label_set_justify (GTK_LABEL (phone), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (phone), 0, 0.5);
+
+  scrolledwindow = gtk_scrolled_window_new (NULL, NULL);
+  gtk_widget_show (scrolledwindow);
+  gtk_box_pack_start (GTK_BOX (rightcolvbox), scrolledwindow, TRUE, TRUE, 0);
+  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolledwindow), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+
+  viewport = gtk_viewport_new (NULL, NULL);
+  gtk_widget_show (viewport);
+  gtk_container_add (GTK_CONTAINER (scrolledwindow), viewport);
+  gtk_viewport_set_shadow_type (GTK_VIEWPORT (viewport), GTK_SHADOW_NONE);
+
+  address = gtk_label_new (_("Address 1\nAddress 2\nAddress 3\nAddress 4"));
+  gtk_widget_show (address);
+  gtk_container_add (GTK_CONTAINER (viewport), address);
+  gtk_label_set_justify (GTK_LABEL (address), GTK_JUSTIFY_LEFT);
+  gtk_misc_set_alignment (GTK_MISC (address), 0, 0);
+
+  /*
+   * The second notebook page
+   */
+  bigphotobutton = gtk_button_new ();
+  gtk_widget_show (bigphotobutton);
+  GTK_WIDGET_UNSET_FLAGS (bigphotobutton, GTK_CAN_FOCUS);
+  gtk_button_set_relief (GTK_BUTTON (bigphotobutton), GTK_RELIEF_NONE);
+  gtk_container_add (GTK_CONTAINER (notebook), bigphotobutton);
+
+  bigphotodrawingarea = gtk_drawing_area_new ();
+  gtk_widget_add_events (GTK_WIDGET (bigphotodrawingarea),
+			 GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK);
+  g_signal_connect (G_OBJECT (bigphotodrawingarea), "expose_event",  
+                    G_CALLBACK (on_expose_event), NULL);
+  gtk_widget_show (bigphotodrawingarea);
+  gtk_container_add (GTK_CONTAINER (bigphotobutton), bigphotodrawingarea);
+  
+  /* end of drawing the GUI */
+
+  
   if (geometry)
     {
       val = XParseGeometry (geometry, &x, &y, &w, &h);
@@ -639,12 +680,12 @@ main (int argc, char *argv[])
   gtk_signal_connect (GTK_OBJECT (GPE_Ownerinfo), "destroy",
                       GTK_SIGNAL_FUNC (gtk_main_quit),
                       NULL);
-//  gtk_signal_connect (GTK_OBJECT (smallphotobutton), "clicked",
-//                      GTK_SIGNAL_FUNC (on_smallphotobutton_clicked),
-//                      NULL);
-//  gtk_signal_connect (GTK_OBJECT (bigphotobutton), "clicked",
-//                      GTK_SIGNAL_FUNC (on_bigphotobutton_clicked),
-//                      NULL);
+  gtk_signal_connect (GTK_OBJECT (smallphotobutton), "clicked",
+		      GTK_SIGNAL_FUNC (on_smallphotobutton_clicked),
+		      notebook);
+  gtk_signal_connect (GTK_OBJECT (bigphotobutton), "clicked",
+		      GTK_SIGNAL_FUNC (on_bigphotobutton_clicked),
+		      notebook);
 
   gtk_widget_show (GPE_Ownerinfo);
 
