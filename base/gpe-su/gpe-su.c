cc8e79fe1888de94227ea6c1f9fa8ddf4603ecd5
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-02-13 01:11:24 +0000

    fix windowing issues
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@2794 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-su/gpe-su.c
+++ base/gpe-su/gpe-su.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2002 Philip Blundell <philb@gnu.org>
+ * Copyright (C) 2002, 2003 Philip Blundell <philb@gnu.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -61,9 +61,10 @@ do_su (void)
   rp = result;
 
   gtk_entry_set_text (GTK_ENTRY (entry), "");
-  gdk_window_withdraw (window->window);
 
-  gtk_main_iteration ();
+  gtk_widget_hide (window);
+  
+  gdk_flush ();
 
   pid = forkpty (&pty, NULL, NULL, NULL);
   
@@ -115,7 +116,8 @@ do_su (void)
     {
       gpe_error_box (result);
       kill (pid, 15);
-      gdk_window_show (window->window);
+      gtk_widget_show (window);
+      gtk_widget_grab_focus (entry);
     }
   else
     gtk_main_quit ();
@@ -158,22 +160,17 @@ main (int argc, char *argv[])
   gtk_widget_realize (window);
   
   hbox = gtk_hbox_new (FALSE, 0);
-  gtk_widget_show (hbox);
   entry = gtk_entry_new ();
-  gtk_widget_show (entry);
   label = gtk_label_new (_("Enter password:"));
-  gtk_widget_show (label);
   gtk_misc_set_alignment (GTK_MISC (label), 0.0, 0.5);
   gtk_entry_set_visibility (GTK_ENTRY (entry), FALSE);
 
   vbox = gtk_vbox_new (FALSE, 0);
-  gtk_widget_show (vbox);
   gtk_box_pack_start (GTK_BOX (vbox), label, FALSE, FALSE, 0);
   gtk_box_pack_start (GTK_BOX (vbox), entry, FALSE, FALSE, 0);
 
   p = gpe_find_icon ("lock");
   pw = gpe_render_icon (window->style, p);
-  gtk_widget_show (pw);
   gtk_box_pack_start (GTK_BOX (hbox), pw, FALSE, FALSE, 4);
 
   gtk_box_pack_start (GTK_BOX (hbox), vbox, TRUE, TRUE, 0);
@@ -197,8 +194,7 @@ main (int argc, char *argv[])
   gtk_signal_connect (GTK_OBJECT (buttonok), "clicked", do_su, NULL);
   gtk_signal_connect (GTK_OBJECT (buttoncancel), "clicked", gtk_main_quit, NULL);
 
-  gtk_widget_show (window);
-
+  gtk_widget_show_all (window);
   gtk_widget_grab_focus (entry);
 
   gtk_main ();
