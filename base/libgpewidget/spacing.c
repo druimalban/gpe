9bbb1d3f549d875f21e02502c028c406e71ebd4d
cmarqu <cmarqu@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-02-19 18:26:44 +0000

    Add link in comment. Change license to LGPL.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@2905 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/libgpewidget/spacing.c
+++ base/libgpewidget/spacing.c
@@ -1,10 +1,20 @@
 /*
- * Copyright (C) 2002 Colin Marquardt <ipaq@marquardt-home.de>
+ * Copyright (C) 2002, 2003 Colin Marquardt <ipaq@marquardt-home.de>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version
- * 2 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
 #include "spacing.h"
@@ -16,6 +26,9 @@
  */
 #define GPE_GNOME_SCALING 2
 
+/* See also
+ *  http://mail.gnome.org/archives/gtk-devel-list/2003-February/msg00009.html
+ */
 
 /* the spacing for categories in a dialog: */
 guint
