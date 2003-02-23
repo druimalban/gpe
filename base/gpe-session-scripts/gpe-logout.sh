66ca1825a6c113adb66a12b7fda502681edbc777
cmarqu <cmarqu@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-02-23 22:27:27 +0000

    Swapped button order change was buggy, fix.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@2974 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-session-scripts/gpe-logout.sh
+++ base/gpe-session-scripts/gpe-logout.sh
@@ -2,4 +2,13 @@
 
 gpe-question --icon /usr/share/pixmaps/gpe-logout.png --question "<span weight='bold' size='larger'>Are you sure you want to log out?</span> 
 
-Unsaved data from applications will be lost." --buttons !gtk-cancel "ok:Log out" && mbcontrol -exit
+Unsaved data from applications will be lost." --buttons !gtk-cancel "ok:Log out"
+
+# check for button number returned by gpe-question:
+if [ $? -eq 1 ]; then
+    echo "Logout.";
+    mbcontrol -exit;
+else
+    echo "Logout cancelled.";
+fi
+
