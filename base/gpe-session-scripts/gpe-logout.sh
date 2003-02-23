a7e693f7c188f750735b616869a419f872c05beb
cmarqu <cmarqu@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-02-23 20:10:57 +0000

    Swap order of the buttons. Make primary text bold and larger, add secondary text. Fix quotes in Pango markup.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@2973 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-session-scripts/gpe-logout.sh
+++ base/gpe-session-scripts/gpe-logout.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gpe-question --icon /usr/share/pixmaps/gpe-logout.png --question "<span weight="bold" size="larger">Are you sure you want to log out?</span> 
+gpe-question --icon /usr/share/pixmaps/gpe-logout.png --question "<span weight='bold' size='larger'>Are you sure you want to log out?</span> 
 
 Unsaved data from applications will be lost." --buttons !gtk-cancel "ok:Log out" && mbcontrol -exit
