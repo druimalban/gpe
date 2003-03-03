a1626a2c53a992ef9e1d1fce1c850168bb80620b
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-03-03 00:18:52 +0000

    start of new ui; not functional at present
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@3055 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/teleport/teleport.c
+++ base/teleport/teleport.c
@@ -15,6 +15,7 @@
 #include <X11/Xatom.h>
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <libintl.h>
 #include <string.h>
 #include <sys/stat.h>
@@ -24,6 +25,8 @@
 #include <gpe/pixmaps.h>
 #include <gpe/smallbox.h>
 #include <gpe/errorbox.h>
+#include <gpe/spacing.h>
+#include <gpe/windows.h>
 
 #include "displays.h"
 
@@ -40,6 +43,36 @@ gboolean grabbed;
 struct display *selected_dpy;
 
 extern gchar *sign_challenge (gchar *text, int length, gchar *target);
+
+Atom atoms[8];
+
+char *atom_names[] = 
+  {
+    "_NET_CLIENT_LIST",
+    "UTF8_STRING",
+    "WM_NAME",
+    "_NET_WM_NAME",
+    "_NET_WM_ICON"
+  };
+
+#define _NET_CLIENT_LIST 0
+#define UTF8_STRING 1
+#define WM_NAME 2
+#define _NET_WM_NAME 3
+#define _NET_WM_ICON 4
+
+struct client_window
+{
+  Window w;
+  gchar *name;
+  GdkPixbuf *icon;
+};
+
+struct gpe_icon my_icons[] = 
+  {
+    { "icon", PREFIX "/share/pixmaps/teleport.png" },
+    { NULL }
+  };
 
 static void
 send_message (Display *dpy, Window w, char *host, gchar *method, gchar *data)
@@ -91,106 +124,45 @@ migrate_to (Display *dpy, Window w, char *host, int display, int screen)
     g_free (data);
 }
 
-static int
-handle_click (Display *dpy, Window w)
+GSList *
+get_clients (Display *dpy)
 {
-  Atom type;
-  int format;
-  unsigned long nitems, bytes_after;
-  unsigned char *prop = NULL;
+  Window *windows;
+  guint nwin, i;
+  GSList *clients = NULL;
 
-  if (XGetWindowProperty (dpy, w, migrate_atom, 0, 0, False,
-			  string_atom, &type, &format, &nitems, &bytes_after, &prop) != Success
-      || type != string_atom)
+  gpe_get_client_window_list (dpy, &windows, &nwin);
+  for (i = 0; i < nwin; i++)
     {
-      Window root, parent, *children;
-      unsigned int nchildren;  
-  
-      XQueryTree (dpy, w, &root, &parent, &children, &nchildren);
-      if (children)
-	XFree (children);
-
-      if (root != w && root != parent)
-	return handle_click (dpy, parent);
-
-      return None;
-    }
-
-  if (nitems)
-    return None;
-
-  if (prop)
-    XFree (prop);
-
-  return w;
-}
-
-static Window 
-find_deepest_window (Display *dpy, Window grandfather, Window parent,
-		     int x, int y)
-{
-  int dest_x, dest_y;
-  Window child;
-  
-  XTranslateCoordinates (dpy, grandfather, parent, x, y,
-			 &dest_x, &dest_y, &child);
-
-  if (child == None)
-    return parent;
-  
-  return find_deepest_window(dpy, parent, child, dest_x, dest_y);
-}
-
-void
-add_cancel (GtkWidget *w, GtkWidget *window)
-{
-  gtk_widget_destroy (window);
-}
-
-void
-add_ok (GtkWidget *w, GtkWidget *window)
-{
-  GtkWidget *host_entry = g_object_get_data (G_OBJECT (window), "entry");
-  GtkWidget *dpy_spin = g_object_get_data (G_OBJECT (window), "dpy_spin");
-  GtkWidget *screen_spin = g_object_get_data (G_OBJECT (window), "screen_spin");
-  GtkAdjustment *dpy_adjustment = gtk_spin_button_get_adjustment (GTK_SPIN_BUTTON (dpy_spin));
-  GtkAdjustment *screen_adjustment = gtk_spin_button_get_adjustment (GTK_SPIN_BUTTON (screen_spin));
-  const gchar *host = gtk_entry_get_text (GTK_ENTRY (host_entry));
-  guint dpy = (guint)gtk_adjustment_get_value (dpy_adjustment);
-  guint screen = (guint)gtk_adjustment_get_value (screen_adjustment);
-  GtkTreeIter iter;
-  struct display *d;
-
-  if (host[0] == 0)
-    {
-      gpe_error_box (_("Must specify hostname"));
-      return;
-    }
-
-  d = add_display (host, dpy, screen);
-
-  gtk_list_store_append (list_store, &iter);
-  gtk_list_store_set (list_store, &iter, 0, d->str, 1, d, -1);
+      Window w = windows[i];
+      gchar *name;
+      struct client_window *cw;
+      GdkPixbuf *icon;
  
-  gtk_widget_destroy (window);
-}
+#if 0
+      if (! can_migrate (dpy, w))
+	continue;
+#endif
 
-void
-remove_callback (GtkWidget *button, GtkWidget *list_view)
-{
-  GtkTreeSelection *sel = gtk_tree_view_get_selection (GTK_TREE_VIEW (list_view));
-  GtkTreeIter iter;
-  GtkTreeModel *model;
+      name = gpe_get_window_name (dpy, w);
 
-  if (gtk_tree_selection_get_selected (sel, &model, &iter))
-    {
-      struct display *d;
-      gtk_tree_model_get (GTK_TREE_MODEL (model), &iter, 1, &d, -1);
-      gtk_list_store_remove (GTK_LIST_STORE (model), &iter);
-      remove_display (d);
+      icon = gpe_get_window_icon (dpy, w);
+
+      cw = g_malloc (sizeof (*cw));
+      cw->w = w;
+      cw->name = name;
+      if (icon)
+	{
+	  cw->icon = gdk_pixbuf_scale_simple (icon, 16, 16, GDK_INTERP_BILINEAR);
+	  gdk_pixbuf_unref (icon);
+	}
+      else
+	cw->icon = NULL;
+
+      clients = g_slist_append (clients, cw);
     }
-  else
-    gpe_error_box (_("No display selected"));
+
+  return clients;
 }
 
 void
@@ -217,164 +189,75 @@ go_callback (GtkWidget *button, GtkWidget *list_view)
 }
 
 void
-add_callback (void)
-{
-  GtkWidget *window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  GtkWidget *ok = gpe_button_new_from_stock (GTK_STOCK_OK, GPE_BUTTON_TYPE_BOTH);
-  GtkWidget *cancel = gpe_button_new_from_stock (GTK_STOCK_CANCEL, GPE_BUTTON_TYPE_BOTH);
-  GtkWidget *hbox = gtk_hbox_new (FALSE, 0);
-  GtkWidget *vbox = gtk_vbox_new (FALSE, 0);
-  GtkWidget *host_label = gtk_label_new (_("Host"));
-  GtkWidget *host_entry = gtk_entry_new ();
-  GtkWidget *host_box = gtk_hbox_new (FALSE, 0);
-  GtkWidget *dpy_label = gtk_label_new (_("Display"));
-  GtkWidget *screen_label = gtk_label_new (_("Screen"));
-  GtkWidget *dpy_spin = gtk_spin_button_new_with_range (0, 255, 1);
-  GtkWidget *screen_spin = gtk_spin_button_new_with_range (0, 255, 1);
-  GtkWidget *dpy_box = gtk_hbox_new (FALSE, 0);
-  GtkWidget *screen_box = gtk_hbox_new (FALSE, 0);
-  GtkWidget *sep = gtk_hseparator_new ();
-
-  gtk_box_pack_start (GTK_BOX (host_box), host_label, FALSE, FALSE, 2);
-  gtk_box_pack_start (GTK_BOX (host_box), host_entry, TRUE, TRUE, 2);
-
-  gtk_box_pack_start (GTK_BOX (screen_box), screen_label, FALSE, FALSE, 2);
-  gtk_box_pack_start (GTK_BOX (screen_box), screen_spin, TRUE, TRUE, 2);
-
-  gtk_box_pack_start (GTK_BOX (dpy_box), dpy_label, FALSE, FALSE, 2);
-  gtk_box_pack_start (GTK_BOX (dpy_box), dpy_spin, TRUE, TRUE, 2);
-
-  gtk_box_pack_start (GTK_BOX (hbox), ok, TRUE, FALSE, 2);
-  gtk_box_pack_start (GTK_BOX (hbox), cancel, TRUE, FALSE, 2);
-
-  gtk_box_pack_start (GTK_BOX (vbox), host_box, FALSE, FALSE, 2);
-  gtk_box_pack_start (GTK_BOX (vbox), dpy_box, FALSE, FALSE, 2);
-  gtk_box_pack_start (GTK_BOX (vbox), screen_box, FALSE, FALSE, 2);
-  gtk_box_pack_start (GTK_BOX (vbox), sep, FALSE, FALSE, 3);
-  gtk_box_pack_start (GTK_BOX (vbox), hbox, FALSE, FALSE, 0);
-
-  g_object_set_data (G_OBJECT (window), "entry", host_entry);
-  g_object_set_data (G_OBJECT (window), "dpy_spin", dpy_spin);
-  g_object_set_data (G_OBJECT (window), "screen_spin", screen_spin);
-
-  g_signal_connect (G_OBJECT (ok), "clicked", G_CALLBACK (add_ok), window);
-  g_signal_connect (G_OBJECT (cancel), "clicked", G_CALLBACK (add_cancel), window);
-
-  gtk_window_set_title (GTK_WINDOW (window), _("New display"));
-
-  gtk_widget_realize (window);
-  gdk_window_set_type_hint (window->window, GDK_WINDOW_TYPE_HINT_DIALOG);
-
-  gtk_container_add (GTK_CONTAINER (window), vbox);
-  
-  gtk_widget_show_all (window);
-}
-
-void
-open_window (void)
+open_window (GSList *clients)
 {
   GtkWidget *window;
-  GtkWidget *add_button;
-  GtkWidget *remove_button;
+  GtkWidget *quit_button;
   GtkWidget *go_button;
-  GtkWidget *hbox;
-  GtkWidget *vbox;
-  GtkWidget *scrolled_window;
-  GtkWidget *list_view;
-  GtkCellRenderer *renderer;
-  GtkTreeViewColumn *column;
+  GtkWidget *hbox1, *hbox2;
+  GtkWidget *option_menu;
+  GtkWidget *display_label;
+  GtkWidget *display_combo;
+  GtkWidget *client_label;
+  GList *strings = NULL;
+  GSList *i;
+  GtkWidget *menu;
 
-  window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  for (i = displays; i; i = i->next)
+    {
+      struct display *d = i->data;
+      strings = g_list_append (strings, d->str);
+    }
 
-  gtk_widget_realize (window);
+  window = gtk_dialog_new ();
 
-  add_button = gpe_button_new_from_stock (GTK_STOCK_ADD, GPE_BUTTON_TYPE_ICON);
-  remove_button = gpe_button_new_from_stock (GTK_STOCK_REMOVE, GPE_BUTTON_TYPE_ICON);
-  go_button = gpe_button_new_from_stock (GTK_STOCK_EXECUTE, GPE_BUTTON_TYPE_ICON);
+  hbox1 = gtk_hbox_new (FALSE, gpe_get_boxspacing ());
+  display_label = gtk_label_new (_("Display"));
+  display_combo = gtk_combo_new ();
+  gtk_combo_set_popdown_strings (GTK_COMBO (display_combo), strings);
+  gtk_box_pack_start (GTK_BOX (hbox1), display_label, FALSE, FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (hbox1), display_combo, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->vbox), hbox1, FALSE, FALSE, gpe_get_boxspacing ());
 
-  hbox = gtk_hbox_new (FALSE, 0);
-  vbox = gtk_vbox_new (FALSE, 0);
+  hbox2 = gtk_hbox_new (FALSE, gpe_get_boxspacing ());
+  client_label = gtk_label_new (_("Window"));
+  option_menu = gtk_option_menu_new ();
+  menu = gtk_menu_new ();
+  gtk_option_menu_set_menu (GTK_OPTION_MENU (option_menu), menu);
+  gtk_box_pack_start (GTK_BOX (hbox2), client_label, FALSE, FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (hbox2), option_menu, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->vbox), hbox2, FALSE, FALSE, gpe_get_boxspacing ());
 
-  scrolled_window = gtk_scrolled_window_new (NULL, NULL);
-  gtk_scrolled_window_set_policy (GTK_SCROLLED_WINDOW (scrolled_window),
-				  GTK_POLICY_NEVER, GTK_POLICY_AUTOMATIC);
+  for (i = clients; i; i = i->next)
+    {
+      struct client_window *cw = i->data;
+      GtkWidget *item = gtk_image_menu_item_new_with_label (cw->name);
+      if (cw->icon)
+	{
+	  GtkWidget *image = gtk_image_new_from_pixbuf (cw->icon);
+	  gtk_widget_show (image);
+	  gtk_image_menu_item_set_image (GTK_IMAGE_MENU_ITEM (item), image);
+	}
 
-  list_view = gtk_tree_view_new_with_model (GTK_TREE_MODEL (list_store));
+      gtk_widget_show (item);
+      gtk_menu_shell_append (GTK_MENU_SHELL (menu), item);
+    }
 
-  renderer = gtk_cell_renderer_text_new ();
-  column = gtk_tree_view_column_new_with_attributes (_("Displays"), renderer,
-						     "text", 0, NULL);
-  gtk_tree_view_append_column (GTK_TREE_VIEW (list_view), column);
+  quit_button = gtk_button_new_from_stock (GTK_STOCK_CLOSE);
+  go_button = gtk_button_new_from_stock (GTK_STOCK_EXECUTE);
 
-  gtk_container_add (GTK_CONTAINER (scrolled_window), list_view);
+  gtk_box_pack_end (GTK_BOX (GTK_DIALOG (window)->action_area), go_button, FALSE, FALSE, 0);
+  gtk_box_pack_end (GTK_BOX (GTK_DIALOG (window)->action_area), quit_button, FALSE, FALSE, 0);
 
-  gtk_box_pack_start (GTK_BOX (vbox), add_button, TRUE, TRUE, 2);
-  gtk_box_pack_start (GTK_BOX (vbox), remove_button, TRUE, TRUE, 2);
-  gtk_box_pack_start (GTK_BOX (vbox), go_button, TRUE, TRUE, 2);
+  gtk_window_set_title (GTK_WINDOW (window), _("Teleport"));
+  gpe_set_window_icon (GTK_WIDGET (window), "icon");
 
-  gtk_box_pack_start (GTK_BOX (hbox), scrolled_window, TRUE, TRUE, 2);
-  gtk_box_pack_start (GTK_BOX (hbox), vbox, FALSE, FALSE, 0);
-
-  gtk_container_add (GTK_CONTAINER (window), hbox);
-
-  gdk_window_set_type_hint (window->window, GDK_WINDOW_TYPE_HINT_TOOLBAR);
-
-  gtk_window_set_title (GTK_WINDOW (window), _("GPE Teleport"));
-
-  gtk_window_set_default_size (GTK_WINDOW (window), 240, 64);
-
-  g_signal_connect (G_OBJECT (add_button), "clicked", G_CALLBACK (add_callback), NULL);
-  g_signal_connect (G_OBJECT (remove_button), "clicked", G_CALLBACK (remove_callback), list_view);
-  g_signal_connect (G_OBJECT (go_button), "clicked", G_CALLBACK (go_callback), list_view);
+  g_signal_connect (G_OBJECT (quit_button), "clicked", G_CALLBACK (g_main_loop_quit), NULL);
+  g_signal_connect (G_OBJECT (go_button), "clicked", G_CALLBACK (go_callback), NULL);
 
   g_signal_connect (G_OBJECT (window), "delete-event", G_CALLBACK (g_main_loop_quit), NULL);
 
   gtk_widget_show_all (window);
-}
-
-GdkFilterReturn
-window_filter (GdkXEvent *xev, GdkEvent *gev, gpointer d)
-{
-  XEvent *ev = (XEvent *)xev;
-  Display *dpy = ev->xany.display;
-
-  if (ev->xany.type == ButtonRelease
-      && ev->xbutton.window == RootWindow (dpy, 0))
-    {
-      Window w;
-
-      XUngrabPointer (dpy, ev->xbutton.time);
-
-      w = find_deepest_window (dpy, RootWindow (dpy, 0), RootWindow (dpy, 0),
-			       ev->xbutton.x, ev->xbutton.y);
-
-      XGrabServer (dpy);
-      w = handle_click (dpy, w);
-      if (w)
-	migrate_to (dpy, w, selected_dpy->host, selected_dpy->dpy, selected_dpy->screen);
-      XUngrabServer (dpy);
-
-      if (w == None)
-	gpe_error_box (_("Cannot migrate this application"));
-
-      return GDK_FILTER_REMOVE;
-    }
-
-  return GDK_FILTER_CONTINUE;
-}
-
-void
-add_initial_displays ()
-{
-  GSList *i;
-
-  for (i = displays; i; i = i->next)
-    {
-      GtkTreeIter iter;
-      struct display *d = i->data;
-      gtk_list_store_append (list_store, &iter);
-      gtk_list_store_set (list_store, &iter, 0, d->str, 1, d, -1);
-    }
 }
 
 int
@@ -382,18 +265,18 @@ main (int argc, char *argv[])
 {
   Display *dpy;
   gchar *home_dir, *d;
+  GSList *clients;
 
   if (gpe_application_init (&argc, &argv) == FALSE)
+    exit (1);
+
+  if (gpe_load_icons (my_icons) == FALSE)
     exit (1);
 
   dpy = GDK_DISPLAY ();
   string_atom = XInternAtom (dpy, "STRING", False);
   migrate_atom = XInternAtom (dpy, "_GPE_DISPLAY_CHANGE", False);
   challenge_atom = XInternAtom (dpy, "_GPE_DISPLAY_CHANGE_RSA_CHALLENGE", False);
-
-  list_store = gtk_list_store_new (2, G_TYPE_STRING, G_TYPE_POINTER);
-
-  gdk_window_add_filter (NULL, window_filter, NULL);
 
   home_dir = g_get_home_dir ();
 
@@ -407,9 +290,13 @@ main (int argc, char *argv[])
   crypt_init ();
 
   displays_init ();
-  add_initial_displays ();
 
-  open_window ();
+  XInternAtoms (dpy, atom_names, sizeof (atom_names) / sizeof (atom_names[0]),
+		False, atoms);
+
+  clients = get_clients (dpy);
+
+  open_window (clients);
 
   gtk_main ();
 
