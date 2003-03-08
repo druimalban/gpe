f0fd421343dd4539ea967e67beaec43e1fa8c96d
cmarqu <cmarqu@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-03-08 11:33:37 +0000

    Define GPE_GNOME_SCALING.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@3140 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/libgpewidget/gpe/spacing.h
+++ base/libgpewidget/gpe/spacing.h
@@ -1,10 +1,20 @@
 /*
- * Copyright (C) 2002 Colin Marquardt <ipaq@marquardt-home.de>
+ * Copyright (C) 2002, 2003 Colin Marquardt <ipaq@marquardt-home.de>
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version
- * 2 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
 #ifndef SPACING_H
@@ -12,12 +22,27 @@
 
 #include <glib.h>
 
-extern guint gpe_get_catspacing ();
+/*
+ * How much to scale down the spacings defined in the Gnome 2 HIG
+ *   (http://developer.gnome.org/projects/gup/hig/1.0/layout.html#layout-window)
+ * for GPE:
+ */
+#define GPE_GNOME_SCALING 2
 
-extern gchar *gpe_get_catindent ();
+/* See also
+ *  http://mail.gnome.org/archives/gtk-devel-list/2003-February/msg00009.html
+ */
 
-extern guint gpe_get_border ();
+/* the spacing for categories in a dialog: */
+guint gpe_get_catspacing (void);
 
-extern guint gpe_get_boxspacing ();
+/* the indent for categories in a dialog: */
+gchar *gpe_get_catindent (void);
+
+/* the border for e.g. a GtkDialog: */
+guint gpe_get_border (void);
+
+/* the spacing in boxes: */
+guint gpe_get_boxspacing (void);
 
 #endif
