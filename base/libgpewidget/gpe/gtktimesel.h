d2ef0e881e9b5c8c0d4decbb207d30d99cd63fb1
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-03-28 22:00:24 +0000

    2003-03-28  Philip Blundell  <philb@gnu.org>
    	* gtktimesel.c: New file.
    	* gpe/gtktimesel.h: Likewise.
    	* Makefile (MEMBERS): Add gtktimesel.
    
    
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@3447 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/libgpewidget/gpe/gtktimesel.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2002, 2003 Philip Blundell <philb@gnu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
+#ifndef GTK_TIME_SEL_H
+#define GTK_TIME_SEL_H
+
+#include <gtk/gtk.h>
+#include <glib-object.h>
+
+#define GTK_TYPE_TIME_SEL                  (gtk_time_sel_get_type ())
+#define GTK_TIME_SEL(obj)                  G_TYPE_CHECK_INSTANCE_CAST ((obj), gtk_time_sel_get_type(), GtkTimeSel)
+#define GTK_TIME_SEL_CONST(obj)	G_TYPE_CHECK_INSTANCE_CAST ((obj), gtk_time_sel_get_type(), GtkTime_Sel const)
+#define GTK_TIME_SEL_CLASS(klass)	G_TYPE_CHECK_CLASS_CAST ((klass), gtk_time_sel_get_type(), GtkTime_SelClass)
+#define GTK_IS_TIME_SEL(obj)	G_TYPE_CHECK_INSTANCE_TYPE ((obj), gtk_time_sel_get_type ())
+
+#define GTK_TIME_SEL_GET_CLASS(obj)	G_TYPE_INSTANCE_GET_CLASS ((obj), gtk_time_sel_get_type(), GtkTime_SelClass)
+
+typedef struct _GtkTimeSel	   GtkTimeSel;
+typedef struct _GtkTimeSelClass    GtkTimeSelClass;
+
+GtkType		gtk_time_sel_get_type (void);
+
+GtkWidget      *gtk_time_sel_new ();
+
+void		gtk_time_sel_get_time (GtkTimeSel *sel, guint *hour, guint *minute);
+void		gtk_time_sel_set_time (GtkTimeSel *sel, guint hour, guint minute);
+
+#endif
---
+++ base/libgpewidget/gtktimesel.c
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2003 Philip Blundell <philb@gnu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
+#include <libintl.h>
+#include <gtk/gtk.h>
+#include "gtktimesel.h"
+
+#define _(x) gettext(x)
+
+struct _GtkTimeSel
+{
+  GtkHBox hbox;
+
+  GtkObject *hour_adj, *minute_adj;
+  GtkWidget *hour_spin, *minute_spin;
+  GtkWidget *label;
+};
+
+struct _GtkTimeSelClass 
+{
+  GtkHBoxClass parent_class;
+};
+
+static GtkHBoxClass *parent_class = NULL;
+
+static void
+gtk_time_sel_init (GtkTimeSel *sel)
+{
+  sel->hour_adj = gtk_adjustment_new (0, 0, 24, 1, 15, 15);
+  sel->minute_adj = gtk_adjustment_new (0, 0, 60, 1, 15, 15);
+
+  sel->hour_spin = gtk_spin_button_new (GTK_ADJUSTMENT (sel->hour_adj), 1, 0);
+  sel->minute_spin = gtk_spin_button_new (GTK_ADJUSTMENT (sel->minute_adj), 1, 0);
+
+  sel->label = gtk_label_new (":");
+
+  gtk_box_pack_start (GTK_BOX (sel), sel->hour_spin, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (sel), sel->label, FALSE, FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (sel), sel->minute_spin, TRUE, TRUE, 0);
+}
+
+static void
+gtk_time_sel_show (GtkWidget *widget)
+{
+  GtkTimeSel *sel;
+
+  g_return_if_fail (widget != NULL);
+  g_return_if_fail (GTK_IS_TIME_SEL (widget));
+
+  sel = GTK_TIME_SEL (widget);
+
+  gtk_widget_show (sel->hour_spin);
+  gtk_widget_show (sel->minute_spin);
+  gtk_widget_show (sel->label);
+
+  GTK_WIDGET_CLASS (parent_class)->show (widget);
+}
+
+static void
+gtk_time_sel_class_init (GtkTimeSelClass * klass)
+{
+  GObjectClass *oclass;
+  GtkWidgetClass *widget_class;
+
+  parent_class = g_type_class_ref (gtk_hbox_get_type ());
+  oclass = (GObjectClass *) klass;
+  widget_class = (GtkWidgetClass *) klass;
+
+  widget_class->show = gtk_time_sel_show;
+}
+
+GtkType
+gtk_time_sel_get_type (void)
+{
+  static GType time_sel_type = 0;
+
+  if (! time_sel_type)
+    {
+      static const GTypeInfo time_sel_info =
+      {
+	sizeof (GtkTimeSelClass),
+	(GBaseInitFunc) NULL,
+	(GBaseFinalizeFunc) NULL,
+	(GClassInitFunc) gtk_time_sel_class_init,
+	(GClassFinalizeFunc) NULL,
+	NULL /* class_data */,
+	sizeof (GtkTimeSel),
+	0 /* n_preallocs */,
+	(GInstanceInitFunc) gtk_time_sel_init,
+      };
+
+      time_sel_type = g_type_register_static (GTK_TYPE_HBOX, "GtkTimeSel", &time_sel_info, (GTypeFlags)0);
+    }
+  return time_sel_type;
+}
+
+void
+gtk_time_sel_get_time (GtkTimeSel *sel, guint *hour, guint *minute)
+{
+  if (hour)
+    *hour = (guint) gtk_adjustment_get_value (GTK_ADJUSTMENT (sel->hour_adj));
+  if (minute)
+    *minute = (guint) gtk_adjustment_get_value (GTK_ADJUSTMENT (sel->minute_adj));
+}
+
+void
+gtk_time_set_set_time (GtkTimeSel *sel, guint hour, guint minute)
+{
+  gtk_adjustment_set_value (GTK_ADJUSTMENT (sel->hour_adj), hour);
+  gtk_adjustment_set_value (GTK_ADJUSTMENT (sel->minute_adj), minute);
+}
+
+GtkWidget *
+gtk_time_sel_new (void)
+{
+  return GTK_WIDGET (g_object_new (gtk_time_sel_get_type (), NULL));
+}
