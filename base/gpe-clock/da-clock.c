3cba15e8d9625febf79d82a5cdbb76c98b06da2c
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-03-29 19:20:33 +0000

    reinstate double-buffering. make hands thicker, darker, more opaque.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@3468 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-clock/da-clock.c
+++ base/gpe-clock/da-clock.c
@@ -20,7 +20,7 @@
 #include <X11/extensions/Xrender.h>
 #include <X11/Xft/Xft.h>
 
-static int clock_radius = 64, border = 2;
+#define CLOCK_RADIUS 64
 
 static guint x_offset, y_offset;
 
@@ -34,8 +34,11 @@ static Display *dpy;
 static XftColor color;
 
 static XftDraw *draw;
-
 static Picture image_pict, src_pict;
+static GdkPixmap *backing_pixmap;
+static GdkGC *backing_gc;
+
+static guint background_width, background_height;
 
 static XftDraw *
 make_draw (GdkDrawable *drawable)
@@ -51,10 +54,8 @@ make_draw (GdkDrawable *drawable)
 
 static void
 draw_hand (GdkDrawable *drawable, 
-	   GdkGC *gc, 
 	   double angle, 
 	   guint x_offset, guint y_offset,
-	   guint clock_radius,
 	   guint length)
 {
   GdkPoint points[5];
@@ -62,33 +63,19 @@ draw_hand (GdkDrawable *drawable,
   int i;
   double sa = sin (angle), ca = cos (angle);
 
-  dpy = GDK_WINDOW_XDISPLAY (drawable);
-
-  if (! draw)
-    draw = make_draw (drawable);
-  if (! image_pict)
-    {
-      XRenderPictureAttributes att;
-      image_pict = XftDrawPicture (draw);
-      att.poly_edge = PolyEdgeSmooth;
-      XRenderChangePicture (dpy, image_pict, CPPolyEdge, &att);
-    }
-  if (! src_pict)
-    src_pict = XftDrawSrcPicture (draw, &color);
-
-  points[0].x = 2;
+  points[0].x = 3;
   points[0].y = 0;
 
-  points[1].x = 2;
+  points[1].x = 3;
   points[1].y = length - 4;
 
   points[2].x = 0;
   points[2].y = length;
 
-  points[3].x = -2;
+  points[3].x = -3;
   points[3].y = length - 4;
 
-  points[4].x = -2;
+  points[4].x = -3;
   points[4].y = 0;
 
   for (i = 0; i < 5; i++)
@@ -103,11 +90,8 @@ draw_hand (GdkDrawable *drawable,
 
   for (i = 0; i < 5; i++)
     {
-      points[i].x += x_offset + clock_radius;
-      points[i].y += y_offset + clock_radius;
-
-      poly[i].x = points[i].x;
-      poly[i].y = points[i].y;
+      poly[i].x = points[i].x + x_offset + CLOCK_RADIUS;
+      poly[i].y = points[i].y + y_offset + CLOCK_RADIUS;
     }
 
   XRenderCompositeDoublePoly (dpy,
@@ -119,6 +103,39 @@ draw_hand (GdkDrawable *drawable,
 			      poly, 5, EvenOddRule);
 }
 
+static GdkGC *
+get_bg_gc (GdkWindow *window, GdkPixmap *pixmap)
+{
+  GdkWindowObject *private = (GdkWindowObject *)window;
+
+  guint gc_mask = 0;
+  GdkGCValues gc_values;
+
+  if (private->bg_pixmap == GDK_PARENT_RELATIVE_BG && private->parent)
+    {
+      return get_bg_gc (GDK_WINDOW (private->parent), pixmap);
+    }
+  else if (private->bg_pixmap && 
+           private->bg_pixmap != GDK_PARENT_RELATIVE_BG && 
+           private->bg_pixmap != GDK_NO_BG)
+    {
+      gc_values.fill = GDK_TILED;
+      gc_values.tile = private->bg_pixmap;
+      gc_values.ts_x_origin = 0;
+      gc_values.ts_y_origin = 0;
+      
+      gc_mask = (GDK_GC_FILL | GDK_GC_TILE | 
+                 GDK_GC_TS_X_ORIGIN | GDK_GC_TS_Y_ORIGIN);
+    }
+  else
+    {
+      gc_values.foreground = private->bg_color;
+      gc_mask = GDK_GC_FOREGROUND;
+    }
+
+  return gdk_gc_new_with_values (pixmap, &gc_values, gc_mask);
+}
+
 static gint
 draw_clock (GtkWidget *widget,
 	    GdkEventExpose *event,
@@ -126,8 +143,7 @@ draw_clock (GtkWidget *widget,
 {
   GtkDrawingArea *darea;
   GdkDrawable *drawable;
-  GdkGC *black_gc;
-  GdkGC *white_gc;
+  GdkGC *gc;
   GdkRectangle pixbuf_rect, intersect_rect;
   double hour_angle, minute_angle;
 
@@ -136,44 +152,90 @@ draw_clock (GtkWidget *widget,
 
   darea = GTK_DRAWING_AREA (widget);
   drawable = widget->window;
-  white_gc = widget->style->white_gc;
-  black_gc = widget->style->black_gc;
 
-  x_offset = (widget->allocation.width / 2) - clock_radius;
-  y_offset = (widget->allocation.height / 2) - clock_radius;
+  gc = widget->style->black_gc;
+
+  x_offset = (widget->allocation.width / 2) - CLOCK_RADIUS;
+  y_offset = (widget->allocation.height / 2) - CLOCK_RADIUS;
+
+  dpy = GDK_WINDOW_XDISPLAY (drawable);
+
+  if (! backing_pixmap)
+    {
+      XRenderPictureAttributes att;
+      GdkGC *tmp_gc;
+      backing_pixmap = gdk_pixmap_new (drawable,
+				       widget->allocation.width, widget->allocation.height,
+				       gdk_drawable_get_depth (drawable));
+      backing_gc = gdk_gc_new (backing_pixmap);
+      draw = make_draw (backing_pixmap);
+      image_pict = XftDrawPicture (draw);
+      att.poly_edge = PolyEdgeSmooth;
+      XRenderChangePicture (dpy, image_pict, CPPolyEdge, &att);
+      src_pict = XftDrawSrcPicture (draw, &color);
+
+      background_width = gdk_pixbuf_get_width (clock_background);
+      background_height = gdk_pixbuf_get_height (clock_background);
+
+      tmp_gc = get_bg_gc (drawable, backing_pixmap);
+
+      gdk_draw_rectangle (backing_pixmap, tmp_gc, TRUE,
+			  0, 0, widget->allocation.width, widget->allocation.height);
+
+      g_object_unref (tmp_gc);
+    }
 
   if (event)
     {
-      gdk_gc_set_clip_rectangle (black_gc, &event->area);
-      gdk_gc_set_clip_rectangle (white_gc, &event->area);
+      gdk_gc_set_clip_rectangle (gc, &event->area);
+      gdk_gc_set_clip_rectangle (backing_gc, &event->area);
     }
 
   if (event)
     {
       pixbuf_rect.x = x_offset;
       pixbuf_rect.y = y_offset;
-      pixbuf_rect.width = gdk_pixbuf_get_width (clock_background);
-      pixbuf_rect.height = gdk_pixbuf_get_height (clock_background);
+      pixbuf_rect.width = background_width;
+      pixbuf_rect.height = background_height;
 
       if (gdk_rectangle_intersect (&pixbuf_rect, &event->area, &intersect_rect) == TRUE)
-	gdk_pixbuf_render_to_drawable (clock_background, drawable, black_gc, intersect_rect.x - x_offset, intersect_rect.y - y_offset, intersect_rect.x, intersect_rect.y, intersect_rect.width, intersect_rect.height, GDK_RGB_DITHER_NONE, 0, 0);
+	gdk_pixbuf_render_to_drawable (clock_background, 
+				       backing_pixmap,
+				       backing_gc, 
+				       intersect_rect.x - x_offset, intersect_rect.y - y_offset, 
+				       intersect_rect.x, intersect_rect.y,
+				       intersect_rect.width, intersect_rect.height,
+				       GDK_RGB_DITHER_NONE, 0, 0);
     }
   else
-    gdk_pixbuf_render_to_drawable (clock_background, drawable, black_gc, 0, 0, x_offset, y_offset, gdk_pixbuf_get_width (clock_background), gdk_pixbuf_get_height (clock_background), GDK_RGB_DITHER_NONE, 0, 0);
+    gdk_pixbuf_render_to_drawable (clock_background, 
+				   backing_pixmap,
+				   backing_gc, 
+				   0, 0, x_offset, y_offset,
+				   gdk_pixbuf_get_width (clock_background), 
+				   gdk_pixbuf_get_height (clock_background), 
+				   GDK_RGB_DITHER_NONE, 0, 0);
  
-  gdk_pixbuf_render_to_drawable (day_night_wheel, drawable, white_gc, 0, 0, (x_offset + clock_radius) - (gdk_pixbuf_get_width (day_night_wheel) / 2), (y_offset + clock_radius) - (gdk_pixbuf_get_height (day_night_wheel) / 2), -1, -1, GDK_RGB_DITHER_NONE, 0, 0);
+  gdk_pixbuf_render_to_drawable (day_night_wheel, backing_pixmap, 
+				 backing_gc, 
+				 0, 0, (x_offset + CLOCK_RADIUS) - (gdk_pixbuf_get_width (day_night_wheel) / 2), 
+				 (y_offset + CLOCK_RADIUS) - (gdk_pixbuf_get_height (day_night_wheel) / 2), 
+				 -1, -1, GDK_RGB_DITHER_NONE, 0, 0);
 
   minute_angle = gtk_adjustment_get_value (minute_adj) * 2 * M_PI / 60;
   hour_angle = gtk_adjustment_get_value (hour_adj) * 2 * M_PI / 12;
 
-  draw_hand (drawable, black_gc, minute_angle, x_offset, y_offset, clock_radius, 
-	     7 * clock_radius / 8);
+  draw_hand (drawable, minute_angle, x_offset, y_offset,
+	     7 * CLOCK_RADIUS / 8);
 
-  draw_hand (drawable, black_gc, hour_angle, x_offset, y_offset, clock_radius, 
-	     2 * clock_radius / 3);
+  draw_hand (drawable, hour_angle, x_offset, y_offset,
+	     3 * CLOCK_RADIUS / 5);
 
-  gdk_gc_set_clip_rectangle (black_gc, NULL);
-  gdk_gc_set_clip_rectangle (white_gc, NULL);
+  gdk_draw_drawable (drawable, gc, backing_pixmap, 0, 0, 0, 0, 
+		     widget->allocation.width, widget->allocation.height);
+
+  gdk_gc_set_clip_rectangle (gc, NULL);
+  gdk_gc_set_clip_rectangle (backing_gc, NULL);
 
   return TRUE;
 }
@@ -194,8 +256,8 @@ calc_angle (int x, int y)
 static void
 button_down (GtkWidget *w, GdkEventButton *b, GtkWidget *scrolled_window)
 {
-  gint x_start = b->x - x_offset - clock_radius;
-  gint y_start = b->y - y_offset - clock_radius;
+  gint x_start = b->x - x_offset - CLOCK_RADIUS;
+  gint y_start = b->y - y_offset - CLOCK_RADIUS;
   double r = sqrt (x_start * x_start + y_start * y_start);
   double start_angle = calc_angle (x_start, y_start);
   double hour_angle = gtk_adjustment_get_value (hour_adj) * 2 * M_PI / 12;
@@ -208,7 +270,7 @@ button_down (GtkWidget *w, GdkEventButton *b, GtkWidget *scrolled_window)
   if (minute_angle < 0)
     minute_angle = -minute_angle;
 
-  if (r > (clock_radius * 2 / 3) || (minute_angle < hour_angle))
+  if (r > (CLOCK_RADIUS * 3 / 5) || (minute_angle < hour_angle))
     hand = TRUE;
   else
     hand = FALSE;
@@ -227,8 +289,8 @@ button_up (GtkWidget *w, GdkEventButton *b)
 static void
 button_drag (GtkWidget *w, GdkEventMotion *m, GtkWidget *scrolled_window)
 {
-  gint x = m->x - x_offset - clock_radius;
-  gint y = m->y - y_offset - clock_radius;
+  gint x = m->x - x_offset - CLOCK_RADIUS;
+  gint y = m->y - y_offset - CLOCK_RADIUS;
   int val;
   double angle;
 
@@ -259,10 +321,11 @@ clock_widget (GtkAdjustment *hadj, GtkAdjustment *madj)
   clock_background = gdk_pixbuf_new_from_file ("./clock.png", NULL);
   day_night_wheel = gdk_pixbuf_new_from_file ("./day-night-wheel.png", NULL);
 
-  gtk_widget_set_usize (w, clock_radius * 2 + 4, clock_radius * 2 + 4);
+  gtk_widget_set_usize (w, CLOCK_RADIUS * 2 + 4, CLOCK_RADIUS * 2 + 4);
 
   gtk_widget_add_events (GTK_WIDGET (w), GDK_BUTTON_PRESS_MASK | GDK_BUTTON_RELEASE_MASK);
 
+  g_signal_connect (G_OBJECT (w), "expose_event", G_CALLBACK (draw_clock), NULL);
   g_signal_connect (G_OBJECT (w), "button-press-event", G_CALLBACK (button_down), NULL);
   g_signal_connect (G_OBJECT (w), "button-release-event", G_CALLBACK (button_up), NULL);
   g_signal_connect (G_OBJECT (w), "motion-notify-event", G_CALLBACK (button_drag), NULL);
@@ -270,16 +333,14 @@ clock_widget (GtkAdjustment *hadj, GtkAdjustment *madj)
   hour_adj = hadj;
   minute_adj = madj;
 
-  g_signal_connect (G_OBJECT (w), "expose_event", G_CALLBACK (draw_clock), NULL);
-
   g_signal_connect (G_OBJECT (hadj), "value_changed", G_CALLBACK (adjustment_value_changed), w);
   g_signal_connect (G_OBJECT (madj), "value_changed", G_CALLBACK (adjustment_value_changed), w);
 
   gtk_widget_set_double_buffered (GTK_WIDGET (w), FALSE);
 
-  color.color.blue = 0xffff;
+  color.color.blue = 0xc000;
   color.color.red = color.color.green = 0;
-  color.color.alpha = 0x4000;
+  color.color.alpha = 0x8000;
 
   return w;
 }
