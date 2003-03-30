377f213f140f62dbe17732149d3b29633b943448
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-03-30 13:22:34 +0000

    add header file, fix warnings
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@3491 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/libschedule/atd.c
+++ base/libschedule/atd.c
@@ -1,10 +1,22 @@
+/*
+ * Copyright (C) 2003 Philip Blundell <philb@gnu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
 #include <fcntl.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <time.h>
 #include <stdio.h>
+#include <string.h>
 
 #include <glib.h>
+
+#include "gpe/schedule.h"
 
 #define ATD_BASE "/var/spool/at"
 
@@ -63,7 +75,7 @@ schedule_set_alarm (guint id, time_t start, const gchar *action)
 }
 
 gboolean
-unschedule_alarm (guint id, time_t start)
+schedule_cancel_alarm (guint id, time_t start)
 {
   gchar *filename = alarm_filename (id, start);
 
---
+++ base/libschedule/gpe/schedule.h
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2003 Philip Blundell <philb@gnu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
+#ifndef SCHEDULE_H
+#define SCHEDULE_H
+
+#include <glib.h>
+#include <time.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+extern gboolean schedule_set_alarm (guint id, time_t start, const gchar *action);
+extern gboolean schedule_cancel_alarm (guint id, time_t start);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
