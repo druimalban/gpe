1fa9c8ee9a9bcee15db206463e987b6a26175928
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-04-18 22:54:32 +0000

    first parts of rfcomm
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@3721 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/gpe-bluetooth/rfcomm.c
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2003 Philip Blundell <philb@gnu.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version
+ * 2 of the License, or (at your option) any later version.
+ */
+
+#include <sys/types.h>
+#include <stdlib.h>
+#include <time.h>
+#include <libintl.h>
+#include <locale.h>
+#include <stdio.h>
+#include <gtk/gtk.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+#include <bluetooth/sdp.h>
+#include <bluetooth/sdp_lib.h>
+#include <bluetooth/rfcomm.h>
+
+#include "main.h"
+#include "sdp.h"
+#include "lap.h"
+
+static gboolean
+create_connection (bdaddr_t *bdaddr, int channel, int *fd, GError *error)
+{
+  struct sockaddr_rc sa;
+  int sk;
+
+  sk = socket (AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
+  if (sk < 0) 
+    return FALSE;
+
+  sa.rc_family  = AF_BLUETOOTH;
+  sa.rc_channel = 0;
+  sa.rc_bdaddr  = src_addr;
+
+  if (bind (sk, (struct sockaddr *) &sa, sizeof (sa)))
+    {
+      close (sk);
+      return FALSE;
+    }
+  
+  sa.rc_channel = channel;
+  sa.rc_bdaddr  = *bdaddr;
+
+  if (connect (sk, (struct sockaddr *) &sa, sizeof (sa)))
+    {
+      close (sk);
+      return FALSE;
+    }
+
+  *fd = sk;
+
+  return TRUE;
+}
+
