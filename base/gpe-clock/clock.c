6701a4e598d7ddcadfde85ef458a04898d4478fe
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-04-20 13:26:57 +0000

    more work on alarm dialog
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@3749 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-clock/clock.c
+++ base/gpe-clock/clock.c
@@ -27,12 +27,30 @@
 #include <gpe/gpetimesel.h>
 #include <gpe/gpeclockface.h>
 
+#include <sqlite.h>
+
 GtkWidget *panel_window, *time_label;
 
 #define _(x) gettext(x)
 
 static gboolean show_seconds;
 static gboolean format_24 = TRUE;
+
+struct alarm_context
+{
+  gboolean active;
+
+  int hour, minute;
+
+  gboolean date_flag;
+  int day, month, year;
+  guint day_mask;
+
+  GtkWidget *active_button;
+  GtkWidget *date_combo, *time_sel;
+  GtkWidget *date_button, *days_button;
+  GtkWidget *day_button[7];
+};
 
 static void
 update_time (GtkWidget *label)
@@ -124,63 +142,97 @@ set_time_window (void)
 }
 
 static void
+button_toggled (GtkWidget *w, struct alarm_context *ctx)
+{
+  int i;
+  gboolean days_selected;
+  
+  ctx->active = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (ctx->active_button));
+  days_selected = gtk_toggle_button_get_active (GTK_TOGGLE_BUTTON (ctx->days_button));
+ 
+  gtk_widget_set_sensitive (ctx->time_sel, ctx->active);
+  gtk_widget_set_sensitive (ctx->date_combo, ctx->active);
+  gtk_widget_set_sensitive (ctx->date_button, ctx->active);
+  gtk_widget_set_sensitive (ctx->days_button, ctx->active);
+
+  for (i = 0; i < 7; i++)
+    gtk_widget_set_sensitive (ctx->day_button[i], ctx->active && days_selected);
+}
+
+static void
 do_set_alarm (GtkWidget *window)
 {
   gtk_widget_destroy (window);
 }
 
+static const nl_item days[] = { ABDAY_2, ABDAY_3, ABDAY_4, ABDAY_5,
+				ABDAY_6, ABDAY_7, ABDAY_1 };
+
 static void
 alarm_window (void)
 {
-  GtkWidget *window = gtk_dialog_new ();
-  GtkWidget *ok_button = gtk_button_new_from_stock (GTK_STOCK_OK);
-  GtkWidget *cancel_button = gtk_button_new_from_stock (GTK_STOCK_CANCEL);
+  GtkWidget *window;
+  GtkWidget *ok_button, *cancel_button;
   GtkWidget *time_label = gtk_label_new (_("Time:"));
-  GtkWidget *date_button, *days_button;
-  GtkWidget *date_entry = gtk_date_combo_new ();
-  GtkWidget *date_hbox = gtk_hbox_new (FALSE, 0);
-  GtkWidget *time_hbox = gtk_hbox_new (FALSE, 0);
-  GtkWidget *time_sel = gpe_time_sel_new ();
+  GtkWidget *date_hbox;
+  GtkWidget *time_hbox;
   GtkWidget *weeklytable;
-  int spacing = gpe_get_boxspacing ();
+  int spacing;
   GSList *radiogroup;
-  static const nl_item days[] = { ABDAY_2, ABDAY_3, ABDAY_4, ABDAY_5,
-                                  ABDAY_6, ABDAY_7, ABDAY_1 };
   int i;
+  struct alarm_context *ctx;
 
+  spacing = gpe_get_boxspacing ();
+  ctx = g_malloc0 (sizeof (struct alarm_context));
+
+  window = gtk_dialog_new ();
   gtk_window_set_title (GTK_WINDOW (window), _("Set alarm"));
+  gtk_container_set_border_width (GTK_CONTAINER (window), gpe_get_border ());
 
   gtk_box_set_spacing (GTK_BOX (GTK_DIALOG (window)->vbox), spacing);
   gtk_container_set_border_width (GTK_CONTAINER (GTK_DIALOG (window)->vbox), gpe_get_border ());
 
-  date_button = gtk_radio_button_new_with_label (NULL, _("Date:"));
-  radiogroup = gtk_radio_button_group (GTK_RADIO_BUTTON (date_button));
-  days_button = gtk_radio_button_new_with_label (radiogroup, _("Weekly, on:"));
+  ctx->active_button = gtk_check_button_new_with_label (_("Alarm active"));
+  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->vbox), ctx->active_button, FALSE, FALSE, 0);
+  g_signal_connect (G_OBJECT (ctx->active_button), "toggled", G_CALLBACK (button_toggled), ctx);
 
-  gtk_box_pack_start (GTK_BOX (date_hbox), date_button, FALSE, FALSE, 0);
-  gtk_box_pack_start (GTK_BOX (date_hbox), date_entry, TRUE, TRUE, 0);
+  ctx->date_button = gtk_radio_button_new_with_label (NULL, _("Date:"));
+  radiogroup = gtk_radio_button_group (GTK_RADIO_BUTTON (ctx->date_button));
+  ctx->days_button = gtk_radio_button_new_with_label (radiogroup, _("Weekly, on:"));
+  g_signal_connect (G_OBJECT (ctx->days_button), "toggled", G_CALLBACK (button_toggled), ctx);
 
+  date_hbox = gtk_hbox_new (FALSE, spacing);
+  ctx->date_combo = gtk_date_combo_new ();
+  gtk_box_pack_start (GTK_BOX (date_hbox), ctx->date_button, FALSE, FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (date_hbox), ctx->date_combo, TRUE, TRUE, 0);
+
+  time_hbox = gtk_hbox_new (FALSE, spacing);
+  ctx->time_sel = gpe_time_sel_new ();
   gtk_box_pack_start (GTK_BOX (time_hbox), time_label, FALSE, FALSE, 0);
-  gtk_box_pack_start (GTK_BOX (time_hbox), time_sel, TRUE, TRUE, 0);
+  gtk_box_pack_start (GTK_BOX (time_hbox), ctx->time_sel, TRUE, TRUE, 0);
 
   gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->vbox), time_hbox, FALSE, FALSE, 0);
   gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->vbox), date_hbox, FALSE, FALSE, 0);
-  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->vbox), days_button, FALSE, FALSE, 0);
+  gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->vbox), ctx->days_button, FALSE, FALSE, 0);
 
   weeklytable = gtk_table_new (2, 4, FALSE);
   for (i = 0; i < 4; i++)
     {
       GtkWidget *b = gtk_check_button_new_with_label (nl_langinfo (days[i]));
       gtk_table_attach_defaults (GTK_TABLE (weeklytable), b, i, i + 1, 0, 1);
+      ctx->day_button[i] = b;
     }
   for (i = 4; i < 7; i++)
     {
       GtkWidget *b = gtk_check_button_new_with_label (nl_langinfo (days[i]));
       gtk_table_attach_defaults (GTK_TABLE (weeklytable), b, i - 4, i - 3, 1, 2);
+      ctx->day_button[i] = b;
     }
   
   gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->vbox), weeklytable, FALSE, FALSE, 0);
 
+  ok_button = gtk_button_new_from_stock (GTK_STOCK_OK);
+  cancel_button = gtk_button_new_from_stock (GTK_STOCK_CANCEL);
   gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->action_area), cancel_button, FALSE, FALSE, 0);
   gtk_box_pack_start (GTK_BOX (GTK_DIALOG (window)->action_area), ok_button, FALSE, FALSE, 0);
 
@@ -188,6 +240,8 @@ alarm_window (void)
 
   g_signal_connect_swapped (G_OBJECT (ok_button), "clicked", G_CALLBACK (do_set_alarm), window);
   g_signal_connect_swapped (G_OBJECT (cancel_button), "clicked", G_CALLBACK (gtk_widget_destroy), window);
+
+  button_toggled (NULL, ctx);
 }
 
 static void
