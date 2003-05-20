dc602288f15e11b01e722f2ac07c06d9f609297c
keithp <keithp@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-05-20 21:09:30 +0000

    Fix up gpe-bootsplash to work on any 1 or 4565 screen
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@3889 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-bootsplash/splash.c
+++ base/gpe-bootsplash/splash.c
@@ -12,6 +12,7 @@
 #include <sys/mman.h>
 #include <fcntl.h>
 #include <linux/fb.h>
+#include <stdio.h>
 
 #define FB "/dev/fb/0"
 #define IMAGE "/usr/share/gpe/splash.png"
@@ -20,19 +21,32 @@
 
 static char *cursoff = "\033[?25l\000";
 
+struct fb_fix_screeninfo fix;
+struct fb_var_screeninfo var;
+int xsize, ysize, stride;
+
+gboolean flip = FALSE;
+gboolean rot;
+gboolean landscape_fb, landscape_image;
+gboolean depth16;
+gboolean mono;
+
 int 
 main(int argc, char *argv[])
 {
   GdkPixbuf *buf;
   guchar *pix;
   int fd;
-  gushort *fb;
+  guchar *fb;
+  guchar *dest, *dest_line;
   int x, y;
-  size_t stride;
+  int fb_x, fb_y;
+  int xoff, yoff;
+  int ostride;
   struct fb_cursorstate curs;
   int tty;
-  int xsize = 240, ysize = 320;
-  gboolean flip = FALSE;
+  int mat[2][3];
+  int i;
 
 #ifdef GTK2
   g_type_init ();
@@ -44,12 +58,6 @@ main(int argc, char *argv[])
   if (buf == NULL)
     exit (1);
 
-  if ((argc > 1) && !strcmp(argv[1], "--flip"))
-    {
-      flip = TRUE;
-    }
-
-  pix = gdk_pixbuf_get_pixels (buf);
   fd = open (FB, O_RDWR);
   if (fd < 0)
     {
@@ -62,43 +70,167 @@ main(int argc, char *argv[])
       perror ("mmap");
       exit (1);
     }
+  
+  if (ioctl (fd, FBIOGET_FSCREENINFO, &fix) < 0)
+    {
+      perror ("FBIOGET_FSCREENINFO");
+      exit (1);
+    }
+  
+  if (ioctl (fd, FBIOGET_VSCREENINFO, &var) < 0)
+    {
+      perror ("FBIOGET_VSCREENINFO");
+      exit (1);
+    }
+  /* XXX handle only a few fb formats */
+  if (fix.visual == FB_VISUAL_TRUECOLOR &&
+      var.bits_per_pixel == 16 &&
+      var.red.offset == 11 && 
+      var.green.offset == 5 &&
+      var.blue.offset == 0)
+  {
+    depth16 = TRUE;
+    mono = FALSE;
+  }
+  else
+  if (fix.visual == FB_VISUAL_MONO10 && 
+      var.bits_per_pixel == 1)
+  {
+    depth16 = FALSE;
+    mono = TRUE;
+  }
+  else
+  {
+    fprintf (stderr, "%s: frame buffer neither 565 nor monochrome\n", argv[0]);
+    exit (1);
+  }
 
   tty = open ("/dev/tty0", O_RDWR);
   if (tty < 0)
-    perror (open);
+    perror ("open");
   else
     {
       write (tty, cursoff, strlen (cursoff));
       close (tty);
     }
 
+  pix = gdk_pixbuf_get_pixels (buf);
   stride = gdk_pixbuf_get_rowstride (buf);
+  xsize = gdk_pixbuf_get_width (buf);
+  ysize = gdk_pixbuf_get_height(buf);
+  
+  landscape_fb = var.xres >= var.yres;
+  landscape_image = xsize >= ysize;
+  rot = landscape_fb != landscape_image;
 
-  for (y = 0; y < ysize; y++)
+  for (i = 1; i < argc; i++)
     {
-      guchar *ppix = pix;
-      for (x = 0; x < xsize; x++)
-	{
-	  guchar red, green, blue;
-	  gushort w;
-	  red = *(ppix++);
-	  green = *(ppix++);
-	  blue = *(ppix++);
-
-	  red >>= 3;
-	  green >>= 2;
-	  blue >>= 3;
-	  w = blue | (green << 5) | (red << 11);
-
-	  if (flip)
-	    {
-	      fb[(y) + (ysize * ((xsize - 1) - x))] = w;
-	    }
-	  else
-	    {
-	      fb[((ysize - 1) - y) + (ysize * x)] = w;
- 	    }
-	}
-      pix += stride;
+      /* can't autodetect this */
+      if (!strcmp(argv[i], "--flip"))
+	flip = TRUE;
+      else if (!strcmp (argv[i], "--rot"))
+	rot = !rot;
+      else if (!strcmp (argv[i], "--mono"))
+	mono = TRUE;
+      else if (!strcmp (argv[i], "--depth1"))
+	depth16 = FALSE;
+      else if (!strcmp (argv[i], "--depth16"))
+	depth16 = TRUE;
     }
+
+  if (rot)
+  {
+    xoff = ((int) var.yres - xsize) / 2;
+    yoff = ((int) var.xres - ysize) / 2;
+    if (flip)
+    {
+      mat[0][0] =  0; mat[0][1] =  1; mat[0][2] = -xoff;
+      mat[1][0] = -1; mat[1][1] =  0; mat[1][2] = ysize - 1 + yoff;
+    }
+    else
+    {
+      mat[0][0] = 0; mat[0][1] = -1; mat[0][2] = xsize - 1 + xoff;
+      mat[1][0] = 1; mat[1][1] =  0; mat[1][2] = -yoff;
+    }
+  }
+  else
+  {
+    xoff = ((int) var.xres - xsize) / 2;
+    yoff = ((int) var.yres - ysize) / 2;
+    if (flip)
+    {
+      mat[0][0] = -1; mat[0][1] =  0; mat[0][2] = xsize - 1 + xoff;
+      mat[1][0] =  0; mat[1][1] = -1; mat[1][2] = ysize - 1 + yoff;
+    }
+    else
+    {
+      mat[0][0] = 1; mat[0][1] = 0; mat[0][2] = - xoff;
+      mat[1][0] = 0; mat[1][1] = 1; mat[1][2] = - yoff;
+    }
+  }
+
+  dest_line = fb;
+  for (fb_y = 0; fb_y < var.yres; fb_y++)
+  {
+    guchar  monobits = 0;
+    dest = dest_line;
+    dest_line += fix.line_length;
+    for (fb_x = 0; fb_x < var.xres; fb_x++)
+    {
+      guchar  *ppix;
+      guchar  red, green, blue;
+      x = mat[0][0] * fb_x + mat[0][1] * fb_y + mat[0][2];
+      y = mat[1][0] * fb_x + mat[1][1] * fb_y + mat[1][2];
+      if (0 <= x && x < xsize && 0 <= y && y < ysize)
+      {
+	ppix = pix + y * stride + x * 3;
+      }
+      else
+	ppix = pix; /* fill with pixel from upper left */
+      red = ppix[0]; green = ppix[1]; blue = ppix[2];
+      if (mono)
+      {
+	int	brightness;
+	int d, i;
+
+#define DITHER_DIM  4
+	static int orderedDither[4][4] = {
+	  {  1,  9,  3, 11 },
+	  { 13,  5, 15,  7 },
+	  {  4, 12,  2, 10 },
+	  { 16,  8, 14,  6 }
+	};
+#define DITHER_SIZE  ((sizeof orderedDither / sizeof orderedDither[0][0]) + 1)
+
+	/* use a simple ordered dither */
+	brightness = (red * 153 + green * 301 + blue * 58) >> 9;
+	d = orderedDither[fb_x&(DITHER_DIM-1)][fb_y&(DITHER_DIM-1)];
+	i = (brightness * DITHER_SIZE + 127) / 255;
+	if (depth16)
+	{
+	  if (i > d)
+	    *((gushort *) dest) = 0xffff;
+	  else
+	    *((gushort *) dest) = 0x0000;
+	  dest += 2;
+	}
+	else
+	{
+	  int bit = fb_x & 7;
+	  if (i > d)
+	    monobits |= 1 << bit;
+	  if (bit == 7)
+	  {
+	    *dest++ = monobits;
+	    monobits = 0;
+	  }
+	}
+      }
+      else
+      {
+	*(gushort *) dest = ((red >> 3) << 11) | ((green >> 2) << 5) | (blue >> 3);
+	dest += 2;
+      }
+    }
+  }
 }
