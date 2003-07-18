bc8fe1b4845d6498a98ba00ee68db78cde612f9d
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-07-18 09:36:14 +0000

    working on php
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@4211 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ html/includes/foot.php
@@ -0,0 +1,11 @@
+<HR>
+<div class="footer">
+<SMALL>
+Last modified: <?php echo $LAST_MODIFIED; ?>
+Page maintained by <?php echo $author ?> <a href='mailto:<?php echo
+	  $authoremail; ?>'>&lt;<?php echo
+	  $authoremail; ?>&gt;</a>.
+</SMALL>
+</div>
+</body> 
+</html>
---
+++ html/includes/head.php
@@ -0,0 +1,28 @@
+<?php
+  if (empty($author)) {
+    $author = "GPE Hackers";
+    $authoremail = "gpe@handhelds.org";
+  }
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<HTML> 
+<HEAD> 
+<TITLE> GPE: <?php echo $addtitle; ></TITLE> 
+<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<LINK REL="stylesheet" HREF="/css/style.css" TYPE="text/css">
+<LINK REL="shortcut icon" HREF="/images/favicon.ico" TYPE="image/x-icon">
+</HEAD>
+<BODY>
+<table border=0>
+<tr>
+<td width=70>
+<img src="/images/gpe-logo.png" alt="[The GPE Logo]">
+</td>
+<td width="*" valign="middle">
+<h1> GPE: <?php echo $addtitle; ?></h1>
+</td> 
+</tr>
+</table>
+<!-- #include file="menu.shtml"-->
+<hr>
