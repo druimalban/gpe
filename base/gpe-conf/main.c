cb65b7bbe22ce382e8e6d07144accd490ab0fea4
florian <florian@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2003-08-27 22:59:51 +0000

    Changed applet name
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@4443 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-conf/main.c
+++ base/gpe-conf/main.c
@@ -100,7 +100,7 @@ struct Applet applets[]=
     { &GpeAdmin_Build_Objects, &GpeAdmin_Free_Objects, &GpeAdmin_Save, &GpeAdmin_Restore , "GPE" ,"admin","GPE Conf Administration"},
     { &Storage_Build_Objects, &Storage_Free_Objects, &Unimplemented_Save, &Storage_Restore , "Storage" ,"storage","Storage Information"},
     { &Serial_Build_Objects, &Serial_Free_Objects, &Serial_Save, &Serial_Restore , "Serial" ,"serial","Serial Port Configuration"},
-    { &Logread_Build_Objects, &Logread_Free_Objects, &Unimplemented_Save, &Logread_Restore , "Logread" ,"logread", "Show logfile"},
+    { &Logread_Build_Objects, &Logread_Free_Objects, &Unimplemented_Save, &Logread_Restore , "Logread" ,"logread", "Logfile Viewer"},
     { &Cardinfo_Build_Objects, &Cardinfo_Free_Objects, &Unimplemented_Save, &Cardinfo_Restore , "Cardinfo" ,"cardinfo","PC/CF Card Info and Config"},
     { &Packages_Build_Objects, &Packages_Free_Objects, &Unimplemented_Save, &Packages_Restore , "Packages" ,"packages","Add and Remove packages"},
     { &Battery_Build_Objects, &Battery_Free_Objects, &Unimplemented_Save, &Battery_Restore , "Battery" ,"battery","Battery info"},
