a1af193908f70515b3cded82a902a97c53ec903c
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2004-02-12 21:37:36 +0000

    implement calendar import
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@5140 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-multisync/calendar.c
+++ base/gpe-multisync/calendar.c
@@ -16,10 +16,46 @@
 
 #include "gpe_sync.h"
 
+#include <gpe/vevent.h>
+#include <gpe/tag-db.h>
+
+#include <mimedir/mimedir-vcal.h>
+
 GList *
 sync_calendar (GList *data, gpe_conn *conn, int newdb)
 {
-  fetch_uid_list (conn->calendar, "select distinct uid from calendar");
+  GSList *list, *i;
+  
+  list = fetch_uid_list (conn->calendar, "select distinct uid from calendar_urn");
+
+  for (i = list; i; i = i->next)
+    {
+      GSList *tags;
+      MIMEDirVCal *vcal;
+      MIMEDirVEvent *vevent;
+      gchar *string;
+      changed_object *obj;
+      int urn = (int)i->data;
+      
+      tags = fetch_tag_data (conn->calendar, "select tag,value from calendar where uid=%d", urn);
+      vevent = vevent_from_tags (tags);
+      gpe_tag_list_free (tags);
+      vcal = mimedir_vcal_new ();
+      mimedir_vcal_add_component (vcal, MIMEDIR_VCOMPONENT (vevent));
+      string = mimedir_vcal_write_to_string (vcal);
+      g_object_unref (vevent);
+      g_object_unref (vcal);
+
+      obj = g_malloc0 (sizeof (*obj));
+      obj->comp = string;
+      obj->uid = g_strdup_printf ("%d", urn);
+      obj->object_type = SYNC_OBJECT_TYPE_CALENDAR;
+      obj->change_type = SYNC_OBJ_MODIFIED; 
+
+      data = g_list_append (data, obj);
+    }
+
+  g_slist_free (list);
 
   return data;
 }
