b9c34ff96f5a02e033bb16efd0793dd9bdc5a80b
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2004-02-29 21:47:07 +0000

    2004-02-29  Phil Blundell  <pb@nexus.co.uk>
    	* todo-db.c (item_data_callback): Fix cut and paste error.
    
    
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@5391 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/libtododb/todo-db.c
+++ base/libtododb/todo-db.c
@@ -52,7 +52,7 @@ item_data_callback (void *arg, int argc, char **argv, char **names)
       else if (!strcasecmp (argv[0], "STATE"))
 	i->state = atoi (argv[1]);
       else if (!strcasecmp (argv[0], "PRIORITY"))
-	i->state = atoi (argv[1]);
+	i->priority = atoi (argv[1]);
       else if (!strcasecmp (argv[0], "CATEGORY"))
 	i->categories = g_slist_append (i->categories, (gpointer)atoi (argv[1]));
       else if (!strcasecmp (argv[0], "DUE"))
