9cee2f2f9ee26557350586a651c5dc112e1a15f7
florian <florian@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2004-06-18 11:45:12 +0000

    Fixed missing libraries (wrong name). Hack to masq out GDK warnings from beeing reported.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@5858 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-su/gpe-su.c
+++ base/gpe-su/gpe-su.c
@@ -112,7 +112,8 @@ do_su (void)
     }
   *rp = 0;
 
-  if (strncmp (result, "Password:", 9))
+  if ((strncmp (result, "Password:", 9))
+	  && (!strstr(result, "Gdk-WARNING")))
     {
       gpe_error_box (result);
       kill (pid, 15);
