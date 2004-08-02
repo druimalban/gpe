fd292506a27301d645309b105f8ded1fdf5508ce
pigeon <pigeon@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2004-08-02 12:21:06 +0000

    A bit more macros and comments
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@6099 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-irc/irc_parse.h
+++ base/gpe-irc/irc_parse.h
@@ -3,10 +3,16 @@
 
 #include "irc.h"
 
+/* This simply does a pointer increment, use it carefully */
 #define STRIP_COLON(x)		if(x[0] == ':') x++
+
+#define IS_CHANNEL(x)		(x[0] == '#' ? TRUE : FALSE)
+
+
 
 gboolean irc_server_parse(IRCServer *server, gchar *line);
 
+/* Returns a nick from a full prefix, free required */
 gchar *irc_prefix_to_nick(gchar *prefix);
 
 #endif
