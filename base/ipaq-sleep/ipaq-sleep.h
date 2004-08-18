c39eea5f745f3a5f6db3eac46545a4542ae9976f
florian <florian@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2004-08-18 08:19:14 +0000

    Released V 0.8. Added new default to config file. Added definitions to work with more different APM implementations.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@6192 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/ipaq-sleep/ipaq-sleep.h
+++ base/ipaq-sleep/ipaq-sleep.h
@@ -22,3 +22,52 @@
 #define bflag	"low_battery"
 
 const char *sleep_command="/usr/bin/apm --suspend";
+
+/* APM stuff */
+
+/* We need to define some of these headers because seemingly
+ * they aren't defined in every apm. Very mysterious
+  */
+  
+  #ifndef AC_LINE_STATUS_OFF
+  #define AC_LINE_STATUS_OFF      (0)
+  #endif
+  
+  #ifndef AC_LINE_STATUS_ON
+  #define AC_LINE_STATUS_ON       (1)
+  #endif
+  
+  #ifndef BATTERY_FLAGS_CHARGING
+  #define BATTERY_FLAGS_CHARGING  (0x8)
+  #endif
+  
+  #ifndef BATTERY_FLAGS_ABSENT
+  #define BATTERY_FLAGS_ABSENT    (0x80)
+  #endif
+  
+  #ifndef BATTERY_PERCENTAGE_UNKNOWN
+  #define BATTERY_PERCENTAGE_UNKNOWN  (-1)
+  #endif
+  
+  #ifndef BATTERY_TIME_UNKNOWN
+  #define BATTERY_TIME_UNKNOWN        (-1)
+  #endif
+  
+  /* These are the headers that are in apm.h - define above 
+   * as we need them.
+   *
+   * #define AC_LINE_STATUS_OFF      (0)
+   * #define AC_LINE_STATUS_ON       (1)
+   * #define AC_LINE_STATUS_BACKUP   (2)
+   * #define AC_LINE_STATUS_UNKNOWN  (0xff)
+   * #define BATTERY_STATUS_HIGH     (0)
+   * #define BATTERY_STATUS_LOW      (1)
+   * #define BATTERY_STATUS_CRITICAL (2)
+   * #define BATTERY_STATUS_ABSENT   (4) 
+   * #define BATTERY_STATUS_UNKNOWN  (0xff) 
+   * #define BATTERY_FLAGS_HIGH      (0x1)
+   * #define BATTERY_FLAGS_LOW       (0x2)
+   * #define BATTERY_FLAGS_CRITICAL  (0x4)
+   * #define BATTERY_FLAGS_ABSENT    (0x80)
+   */
+                  
