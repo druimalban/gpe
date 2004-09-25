80ffa163f34bf575f4d67b7dd3512eda988fd0a0
cmarqu <cmarqu@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2004-09-25 14:33:06 +0000

    Use favicon.png.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@6451 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- html/includes/head.php
+++ html/includes/head.php
@@ -11,7 +11,7 @@
 <TITLE> GPE: <?php echo $addtitle; ?></TITLE> 
 <META http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <LINK REL="stylesheet" HREF="/css/style.css" TYPE="text/css">
-<LINK REL="shortcut icon" HREF="/images/favicon.ico" TYPE="image/x-icon">
+<LINK REL="shortcut icon" HREF="/images/favicon.png" TYPE="image/png">
 </HEAD>
 <BODY>
 <table border=0 width="100%">
