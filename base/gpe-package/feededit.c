8254a15fe7d4d7312a9798df929e1cebce069fec
florian <florian@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2004-10-19 12:46:31 +0000

    Support for writing new feed configuration. Finished feed editor. Fixed several bugs, changed file location.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@6584 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-package/feededit.c
+++ base/gpe-package/feededit.c
@@ -26,12 +26,132 @@
 
 #include <gtk/gtk.h>
 #include <gpe/spacing.h>
+#include <gpe/errorbox.h>
+
 #include "feededit.h"
 
-#define IPKG_FEEDCONFIG "/tmp/feeds.conf"
+#define IPKG_FEEDCONFIG "/etc/ipkg/feeds.conf"
 
+#warning todo: check editable(view, del, edit), check valid
 extern GtkWidget *fMain;
 static GtkListStore *feedstore;
+
+gboolean 
+write_feed(GtkTreeModel *model, GtkTreePath *path,
+           GtkTreeIter *iter,
+           gpointer data)
+{
+	FILE *fp = data;
+	gchar *name = NULL, *url = NULL;
+	gint type;
+
+	gtk_tree_model_get (GTK_TREE_MODEL(model), iter, 
+	                    FEED_NAME, &name, 
+	                    FEED_URL, &url,
+                        FEED_TYPE, &type, -1);
+	if (type != FT_PROTECTED)
+	{
+		if (type == FT_UNCOMPRESED)
+			fprintf(fp, "src \t%s \t%s\n", name, url);
+		else
+			fprintf(fp, "src/gz \t%s \t%s\n", name, url);
+		g_free(name);
+		g_free(url);
+	}
+	return FALSE;	
+}
+	
+static void
+write_feeds_to_file(const gchar *filename)
+{
+	FILE *fp = fopen(filename, "w");
+	
+	if (fp != NULL)
+	{
+		gtk_tree_model_foreach(GTK_TREE_MODEL(feedstore), write_feed, fp);
+		fclose(fp);
+	}
+	else
+	{
+		gchar *err = g_strdup_printf("%s %s %s", _("Could not open"),
+		                             filename, _("for writing."));
+		gpe_error_box(err);
+		g_free(err);
+	}
+}
+
+static void
+edit_feed(GtkTreeIter *iter)
+{
+	GtkWidget *dialog, *btn;
+	GtkWidget *table = gtk_table_new(2, 3, FALSE);
+	GtkWidget *cbCompressed;
+	GtkWidget *label;
+	GtkWidget *eName, *eURL;
+	gchar *name = NULL, *url = NULL;
+	gint type = FT_COMPRESSED;
+	
+	gtk_table_set_col_spacings(GTK_TABLE(table), gpe_get_boxspacing());
+	gtk_table_set_row_spacings(GTK_TABLE(table), gpe_get_boxspacing());
+	
+	dialog = gtk_dialog_new_with_buttons (_("Feed Properties"),
+	             GTK_WINDOW (fMain),
+				 GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_MODAL,
+				 GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL, NULL);
+	btn = gtk_dialog_add_button(GTK_DIALOG(dialog), GTK_STOCK_OK, 
+	                            GTK_RESPONSE_ACCEPT);
+	GTK_WIDGET_SET_FLAGS(btn, GTK_CAN_DEFAULT);
+	gtk_widget_grab_default(btn);
+	
+	label = gtk_label_new(_("Name"));
+	gtk_misc_set_alignment(GTK_MISC(label), 1, 0.5);
+	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 0, 1, GTK_FILL, GTK_FILL, 0, 0);
+	eName = gtk_entry_new_with_max_length(64);
+	gtk_table_attach(GTK_TABLE(table), eName, 1, 2, 0, 1, GTK_FILL, GTK_FILL, 0, 0);
+	label = gtk_label_new(_("Location (URL)"));
+	gtk_misc_set_alignment(GTK_MISC(label), 1, 0.5);
+	gtk_table_attach(GTK_TABLE(table), label, 0, 1, 1, 2, GTK_FILL, GTK_FILL, 0, 0);
+	eURL = gtk_entry_new_with_max_length(255);
+	gtk_table_attach(GTK_TABLE(table), eURL, 1, 2, 1, 2, GTK_FILL, GTK_FILL, 0, 0);
+	cbCompressed = gtk_check_button_new_with_label(_("Compressed"));
+	gtk_table_attach(GTK_TABLE(table), cbCompressed, 0, 2, 2, 3, GTK_FILL, GTK_FILL, 0, 0);
+	gtk_tree_model_get(GTK_TREE_MODEL(feedstore), iter, 
+	                   FEED_NAME, &name, 
+					   FEED_URL, &url,
+                       FEED_TYPE, &type, -1);
+    if (name) 
+	{
+		gtk_entry_set_text(GTK_ENTRY(eName), name);
+		g_free(name);
+	}
+    if (url) 
+	{
+		gtk_entry_set_text(GTK_ENTRY(eURL), url);
+		g_free(url);
+	}
+	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(cbCompressed), 
+	                             type == FT_COMPRESSED);
+	gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), table, FALSE, TRUE, 0);
+	gtk_widget_show_all(dialog);
+	if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) 
+	{
+		name = gtk_editable_get_chars(GTK_EDITABLE(eName), 0, -1);
+		url = gtk_editable_get_chars(GTK_EDITABLE(eURL), 0, -1);
+		if (gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(cbCompressed)))
+			type = FT_COMPRESSED;
+		else
+			type = FT_UNCOMPRESED;
+			                             
+		gtk_list_store_set(feedstore, iter, 
+		                   FEED_NAME, name, 
+		                   FEED_URL, url,
+		                   FEED_TYPE, type, -1);
+		g_free(name);
+		g_free(url);
+		write_feeds_to_file(IPKG_FEEDCONFIG);
+	}
+	gtk_widget_destroy(dialog);
+}
 
 static void
 read_feeds_from_file(const gchar *filename)
@@ -68,7 +188,11 @@ read_feeds_from_file(const gchar *filename)
 void
 on_add_feed(GtkWidget *button, gpointer data)
 {
+	GtkTreeIter iter;
 	
+	gtk_list_store_append(feedstore, &iter);
+	gtk_list_store_set(feedstore, &iter, FEED_TYPE, FT_COMPRESSED, -1);
+	edit_feed(&iter);
 }
 
 void
@@ -85,13 +209,14 @@ on_remove_feed(GtkWidget *button, gpointer data)
 		                               (GtkTreeModel**)(&feedstore), &iter)) 
 	{
 		dialog = gtk_message_dialog_new (GTK_WINDOW (fMain),
-					 GTK_DIALOG_DESTROY_WITH_PARENT,
+					 GTK_DIALOG_DESTROY_WITH_PARENT | GTK_DIALOG_MODAL,
 					 GTK_MESSAGE_QUESTION,
 					 GTK_BUTTONS_YES_NO,
 					 _("Delete selected feed?"));
 		if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_YES) 
 		{
 			gtk_list_store_remove(feedstore , &iter);
+			write_feeds_to_file(IPKG_FEEDCONFIG);
 		}
 		gtk_widget_destroy(dialog);
 	}
@@ -100,7 +225,17 @@ on_remove_feed(GtkWidget *button, gpointer data)
 void
 on_edit_feed(GtkWidget *button, gpointer data)
 {
+	GtkTreeIter iter;
+	GtkTreeSelection *sel;
+	GtkWidget *treeview = data;
 	
+	sel = gtk_tree_view_get_selection(GTK_TREE_VIEW(treeview));
+	
+	if (gtk_tree_selection_get_selected(sel, 
+		                               (GtkTreeModel**)(&feedstore), &iter)) 
+	{
+		edit_feed(&iter);
+	}
 }
 
 GtkWidget *
