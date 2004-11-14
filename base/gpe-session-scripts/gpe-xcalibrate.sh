be55c4a1e44ac1f9f3079cb448c4a9dca224c476
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2004-11-14 19:33:04 +0000

    Remove legacy cruft; just call xtscal.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@6669 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-session-scripts/gpe-xcalibrate.sh
+++ base/gpe-session-scripts/gpe-xcalibrate.sh
@@ -1,13 +1,3 @@
 #! /bin/sh
-if test -x /usr/X11R6/bin/xcalibrate; then
-    # don't look at xcalibrate's exit code, it always returns 1:
-    /usr/X11R6/bin/xcalibrate;
-    /usr/X11R6/bin/xcalibrate -view >/etc/xcalibrate.conf;
 
-    ## Beginning with xcalibrate 0.2-2, we could use the line below,
-    ## but it's maybe too dangerous until ipkg supports versioned dependencies:    
-    # /usr/X11R6/bin/xcalibrate && /usr/X11R6/bin/xcalibrate -view >/etc/xcalibrate.conf;
-fi
-if test -x /usr/bin/xtscal; then
-    xtscal >/etc/xcalibrate.conf
-fi
+exec xtscal
