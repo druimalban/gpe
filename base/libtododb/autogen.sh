aa6814b3e21e1bc89b9a65f5528230182224a9c4
florian <florian@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2005-06-03 23:14:21 +0000

    Merge in support for maemo debian packages. Support files for package creation. autogen.sh: Don't call configure here.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@7182 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/libtododb/autogen.sh
+++ base/libtododb/autogen.sh
@@ -4,9 +4,3 @@ aclocal $ACLOCAL_FLAGS
 
 automake -a $am_opt
 autoconf
-./configure --enable-maintainer-mode "$@"
-
-echo "Fixing libtool..."
-patch -p0 < libtool-cross.patch
-
-echo "Now type 'make' to compile"
