06fc44f1e3d745f7036d2289e3282754eae60961
martin <martin@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2005-08-02 10:26:52 +0000

    No real changes, but cvs wants them to be listed.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@7351 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-opensync/src/gpe_xml.c
+++ base/gpe-opensync/src/gpe_xml.c
@@ -1,29 +1,5 @@
 #include "gpe_sync.h"
 
-/* This function converts a IP adress, given in a string
- *  * into an array of char. Should be secure.
- *   */
-void parse_ip(char *ip_str, char *ip_char)
-{
-	int len = strlen(ip_str);
-	int i=0, i2=0, bi=0;
-	char buf[3]="   ";
-
-	for(i=0; (i<len) && (i2 < 4); i++) {
-		if( (ip_str[i] == '.') || (bi > 3) ) {
-			ip_char[i2]=atoi(buf);
-			i2++;
-			bi=0;
-			strcpy(buf,"   ");
-		}
-		else {
-			buf[bi]=ip_str[i];
-			bi++;
-		}
-	}
-	ip_char[i2]=atoi(buf);
-}
-
 /*This just opens the config file and returns the settings in the env */
 osync_bool gpe_parse_settings(gpe_environment *env, char *data, int size)
 {
@@ -33,13 +9,11 @@ osync_bool gpe_parse_settings(gpe_environment *env, char *data, int size)
 	osync_debug("GPE-SYNC", 4, "start: %s", __func__);
 
 	// Set the defaults
-	env->ip[0]=127;
-	env->ip[1]=0;
-	env->ip[2]=0;
-	env->ip[3]=1;
+	env->device_addr = (char*)malloc(sizeof(char)*10);
+	strcpy(env->device_addr, "127.0.0.1");
 	env->port=6666;
-	env->user = (char*)malloc(sizeof(char)*9);
-	strcpy(env->user, "gpeuser");
+	env->username = (char*)malloc(sizeof(char)*9);
+	strcpy(env->username, "gpeuser");
 	
 	doc = xmlParseMemory(data, size);
 	
@@ -68,10 +42,10 @@ osync_bool gpe_parse_settings(gpe_environment *env, char *data, int size)
 		if (str) {
 			if (!xmlStrcmp(cur->name, (const xmlChar *)"handheld_ip")) {
 				// convert the string to an ip
-				parse_ip(str, env->ip);
+				env->device_addr = g_strdup(str);
 			}
 			if (!xmlStrcmp(cur->name, (const xmlChar *)"handheld_user")) {
-				env->user = g_strdup(str);
+				env->username = g_strdup(str);
 			}
 			if (!xmlStrcmp(cur->name, (const xmlChar *)"handheld_port")) {
 				// convert string to an int
