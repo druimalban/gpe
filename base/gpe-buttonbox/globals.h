4606d2e350d86febf1f8b393561e507ef4588464
florian <florian@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2005-08-17 16:25:09 +0000

    Add missing file.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@7426 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/gpe-buttonbox/globals.h
@@ -0,0 +1,16 @@
+#define _NET_WM_WINDOW_TYPE_DOCK 0
+#define _NET_WM_WINDOW_TYPE 1
+#define _NET_WM_STATE_HIDDEN 2
+#define _NET_WM_SKIP_PAGER 3
+#define _NET_WM_WINDOW_TYPE_DESKTOP 4
+#define _PSION_DESKTOP_SWIZZLE 5
+#define _NET_ACTIVE_WINDOW 6
+#define _NET_SHOWING_DESKTOP 7
+#define _NET_SYSTEM_TRAY_OPCODE 8
+#define _NET_SYSTEM_TRAY_MESSAGE_DATA 9
+#define MANAGER 10
+#define _NET_SYSTEM_TRAY_Sn 11
+
+extern Atom atoms[];
+extern Display *dpy;
+extern GtkWidget *window;
