7c5b222efeb774e2ff4dbcc13ffed14dfccdb358
martin <martin@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2005-08-31 15:18:46 +0000

    Removed the add_/remove_/modify_item functions. They are implemented in contact.c, calendar.c and todo.c
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@7561 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-opensync/src/utils.c
+++ base/gpe-opensync/src/utils.c
@@ -1,86 +1,9 @@
 #include "gpe_sync.h"
 
-osync_bool add_item (OSyncContext *ctx, const char *type, const char *data, gchar **error)
-{
-        gpe_environment *env = (gpe_environment *)osync_context_get_plugin_data(ctx);
-	osync_debug("GPE_SYNC", 2, "Adding item:\n%s", data);
-	gchar *result = NULL;
-	osync_bool state = FALSE;
-	gchar vtype[7] = "";
-
-	if (!strcmp (type, "contact"))
-		sprintf (vtype, "vcard");
-	else if (!strcmp (type, "event"))
-		sprintf (vtype, "vevent");
-	else if (!strcmp (type, "todo"))
-		sprintf (vtype, "vtodo");
-			
-	gpesync_client_exec_printf (env->client, "add %s %s", client_callback_string, &result, error, vtype, data);
-
-	if (!strcasecmp (result, "OK\n"))
-		state = TRUE;
-
-	if (result)
-		g_free (result);
-
-	return state;
-}
-
-osync_bool modify_item (OSyncContext *ctx, const char *type, unsigned int uid, const char *data, gchar **error)
-{
-        gpe_environment *env = (gpe_environment *)osync_context_get_plugin_data(ctx);
-	osync_debug("GPE_SYNC", 2, "Modifying item %d:\n%s", uid, data);
-	gchar *result = NULL;
-	osync_bool state = FALSE;
-	gchar vtype[7] = "";
-
-	if (!strcmp (type, "contact"))
-		sprintf (vtype, "vcard");
-	else if (!strcmp (type, "event"))
-		sprintf (vtype, "vevent");
-	else if (!strcmp (type, "todo"))
-		sprintf (vtype, "vtodo");
-			
-	gpesync_client_exec_printf (env->client, "modify %s %d %s", client_callback_string, &result, error, vtype, uid, data);
-
-	if (!strcasecmp (result, "OK\n"))
-		state = TRUE;
-
-	if (result)
-		g_free (result);
-
-	return state;
-}
-osync_bool delete_item (OSyncContext *ctx, const char *type, unsigned int uid, gchar **error)
-{
-        gpe_environment *env = (gpe_environment *)osync_context_get_plugin_data(ctx);
-	osync_debug("GPE_SYNC", 2, "Deleting item %d", uid);
-	gchar *result = NULL;
-	osync_bool state = FALSE;
-	gchar vtype[7] = "";
-
-	if (!strcmp (type, "contact"))
-		sprintf (vtype, "vcard");
-	else if (!strcmp (type, "event"))
-		sprintf (vtype, "vevent");
-	else if (!strcmp (type, "todo"))
-		sprintf (vtype, "vtodo");
-			
-	gpesync_client_exec_printf (env->client, "del %s %d", client_callback_string, &result, error, vtype, uid);
-
-	if (!strcasecmp (result, "OK\n"))
-		state = TRUE;
-
-	if (result)
-		g_free (result);
-
-	return state;
-}
-
-osync_bool parse_uid_modified (gchar *string, gchar **uid, gchar **modified)
+osync_bool parse_value_modified (gchar *string, gchar **value, gchar **modified)
 {
 	gchar *c = NULL;
-	*uid = string;
+	*value = string;
 	*modified = (strchr (string, ':'));
 	if (*modified == NULL)
 		return FALSE;
@@ -90,7 +13,7 @@ osync_bool parse_uid_modified (gchar *string, gchar **uid, gchar **modified)
 	if (c)
 		c[0] = '\0';
 	
-	c = strchr (*uid, ':');
+	c = strchr (*value, ':');
 	c[0] = '\0';
 
 	return TRUE;
@@ -101,7 +24,8 @@ osync_bool report_change (OSyncContext *ctx, gchar *type, gchar *uid, gchar *has
 {
 	gpe_environment *env = (gpe_environment *)osync_context_get_plugin_data (ctx);
 	OSyncChange *change = osync_change_new ();
-	osync_change_set_uid (change, uid);
+	osync_change_set_uid (change, g_strdup(uid));
+
 	if (!strcmp (type, "contact"))
 	{
 		osync_change_set_objtype_string (change, "contact");
@@ -118,8 +42,8 @@ osync_bool report_change (OSyncContext *ctx, gchar *type, gchar *uid, gchar *has
 		osync_change_set_objformat_string (change, "vtodo20");
 	}
 
-	osync_change_set_hash (change, hash);
-	osync_change_set_data (change, data, strlen (data), TRUE);
+	osync_change_set_hash (change, g_strdup(hash));
+	osync_change_set_data (change, g_strdup(data), strlen (data), TRUE);
 	
 	if (osync_hashtable_detect_change (env->hashtable, change)) {
 		osync_context_report_change (ctx, change);
--- base/gpe-opensync/src/utils.h
+++ base/gpe-opensync/src/utils.h
@@ -1,13 +1,14 @@
 #ifndef UTILS_H
 #define UTILS_H
 
+
 osync_bool add_item (OSyncContext *ctx, const char *type, const char *data, gchar **error);
 
 osync_bool modify_item (OSyncContext *ctx, const char *type, unsigned int uid, const char *data, gchar **error);
 
 osync_bool delete_item (OSyncContext *ctx, const char *type, unsigned int uid, gchar **error);
 
-osync_bool parse_uid_modified (gchar *string, gchar **uid, gchar **modified);
+osync_bool parse_value_modified (gchar *string, gchar **uid, gchar **modified);
 
 osync_bool report_change (OSyncContext *ctx, gchar *type, gchar *uid, gchar *hash, gchar *data);
 
