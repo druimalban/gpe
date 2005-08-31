f25e747dd8352e26bfe2a2f6c89148714dc8518c
martin <martin@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2005-08-31 20:53:55 +0000

    Added license header, removed port property.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@7585 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-opensync/src/gpe_xml.c
+++ base/gpe-opensync/src/gpe_xml.c
@@ -1,3 +1,23 @@
+/*
+ * gpe-sync - A plugin for the opensync framework
+ * Copyright (C) 2005  Martin Felis <martin@silef.de>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ * 
+ */
+
 #include "gpe_sync.h"
 
 /*This just opens the config file and returns the settings in the env */
@@ -11,7 +31,6 @@ osync_bool gpe_parse_settings(gpe_environment *env, char *data, int size)
 	// Set the defaults
 	env->device_addr = (char*)malloc(sizeof(char)*10);
 	strcpy(env->device_addr, "127.0.0.1");
-	env->port=6666;
 	env->username = (char*)malloc(sizeof(char)*9);
 	strcpy(env->username, "gpeuser");
 	
@@ -46,10 +65,6 @@ osync_bool gpe_parse_settings(gpe_environment *env, char *data, int size)
 			}
 			if (!xmlStrcmp(cur->name, (const xmlChar *)"handheld_user")) {
 				env->username = g_strdup(str);
-			}
-			if (!xmlStrcmp(cur->name, (const xmlChar *)"handheld_port")) {
-				// convert string to an int
-				env->port = atoi(str);
 			}
 			xmlFree(str);
 		}
