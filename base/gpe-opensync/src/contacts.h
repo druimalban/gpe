4084a6c967bc694681657230f8e99c31bbf27435
martin <martin@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2005-09-01 12:48:11 +0000

    Made gpe_contacts_get_changes return TRUE or FALSE.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@7630 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-opensync/src/contacts.h
+++ base/gpe-opensync/src/contacts.h
@@ -1,7 +1,28 @@
+/*
+ * gpe-sync - A plugin for the opensync framework
+ * Copyright (C) 2005  Martin Felis <martin@silef.de>
+ * 
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ * 
+ */
+
+
 #ifndef CONTACTS_H
 #define CONTACTS_H
 
-void gpe_contacts_get_changes(OSyncContext *ctx);
+osync_bool gpe_contacts_get_changes(OSyncContext *ctx);
 void gpe_contacts_setup(OSyncPluginInfo *info);
 
 #endif /* CONTACTS_H */
