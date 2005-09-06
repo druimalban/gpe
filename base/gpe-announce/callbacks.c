03d2c81871748f33676808aa6aca6539e29de5c6
ldecicco <ldecicco@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2005-09-06 15:24:10 +0000

    Fixing previous cvs conflict in callbacks.c
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@7654 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-announce/callbacks.c
+++ base/gpe-announce/callbacks.c
@@ -22,8 +22,10 @@
 #include <time.h>
 #include <fcntl.h>
 #include <gtk/gtk.h>
+
 #include <gst/gst.h>
-#include <dlfcn.h>	
+#include <dlfcn.h>
+
 #include "announce.h"
 #include <sys/ioctl.h>
 #include <linux/soundcard.h>
@@ -34,6 +36,7 @@
 
 #include <linux/types.h>
 #include <linux/ioctl.h>
+
 #include "buzzer.h"
 
 #define MIXER "/dev/mixer"
@@ -41,13 +44,26 @@
 #define SIZE 256
 #define SNOOZE 5
 
+/* Flag to reset sound volume after playing		*/
+static gboolean VolumeReset = TRUE;
+
 /* Flag to stop alarm sound thread from playing		*/
 static gboolean PlayAlarmStop = TRUE;
 
-<<<<<<< callbacks.c
 /* Flag for current sound configuration */
 static int sound_config;
 
+int fd;
+int curl, curr, curpcml, curpcmr;	
+pthread_t SoundThread;
+
+int buzzerfd = -1;
+
+#define BUZZER_FILE "/dev/misc/buzzer"
+#define CFG_NOSOUND 	-2
+#define CFG_AUTOMATIC 	-1
+
+/* GST player */
 struct player
 {
 	GstElement *filesrc, *decoder, *audiosink, *thread, *volume;
@@ -63,38 +79,14 @@ extern void *gst_handle;
 extern char *filename;
 player_t alarm_player=NULL;
 extern int alarm_volume;
-=======
-/* Flag for current sound configuration */
-static int sound_config;
-
->>>>>>> 1.15
-int fd;
-<<<<<<< callbacks.c
-int curl, curr, curpcml, curpcmr;
-=======
-int curl, curr, curpcml, curpcmr;	
->>>>>>> 1.15
-pthread_t SoundThread;
-
-int buzzerfd = -1;
 
 void play_url(player_t p, char *url);
-
-void play_gsm_sound_loop(char *);
 static void player_stop (player_t p);
 void player_set_volume (player_t p, int v);
 
 player_t player_new (int);
 
-#define BUZZER_FILE "/dev/misc/buzzer"
-<<<<<<< callbacks.c
-#define CFG_NOSOUND    -2
-#define CFG_AUTOMATIC  -1
-=======
-#define CFG_NOSOUND 	-2
-#define CFG_AUTOMATIC 	-1
 
->>>>>>> 1.15
 void open_buzzer (void)
 {
 	buzzerfd = open (BUZZER_FILE, O_WRONLY);
@@ -125,55 +117,6 @@ void buzzer_off (int sig)
 	exit (128 + sig);
 }
 
-<<<<<<< callbacks.c
-/*
- * This reads configuration from configfile.
- * It returns a (positive) number from 0 to 100 to indicate a fixed volume
- * or a negative constant to indicate automatic volume setting (default)
- * or disabled alarm sound.
- */
-int get_config (void)
-{
-	int result = CFG_AUTOMATIC;
-	char *filename = g_strdup_printf("%s/.gpe/alarm.conf", g_get_home_dir());
-	FILE *cfgfile;
-	int enabled = 1;
-	int automatic = 1;
-	int level = 0;
-
-	cfgfile = fopen(filename, "r");
-	if (cfgfile)
-	{
-		int val = -1, ret;
-		char buf[128];
-		while (fgets(buf, 128, cfgfile))
-		{
-			ret = sscanf(buf, "enabled %d", &val);
-			if (ret)
-				enabled = val;
-			ret = sscanf(buf, "automatic %d", &val);
-			if (ret)
-				automatic = val;
-			ret = sscanf(buf, "level %d", &val);
-			if (ret)
-			{
-				if ((val >= 0) && (val <=100))
-					level = val;
-			}
-		}
-		fclose(cfgfile);
-		if (!enabled)
-			result = CFG_NOSOUND;
-		else
-			if (!automatic)
-				result = level;
-	}
-	g_free(filename);
-	return result;
-}
-					
-int get_vol(int *left, int *right, int channel)
-=======
 
 /* 
  * This reads configuration from configfile. 
@@ -226,7 +169,6 @@ int get_config (void)
 }
 
 int get_vol(int *left, int *right, int channel)
->>>>>>> 1.15
 {
 	int vol;
 	int err;
@@ -244,12 +186,8 @@ int get_vol(int *left, int *right, int channel)
 	return err;
 }
 
-<<<<<<< callbacks.c
-int set_vol(int left, int right, int channel)
-=======
 
 int set_vol(int left, int right, int channel)
->>>>>>> 1.15
 {
 	int vol = left | (right << 8);
 	int err;
@@ -269,10 +207,10 @@ static void schedule_alarm(const gchar *buf, time_t when)
 	gchar *text;
 
 	if (buf)
-		text = g_strdup_printf ("/usr/bin/gpe-announce '%s'\n", buf);
+		text = g_strdup_printf ("gpe-announce '%s'\n", buf);
 	else
-		text = g_strdup_printf ("/usr/bin/gpe-announce\n");
-	schedule_set_alarm (1234, when, text);
+		text = g_strdup_printf ("gpe-announce\n");
+	schedule_set_alarm (1234, when, text, FALSE);
 	g_free (text);
 }
 
@@ -284,9 +222,6 @@ void play_melody(guint Tone1Pitch, guint Tone1Duration,
 	int i;
 	extern int times;
 	
-	if (sound_config == CFG_NOSOUND)
-		return;
-
 	if (sound_config == CFG_NOSOUND)
 		return;
 
@@ -312,14 +247,9 @@ void play_melody(guint Tone1Pitch, guint Tone1Duration,
 						 Tone2Duration);
 		}
 		soundgen_pause(snd_dev, TonePause);
-<<<<<<< callbacks.c
-		if (sound_config == CFG_AUTOMATIC)
-			switch (times) {
-=======
 
 		if (sound_config == CFG_AUTOMATIC)
 			switch (times) {
->>>>>>> 1.15
 				case 0:
 					set_vol(50,50,SOUND_MIXER_VOLUME);
 					break;
@@ -368,54 +298,40 @@ void play_melody(guint Tone1Pitch, guint Tone1Duration,
 
 void *play_alarm()
 {
-	if(filename != NULL){
-
+	/* Play sound file using gst */
+	if (filename != NULL && gst_handle)
+	{
 		alarm_player = player_new(alarm_volume);
 		play_url(alarm_player,filename);
-			
-	}else{
-		//Play Ambulance `melody`
-		play_melody(800,400,1,1100,400,2,2000);
+		
 	}
-	
-	pthread_exit(NULL);
-	
-return (NULL);
+	else
+	{
+		/* Play Ambulance `melody` */
+		play_melody(800,400,1,1100,400,2,2000);
+	        pthread_exit(NULL);
+	}	
+	return (NULL);
 }
 
 gint bells_and_whistles ()
 {
 	sound_config = get_config();
 	
+	VolumeReset = TRUE;
+	
 	open_buzzer ();
-<<<<<<< callbacks.c
-	 if((get_vol(&curl, &curr, SOUND_MIXER_VOLUME) == -1)
-		|| (get_vol(&curpcml, &curpcmr, SOUND_MIXER_PCM) == -1))
-=======
   
 	if((get_vol(&curl, &curr, SOUND_MIXER_VOLUME) == -1)
 		|| (get_vol(&curpcml, &curpcmr, SOUND_MIXER_PCM) == -1))
->>>>>>> 1.15
+	{
+		VolumeReset = FALSE;
 		printf("Unable to get volume\n");
-<<<<<<< callbacks.c
-	set_vol(100, 100, SOUND_MIXER_PCM);
-=======
+	}
 	set_vol(100, 100, SOUND_MIXER_PCM);
 	
->>>>>>> 1.15
 	signal (SIGINT, buzzer_off);
-	if (sound_config != CFG_NOSOUND)
-		set_buzzer (1000, 500);
-	if (sound_config != CFG_NOSOUND)
-	{
-		if (sound_config == CFG_AUTOMATIC)
-			set_vol(50, 50, SOUND_MIXER_VOLUME);
-		else
-			set_vol(sound_config, sound_config, SOUND_MIXER_VOLUME);
-	}
 
-<<<<<<< callbacks.c
-=======
 	if (sound_config != CFG_NOSOUND)
 		set_buzzer (1000, 500);
 	
@@ -426,7 +342,6 @@ gint bells_and_whistles ()
 		else
 			set_vol(sound_config, sound_config, SOUND_MIXER_VOLUME);
 	}
->>>>>>> 1.15
 	PlayAlarmStop = FALSE;
 	if (pthread_create(&SoundThread, NULL, play_alarm, NULL) != 0) {
 		g_print("pthread_create() failed\n");
@@ -473,8 +388,11 @@ gint on_ok_clicked (GtkButton *button, gpointer user_data)
 	PlayAlarmStop = TRUE;
 	set_buzzer (0, 0);
 	pthread_join(SoundThread, NULL);
-	set_vol(curl, curr, SOUND_MIXER_VOLUME);
-	set_vol(curpcml, curpcmr, SOUND_MIXER_PCM);
+	if (VolumeReset)
+	{
+		set_vol(curl, curr, SOUND_MIXER_VOLUME);
+		set_vol(curpcml, curpcmr, SOUND_MIXER_PCM);
+	}
 	
 	gtk_main_quit();
 	return(FALSE);
@@ -484,9 +402,6 @@ gint on_mute_clicked (GtkButton *button, gpointer user_data)
 {
 	PlayAlarmStop = TRUE;
 	set_buzzer (0, 0);
-	if(alarm_player != NULL){	
-		player_stop(alarm_player);
-	}
 	return(FALSE);
 }
 
@@ -494,48 +409,50 @@ player_t
 player_new (int volume_value)
 {
 	player_t p = g_malloc0 (sizeof (struct player));
-	char *src = getenv ("NMF_SRC");
-	char *sink = getenv ("NMF_SINK");
+	char *src ;
+	char *sink ;
 	
-	if (!src)
-		src = "filesrc";
-	if (!sink)
-		sink = "esdsink";
-	printf("src: %s sink: %s\n",src,sink);	
+	src = "filesrc";
+	sink = "esdsink";
 	p->source_elem = g_strdup (src);
 	p->sink_elem = g_strdup (sink);
-	if(volume_value>0){
+	if (volume_value>0)
+	{
 		p->volume_value=volume_value;
-	}else{
+	}
+	else
+	{
 		p->volume_value=256; /* Set to the max! */
 	}
-	if(gst_handle){
-            void (*gst_init)(int *argc, char **argv[]);
-            gst_init = dlsym(gst_handle, "gst_init");
-      	    gst_init(NULL,NULL);
-	}else{
+	if (gst_handle)
+	{
+		void (*gst_init)(int *argc, char **argv[]);
+		gst_init = dlsym(gst_handle, "gst_init");
+		gst_init(NULL,NULL);
+	} 
+	else
+	{
 		printf("ERROR!\n");
 	}
 	return p;
 }
 
+
 /* Stolen from gpe-nmf :) , thank you philip */
 void play_url(player_t p, char *url)
 {
-	if(gst_handle){
-
+	if (gst_handle)
+	{
 		GstElement* (*gst_thread_new)(const gchar *name);
 		GstElement* (*gst_element_factory_make)(const gchar *factoryname,
-			                                      const gchar *name);
-
+				const gchar *name);
+		
 		void   (*gst_bin_add_many)(GstBin *bin,GstElement *element_1,...);
 		gboolean  (*gst_element_link_many)(GstElement *element_1,
-						   GstElement *element_2,
-						   ...);
-
+				GstElement *element_2,...);
 		GstElementStateReturn (*gst_element_set_state) (GstElement *element,
-		                                             GstElementState state);
-	
+				GstElementState state);
+		
 		gst_thread_new = dlsym(gst_handle,"gst_thread_new");
 		gst_element_factory_make = dlsym(gst_handle,"gst_element_factory_make");
 		gst_bin_add_many = dlsym(gst_handle,"gst_bin_add_many");
@@ -543,25 +460,12 @@ void play_url(player_t p, char *url)
 		gst_element_set_state = dlsym(gst_handle, "gst_element_set_state");
 		
 		p->thread = (*gst_thread_new) ("thread");
-	
+		
 		p->filesrc   = gst_element_factory_make (p->source_elem, "disk_source");
 		p->decoder   = gst_element_factory_make ("spider", "decoder");
 		p->volume    = gst_element_factory_make ("volume", "volume");
 		p->audiosink = gst_element_factory_make (p->sink_elem , "play_audio");
-	
 		g_object_set (G_OBJECT (p->filesrc), "location", url, NULL);
-	
-		if (!p->filesrc)
-			printf("filesrc error\n");
-		
-		if(!p->decoder)
-			printf("decoder error\n");	
-			
-		if(!p->volume)
-			printf("Volume error\n");
-	
-		if(!p->audiosink)
-		 	printf("audiosink error\n");
 		
 		player_set_volume(p,p->volume_value);
 		gst_bin_add_many ( (p->thread), p->filesrc, p->decoder, p->volume, p->audiosink, NULL);
@@ -569,19 +473,21 @@ void play_url(player_t p, char *url)
 		gst_element_set_state (p->thread, GST_STATE_PLAYING);
 	}
 }
+
+
 static void
 player_stop (player_t p)
-{
-    if(gst_handle){
-	  void (* gst_element_set_state)();
-	  gst_element_set_state = dlsym(gst_handle,"gst_element_set_state");
-	  if (p->thread)
-	  {
-		  gst_element_set_state (p->thread, GST_STATE_NULL);
-	  }
-   }
+{	
+	if (gst_handle)
+	{
+		void (* gst_element_set_state)();
+		gst_element_set_state = dlsym(gst_handle,"gst_element_set_state");
+		if (p->thread)
+		{
+			gst_element_set_state (p->thread, GST_STATE_NULL);
+		}
+	}
 }
-
 
 void
 player_set_volume (player_t p, int v)
@@ -594,4 +500,3 @@ player_set_volume (player_t p, int v)
 	
 	g_object_set_property (G_OBJECT (p->volume), "volume", &value);
 }
-
