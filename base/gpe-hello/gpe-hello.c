3cfdd881bd915223bd89b3bee11fd04d241a9e07
philippe <philippe@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2005-09-07 23:29:01 +0000

    demo tutorial program for teaching GPE programming. Initial try.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@7659 d4735bf7-8e1f-0410-bce4-ef584eb11c25

---
+++ base/gpe-hello/gpe-hello.c
@@ -0,0 +1,106 @@
+/*
+ * gpe-hello v0.1
+ *
+ * Tutorial program to learn programming for GPE 
+ *
+ * Copyright (c) 2005 Philippe De Swert
+ *
+ * Contact : philippedeswert@scarlet.be
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+/* the usual suspects */
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/* gtk includes */
+#include <gtk/gtk.h>
+#include <glib.h>
+
+/* internationalisation aka i8n */
+#include <libintl.h>
+#include <locale.h> 
+
+/* gpe includes */
+#include <gpe/init.h>
+#include <gpe/errorbox.h>
+#include <gpe/pixmaps.h>
+#include <gpe/picturebutton.h> 
+
+//#define DEBUG /* uncomment this if you want debug output*/
+
+struct gpe_icon my_icons[] = 
+{
+  { "hello", PREFIX "/share/pixmaps/gpe-hello.png" },
+  {NULL, NULL}
+};
+
+static void hello_box (void);
+
+int
+main (int argc, char *argv[])
+{
+  GtkWidget *app;
+  GtkWidget *contentbox, *quit_button, *hello_button;
+
+  if (gpe_application_init (&argc, &argv) == FALSE)
+    exit (1);
+
+  if (gpe_load_icons (my_icons) == FALSE)
+    exit (1);
+
+  //create application window
+  app = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  g_signal_connect (G_OBJECT (app), "delete-event", gtk_main_quit, NULL);
+  gtk_window_set_title (GTK_WINDOW (app), "GPE HELLO");
+  gtk_widget_realize (app);
+
+  //set fullscreen (uncomment the following line if you want a fullscreen application)
+  //gtk_window_fullscreen(GTK_WINDOW(app));
+
+  //create boxes
+  contentbox = gtk_hbox_new (FALSE, 0);
+
+  //show icon in window decoration
+  gpe_set_window_icon (app, "hello");
+
+  //add a hello & quit button with a gtk stock icon
+  hello_button = gpe_button_new_from_stock(GTK_STOCK_ABOUT, GPE_BUTTON_TYPE_BOTH);
+  quit_button = gpe_button_new_from_stock(GTK_STOCK_QUIT, GPE_BUTTON_TYPE_BOTH);
+
+  //attach signals to the buttons
+  g_signal_connect(GTK_OBJECT(hello_button), "clicked",
+		   G_CALLBACK(hello_box), NULL);
+  g_signal_connect(GTK_OBJECT(quit_button), "clicked",
+		   G_CALLBACK(gtk_main_quit), NULL);
+
+  //add buttons to the box
+  gtk_box_pack_start(GTK_BOX(contentbox), GTK_WIDGET(hello_button), TRUE, TRUE, 5);
+  gtk_box_pack_start(GTK_BOX(contentbox), GTK_WIDGET(quit_button), TRUE, TRUE, 5);
+
+  //make everything viewable
+  gtk_container_add (GTK_CONTAINER (app), contentbox);
+
+  gtk_widget_show_all (GTK_WIDGET (contentbox));
+  gtk_widget_show (GTK_WIDGET (app));
+  gtk_main ();
+
+  exit (0);
+}
+
+static void hello_box (void)
+{
+ gpe_error_box(_("Welcome to GPE!"));
+}
