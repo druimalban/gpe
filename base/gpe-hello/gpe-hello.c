edb66e30a116a4cf8c5eb305f19a11d0fb6331ed
florian <florian@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2005-11-01 11:27:18 +0000

    Replace C++ comments, some minor improvememnts. Add more verbose comments, add toolbar. Add GPE typical sizings and text area.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@7804 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-hello/gpe-hello.c
+++ base/gpe-hello/gpe-hello.c
@@ -34,23 +34,69 @@
 #include <locale.h> 
 
 /* gpe includes */
-#include <gpe/init.h>
-#include <gpe/errorbox.h>
-#include <gpe/pixmaps.h>
-#include <gpe/picturebutton.h> 
+#include <gpe/init.h>           /* gpe_application_init */
+#include <gpe/errorbox.h>       /* gpe_error_box */
+#include <gpe/pixmaps.h>        /* gpe_load_icons */
+#include <gpe/picturebutton.h>  /* gpe_button_new_from_stock */
+#include <gpe/spacing.h>        /* gpe_get_border, gpe_get_boxspacing */
 
 #define _(x) gettext(x)
 //#define DEBUG /* uncomment this if you want debug output*/
 
+/*
+ * This struct holds all pixmaps handled by libgpewidget functions.
+ * It consists of a set of identifiers and icon files. For files in the
+ * default GPE icon location you can omit the path and the extension.
+ */
 struct gpe_icon my_icons[] = 
 {
   { "hello", PREFIX "/share/pixmaps/gpe-hello.png" },
   {NULL, NULL}
 };
 
-static void hello_box (void)
+/* Just do someting... this displays a nasty error box */
+static void 
+hello_box (void)
 {
  gpe_error_box(_("Welcome to GPE!"));
+}
+
+
+/* 
+ * This creates the toolbar, it is not essential to have this code in a 
+ * separate function, but it makes the code easier to read.
+ */
+static GtkWidget*
+toolbar_create(void)
+{
+  GtkWidget *toolbar;
+  GtkToolItem *item;
+  
+  /* Create a toolbar widget and define its orientation. The size and layout
+     is defined externally via xsettings. */
+  toolbar = gtk_toolbar_new();
+  gtk_toolbar_set_orientation(GTK_TOOLBAR(toolbar), GTK_ORIENTATION_HORIZONTAL);
+
+  /* Create a tool button and add it to the leftmost free position.*/
+  item = gtk_tool_button_new_from_stock(GTK_STOCK_ABOUT);
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), item, -1);
+
+  /* Connect "clicked" signal of the toolbutton. */
+  g_signal_connect(G_OBJECT(item), "clicked", G_CALLBACK(hello_box), NULL);
+
+  /* Create a separator taking up the free space to the end of the toolbar. */
+  item = gtk_separator_tool_item_new();
+  gtk_separator_tool_item_set_draw(GTK_SEPARATOR_TOOL_ITEM(item), FALSE);
+  gtk_tool_item_set_expand(item, TRUE);
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), item, -1);
+    
+  /* Create the close button and add it to the leftmost free position.*/
+  item = gtk_tool_button_new_from_stock(GTK_STOCK_QUIT);
+  gtk_toolbar_insert(GTK_TOOLBAR(toolbar), item, -1);
+
+  /* Connect "clicked" signal of the toolbutton to close the application. */
+  g_signal_connect(G_OBJECT(item), "clicked", G_CALLBACK(gtk_main_quit), NULL);
+  return toolbar;
 }
 
 int
@@ -58,6 +104,7 @@ main (int argc, char *argv[])
 {
   GtkWidget *app;
   GtkWidget *contentbox, *quit_button, *hello_button;
+  GtkWidget *mainbox, *toolbar, *textarea;
 
   if (gpe_application_init (&argc, &argv) == FALSE)
     exit (1);
@@ -65,39 +112,58 @@ main (int argc, char *argv[])
   if (gpe_load_icons (my_icons) == FALSE)
     exit (1);
 
-  //create application window
+  /* create application window */
   app = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+  /* gtk_window_set_type_hint(GTK_WINDOW(app), GDK_WINDOW_TYPE_HINT_DIALOG); */
+  
   g_signal_connect (G_OBJECT (app), "delete-event", gtk_main_quit, NULL);
   gtk_window_set_title (GTK_WINDOW (app), "GPE HELLO");
   
   gtk_widget_realize (app);
 
-  //show icon in window decoration
+  /* show icon in window decoration */
   gpe_set_window_icon (app, "hello");
 
-  //set fullscreen (uncomment the following line if you want a fullscreen application)
-  //gtk_window_fullscreen(GTK_WINDOW(app));
+  /* set fullscreen (uncomment the following line if you want a fullscreen application)
+   * On PDAs we ususall have Matchbox doing this for use */
+  /* gtk_window_fullscreen(GTK_WINDOW(app)); */
 
-  //create boxes
-  contentbox = gtk_hbox_new (FALSE, 0);
+  /* call function creating main window toolbar, see above */
+  toolbar = toolbar_create();
+  
+  /* the text area is an example widget taking up the major part of the window */
+  textarea = gtk_text_view_new();
 
-  //add a hello & quit button with a gtk stock icon
+  /* create boxes */
+  contentbox = gtk_hbox_new (FALSE, gpe_get_boxspacing());
+  mainbox = gtk_vbox_new (FALSE, gpe_get_boxspacing());
+
+  /* set border with */
+  gtk_container_set_border_width(GTK_CONTAINER(contentbox), gpe_get_border());
+
+  /* add a hello & quit button with a gtk stock icon */
   hello_button = gpe_button_new_from_stock(GTK_STOCK_ABOUT, GPE_BUTTON_TYPE_BOTH);
   quit_button = gpe_button_new_from_stock(GTK_STOCK_QUIT, GPE_BUTTON_TYPE_BOTH);
 
-  //attach signals to the buttons
+  /* attach signals to the buttons */
   g_signal_connect(GTK_OBJECT(hello_button), "clicked",
 		   G_CALLBACK(hello_box), NULL);
   g_signal_connect(GTK_OBJECT(quit_button), "clicked",
 		   G_CALLBACK(gtk_main_quit), NULL);
 
-  //add buttons to the box
-  gtk_box_pack_start(GTK_BOX(contentbox), hello_button, TRUE, TRUE, 5);
-  gtk_box_pack_start(GTK_BOX(contentbox), quit_button, TRUE, TRUE, 5);
+  /* add buttons to the box */
+  gtk_box_pack_start(GTK_BOX(contentbox), hello_button, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(contentbox), quit_button, TRUE, TRUE, 0);
 
-  //make everything viewable
-  gtk_container_add (GTK_CONTAINER (app), contentbox);
+  /* pack the box with buttons and and the toolbar to the main box */
+  gtk_box_pack_start(GTK_BOX(mainbox), toolbar, FALSE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(mainbox), textarea, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(mainbox), contentbox, FALSE, TRUE, 0);
 
+  /* add the main box to the window */
+  gtk_container_add (GTK_CONTAINER (app), mainbox);
+
+  /* make everything visible and start the main loop */
   gtk_widget_show_all (app);
   gtk_main ();
 
