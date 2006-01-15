b6100db9d7e5827050205a51ad3bb3d4775e5117
pb <pb@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2006-01-15 17:50:37 +0000

    update date in copyright
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@8060 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-calendar/day_popup.c
+++ base/gpe-calendar/day_popup.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2002, 2003 Philip Blundell <philb@gnu.org>
+ * Copyright (C) 2002, 2003, 2006 Philip Blundell <philb@gnu.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
