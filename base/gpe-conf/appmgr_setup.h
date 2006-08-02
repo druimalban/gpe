9ebc6ce522cce792c57eee671cec83d961dfd9de
florian <florian@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2006-08-02 12:13:19 +0000

    Remove unused files. Release version 0.2.0.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@8689 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-conf/appmgr_setup.h
+++
@@ -1,4 +0,0 @@
-GtkWidget *Appmgr_Build_Objects();
-void Appmgr_Free_Objects();
-void Appmgr_Save();
-void Appmgr_Restore();
--- base/gpe-conf/users.h
+++
@@ -1,4 +0,0 @@
-GtkWidget *Users_Build_Objects(gboolean password_only);
-void Users_Save();
-void Users_Free_Objects();
-void Users_Restore();
