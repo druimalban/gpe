9af861d1784e3da7d658d59bac2f34bf26c4d5e5
florian <florian@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2006-08-07 15:40:32 +0000

    Default to "Europe/Berlin", Vienna is not in the default zoneinfo package
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@8712 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-conf/modules/timeanddate.c
+++ base/gpe-conf/modules/timeanddate.c
@@ -178,7 +178,7 @@ get_current_tz(void)
 	{
 		g_printerr ("Could not read timezone file (%s), using default.\n", err->message);
 		g_error_free (err);
-		result = g_strdup ("Europe/Vienna");
+		result = g_strdup ("Europe/Berlin");
 	}
 	return result;  
 }
@@ -541,7 +541,7 @@ Time_Build_Objects(gboolean nonroot)
 	if (selTimezoneAreaIndx == -1)
 	{
 		g_free(fstr);
-		fstr = g_strdup("Europe/Vienna");
+		fstr = g_strdup("Europe/Berlin");
 		selTimezoneAreaIndx	= getTimezoneAreaIndexByTimezoneName(fstr);
 	}
 	if (selTimezoneAreaIndx == -1)
