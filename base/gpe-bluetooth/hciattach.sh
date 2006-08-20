6f8b24b1cbb7226e333bfca3978ac7a0a1881b61
florian <florian@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2006-08-20 13:25:12 +0000

    Add support for bluetooth script handling. (by Philipp Zabel)
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@8757 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/gpe-bluetooth/hciattach.sh
+++ base/gpe-bluetooth/hciattach.sh
@@ -3,7 +3,12 @@
 if [ -f /etc/sysconfig/bluetooth ]; then
   . /etc/sysconfig/bluetooth
 
-  exec /sbin/hciattach -n $BLUETOOTH_PORT $BLUETOOTH_PROTOCOL $BLUETOOTH_SPEED
+  if [ -f "$BLUETOOTH_SCRIPT" ]; then
+    BTS="-S $BLUETOOTH_SCRIPT"
+  fi
+
+  exec /sbin/hciattach -n $BLUETOOTH_PORT $BLUETOOTH_PROTOCOL $BLUETOOTH_SPEED $BTS
+
 else
   echo "Bluetooth not configured"
   exit 1
