024105a9d0d142b0cc07a7370318b39e0556947d
philippe <philippe@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2007-09-04 17:16:36 +0000

    small website update/refresh
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@9208 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- html/includes/head.php
+++ html/includes/head.php
@@ -6,32 +6,80 @@
 ?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
-<HTML> 
-<HEAD> 
-<TITLE> GPE: <?php echo $addtitle; ?></TITLE> 
-<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<LINK REL="stylesheet" HREF="/css/style.css" TYPE="text/css">
-<LINK REL="shortcut icon" HREF="/images/favicon.png" TYPE="image/png">
-</HEAD>
-<BODY>
-<div id="website">
-<div id="header"><table border="0" width="100%">
-<tr>
-<td width="60" height="64">
-<a href="http://gpe.linuxtogo.org">
-<img src="/images/gpe-logo2.png" alt="[The GPE Logo] > gpe.linuxtogo.org">
-</td>
-<td width="*" valign="middle" height="64" halign="middle">
-<h1> GPE: <?php echo $addtitle; ?></h1>
-</td>
-<td halign="right">
-<a href="http://www.linuxtogo.org">
-<img src="/images/logo-w.png" alt="[The linuxtogo.org Logo]> linuxtogo.org">
-</a>
-</td>
-</tr>
-</table>
+<html>
+<head>
+<title>GPE: The GPE Palmtop Environment</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="shortcut icon" href="http://gpe.linuxtogo.org/images/favicon.png" type="image/png">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body><script type="text/javascript">
+if (document.images)
+   {
+     pic1on= new Image(100,28);
+     pic1on.src="images/Home-o.jpg";  
+     pic2on= new Image(100,28);
+     pic2on.src="images/Projects-o.jpg";
+     pic3on= new Image(100,27);
+     pic3on.src="images/Documentation-o.jpg";  
+     pic4on= new Image(100,28);
+     pic4on.src="images/Download-o.jpg";
+     pic5on= new Image(100,28);
+     pic5on.src="images/Contact-o.jpg";
+     pic6on= new Image(100,28);
+     pic6on.src="images/Resources-o.jpg"
+
+
+     pic1off= new Image(100,28);
+     pic1off.src="images/Home.jpg";
+     pic2off= new Image(100,28);
+     pic2off.src="images/Projects.jpg";
+     pic3off= new Image(100,27);
+     pic3off.src="images/Documentation.jpg";
+     pic4off= new Image(100,28);
+     pic4off.src="images/Download.jpg";
+     pic5off= new Image(100,28);
+     pic5off.src="images/Contact.jpg";
+     pic6off= new Image(100,28);
+     pic6off.src="images/Resources.jpg";
+
+   }
+
+function lightup(imgName)
+ {
+   if (document.images)
+    {
+      imgOn=eval(imgName + "on.src");
+      document[imgName].src= imgOn;
+    }
+ }
+
+function turnoff(imgName)
+ {
+   if (document.images)
+    {
+      imgOff=eval(imgName + "off.src");
+      document[imgName].src= imgOff;
+    }
+ }
+</script>
+
+<div class="header">
+<a href="http://gpe.linuxtogo.org/">
+<img class="logo" src="images/logo3s.png" alt="[The GPE Logo] > gpe.linuxtogo.org"></a>
+<a href="http://www.linuxtogo.org/"><img class="logow" src="images/logo-w.png" alt="[The Linuxtogo.org] > www.linuxtogo.org"></a>
 </div>
-<?php include("http://gpe.linuxtogo.org/includes/menu.php"); ?>
+
+<div class="menu" align="left">
+<ul class="menu">
+<li><a href="http://gpe.linuxtogo.org/"onMouseover="lightup('pic1')" onMouseout="turnoff('pic1')"><img src="images/Home.jpg" alt="Home" name="pic1" width="100" height="28"></a></li>
+<li><a href="http://gpe.linuxtogo.org/projects/"onMouseover="lightup('pic2')" onMouseout="turnoff('pic2')"><img src="images/Projects.jpg" alt="Projects" name="pic2" width="100" height="28"></a></li>
+<li><a href="http://gpe.linuxtogo.org/documentation.phtml"onMouseover="lightup('pic3')" onMouseout="turnoff('pic3')"><img src="images/Documentation.jpg" alt="Documentation" name="pic3" width="100" height="27" id="pic3"></a></li>
+<li><a href="http://gpe.linuxtogo.org/download.phtml"onMouseover="lightup('pic4')" onMouseout="turnoff('pic4')"><img src="images/Download.jpg" alt="Download" name="pic4" width="100" height="28" id="pic4"></a></li>
+<li><a href="http://gpe.linuxtogo.org/contact.phtml"onMouseover="lightup('pic5')" onMouseout="turnoff('pic5')"><img src="images/Contact.jpg" alt="Contact" name="pic5" width="100" height="28" id="pic5"></a></li>
+<li><a href="http://gpe.linuxtogo.org/linkres.phtml"onMouseover="lightup('pic6')" onMouseout="turnoff('pic6')"><img src="images/Resources.jpg" alt="Resources" name="pic6" width="100" height="28" id="pic6"></a></li>
+</ul>
+</div>
 <hr>
-<div id="content">
+<div class="content">
+
