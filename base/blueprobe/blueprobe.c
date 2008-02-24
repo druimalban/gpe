3e09249bef3c1b84446ba464ebf950d451d52355
fboor <fboor@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2008-02-24 19:18:32 +0000

    Apply patch (from OE) to build against uclibc.
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@9352 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/blueprobe/blueprobe.c
+++ base/blueprobe/blueprobe.c
@@ -25,14 +25,22 @@ uart_speed(int s)
     return B57600;
   case 115200:
     return B115200;
+#ifdef B230400
   case 230400:
     return B230400;
+#endif
+#ifdef B460800
   case 460800:
     return B460800;
+#endif
+#ifdef B921600
   case 921600:
     return B921600;
+#endif
+#ifdef B1000000
   case 1000000:
     return B1000000;
+#endif
   default:
     return B57600;
   }
