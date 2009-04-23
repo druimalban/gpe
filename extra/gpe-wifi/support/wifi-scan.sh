9d84323d26294c4da48ca5374406cc957f529766
prpplague <prpplague@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2009-04-23 18:58:14 +0000

    add support for listing the connected profile
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@9892 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- extra/gpe-wifi/support/wifi-scan.sh
+++ extra/gpe-wifi/support/wifi-scan.sh
@@ -3,9 +3,13 @@ echo "starting scan!"
 if [ -e /tmp/wifi.scan ] ; then
     /bin/rm -rf /tmp/wifi.scan
 fi
+connected=`iwconfig eth0 | grep -e "ESSID" | cut -f 2 -d ':' | cut -f 2 -d '"'`
+echo "connected = ${connected}" >> /tmp/wifi.scan
 
 for net_list in `iwlist eth0 scan | grep -e "ESSID" | cut -f 2 -d ':' | cut -f 2 -d '"'` ; do
-    echo "essid = ${net_list}" >> /tmp/wifi.scan
+	if [ "$connected" != "$net_list" ] ; then 
+    		echo "essid = ${net_list}" >> /tmp/wifi.scan
+	fi
 done
 echo "scan complete!"
 sleep 2
