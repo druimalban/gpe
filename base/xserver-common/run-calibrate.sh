b827432c93ce5c05992b1a5b41317068c15ee250
fboor <fboor@d4735bf7-8e1f-0410-bce4-ef584eb11c25>
2010-09-15 21:23:40 +0000

    Prefer ts_calibrate over xtscal
    git-svn-id: svn://projects.linuxtogo.org/svn/gpe/trunk@10091 d4735bf7-8e1f-0410-bce4-ef584eb11c25

--- base/xserver-common/run-calibrate.sh
+++ base/xserver-common/run-calibrate.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-if [ -x /usr/bin/xtscal ]; then
-	exec /usr/bin/xtscal
+if [ -x /usr/bin/ts_calibrate ]; then
+	exec /usr/bin/ts_calibrate
 else
-	if [ -x /usr/bin/ts_calibrate ]; then
-	        exec /usr/bin/ts_calibrate
+	if [ -x /usr/bin/xtscal ]; then
+	        exec /usr/bin/xtscal
 	fi
 fi
 
